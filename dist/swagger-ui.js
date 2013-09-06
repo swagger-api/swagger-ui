bc932401999143a06ac5576e10b4681a9433b233
Tony Tam <fehguy@gmail.com>
2013-09-05 20:03:31 -0700

    fixed merge issue

--- dist/swagger-ui.js
+++ dist/swagger-ui.js
@@ -655,7 +655,7 @@ function program13(depth0,data) {
 function program14(depth0,data) {
   
   var buffer = "", stack1;
-  buffer += "\n        <option value='";
+  buffer += "\n        <option selected=\"\" value='";
   if (stack1 = helpers.value) { stack1 = stack1.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.value; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
   buffer += escapeExpression(stack1)
