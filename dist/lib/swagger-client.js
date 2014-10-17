d16b149f3997920defd1b06f24096f3d7f822542
Tony Tam <fehguy@gmail.com>
2014-10-17 08:52:44 -0700

    fix for #632, param names other than

--- dist/lib/swagger-client.js
+++ dist/lib/swagger-client.js
@@ -1,5 +1,61 @@
 // swagger-client.js
 // version 2.1.0-alpha.1
+/**
+ * Array Model
+ **/
+var ArrayModel = function(definition) {
+  this.name = "name";
+  this.definition = definition || {};
+  this.properties = [];
+  this.type;
+  this.ref;
+
+  var requiredFields = definition.enum || [];
+  var items = definition.items;
+  if(items) {
+    var type = items.type;
+    if(items.type) {
+      this.type = typeFromJsonSchema(type.type, type.format);
+    }
+    else {
+      this.ref = items['$ref'];
+    }
+  }
+}
+
+ArrayModel.prototype.createJSONSample = function(modelsToIgnore) {
+  var result;
+  modelsToIgnore = (modelsToIgnore||{})
+  if(this.type) {
+    result = type;
+  }
+  else if (this.ref) {
+    var name = simpleRef(this.ref);
+    result = models[name].createJSONSample();
+  }
+  return [ result ];
+};
+
+ArrayModel.prototype.getSampleValue = function(modelsToIgnore) {
+  var result;
+  modelsToIgnore = (modelsToIgnore || {})
+  if(this.type) {
+    result = type;
+  }
+  else if (this.ref) {
+    var name = simpleRef(this.ref);
+    result = models[name].getSampleValue(modelsToIgnore);
+  }
+  return [ result ];
+}
+
+ArrayModel.prototype.getMockSignature = function(modelsToIgnore) {
+  var propertiesStr = [];
+
+  if(this.ref) {
+    return models[simpleRef(this.ref)].getMockSignature();
+  }
+};
 
 /**
  * SwaggerAuthorizations applys the correct authorization to an operation being executed
@@ -96,63 +152,7 @@ PasswordAuthorization.prototype.apply = function(obj, authorizations) {
   var base64encoder = this._btoa;
   obj.headers["Authorization"] = "Basic " + base64encoder(this.username + ":" + this.password);
   return true;
-};/**
- * Array Model
- **/
-var ArrayModel = function(definition) {
-  this.name = "name";
-  this.definition = definition || {};
-  this.properties = [];
-  this.type;
-  this.ref;
-
-  var requiredFields = definition.enum || [];
-  var items = definition.items;
-  if(items) {
-    var type = items.type;
-    if(items.type) {
-      this.type = typeFromJsonSchema(type.type, type.format);
-    }
-    else {
-      this.ref = items['$ref'];
-    }
-  }
-}
-
-ArrayModel.prototype.createJSONSample = function(modelsToIgnore) {
-  var result;
-  modelsToIgnore = (modelsToIgnore||{})
-  if(this.type) {
-    result = type;
-  }
-  else if (this.ref) {
-    var name = simpleRef(this.ref);
-    result = models[name].createJSONSample();
-  }
-  return [ result ];
-};
-
-ArrayModel.prototype.getSampleValue = function(modelsToIgnore) {
-  var result;
-  modelsToIgnore = (modelsToIgnore || {})
-  if(this.type) {
-    result = type;
-  }
-  else if (this.ref) {
-    var name = simpleRef(this.ref);
-    result = models[name].getSampleValue(modelsToIgnore);
-  }
-  return [ result ];
-}
-
-ArrayModel.prototype.getMockSignature = function(modelsToIgnore) {
-  var propertiesStr = [];
-
-  if(this.ref) {
-    return models[simpleRef(this.ref)].getMockSignature();
-  }
-};
-var __bind = function(fn, me){
+};var __bind = function(fn, me){
   return function(){
     return fn.apply(me, arguments);
   };
