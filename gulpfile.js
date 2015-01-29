ecb8df4b95047aa691a1a934d4eac70af201ac9b
Mohsen Azimi <msn.azi@gmail.com>
2015-01-29 10:26:57 -0800

    Add less and copy html files

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var coffee = require('gulp-coffee');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require("gulp-rename");
+var less = require('gulp-less');
 
 /*
  * Clean ups ./dist folder
@@ -26,7 +27,7 @@ gulp.task('dist', function() {
   return gulp
     .src([
       './src/main/coffeescript/view/**/*.coffee',
-      './src/main/coffeescript/SwaggerUi'
+      './src/main/coffeescript/SwaggerUi.coffee'
     ])
     .pipe(coffee({bare: true}))
     .pipe(concat('swagger-ui.js'))
@@ -37,15 +38,36 @@ gulp.task('dist', function() {
     .on('error', gutil.log);
 });
 
+/*
+ * Processes less files into CSS files
+*/
+gulp.task('less', function() {
+
+  gulp
+    .src('./src/main/less/screen.less')
+    .pipe(less())
+    .pipe(gulp.dest('./src/main/html/css/screen.css'));
+
+  gulp
+    .src('./src/main/less/reset.less')
+    .pipe(less())
+    .pipe(gulp.dest('./src/main/html/css/reset.css'));
+});
 
 /*
- * Copy lib files
+ * Copy lib and html folders
 */
 gulp.task('copy', function() {
 
-  return gulp
+  // copy JavaScript files inside lib folder
+  gulp
     .src(['./lib/**/*.js'])
     .pipe(gulp.dest('./dist/lib'));
+
+  // copy all files inside html folder
+  gulp
+    .src(['./src/main/html/**/*'])
+    .pipe(gulp.dest('./dist'));
 });
 
 
