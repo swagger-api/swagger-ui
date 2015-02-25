82323e2eec1e081bc6b9e9b0585e7c8b2ae09d8a
Mohsen Azimi <msn.azi@gmail.com>
2015-02-25 13:52:17 -0800

    When running `gulp watch` watch handlebars file changes too

--- gulpfile.js
+++ gulpfile.js
@@ -118,7 +118,7 @@ gulp.task('copy', ['less'], function() {
  * Watch for changes and recompile
  */
 gulp.task('watch', function() {
-  return watch(['./src/**/*.{coffee,js,less}'], function() {
+  return watch(['./src/**/*.{coffee,js,less,handlebars}'], function() {
     gulp.start('default');
   });
 });
