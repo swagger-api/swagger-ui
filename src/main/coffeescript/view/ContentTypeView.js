3944828c60934f0332fd47242987fac9f0cd02e6
Mohsen Azimi <msn.azi@gmail.com>
2015-03-12 11:11:20 -0700

    Convert view/ContentTypeView.coffee

---
+++ src/main/coffeescript/view/ContentTypeView.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var ContentTypeView = Backbone.View.extend({
+  initialize: function() {},
+
+  render: function(){
+    $(this.el).html(Handlebars.templates.content_type(this.model));
+
+    $('label[for=contentType]', $(this.el)).text('Response Content Type');
+
+    return this;
+  }
+});
