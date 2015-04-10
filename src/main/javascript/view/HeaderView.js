0e9db22ec8158d9b7c49ce3f231025a6b9b68e79
Maks3w <github.maks3w@virtualplanets.net>
2015-04-10 15:53:26 +0200

    Remove dead code
    Buttons not longer exists on templates

--- src/main/javascript/view/HeaderView.js
+++ src/main/javascript/view/HeaderView.js
@@ -2,26 +2,12 @@
 
 SwaggerUi.Views.HeaderView = Backbone.View.extend({
   events: {
-    'click #show-pet-store-icon'    : 'showPetStore',
-    'click #show-wordnik-dev-icon'  : 'showWordnikDev',
     'click #explore'                : 'showCustom',
     'keyup #input_baseUrl'          : 'showCustomOnKeyup',
     'keyup #input_apiKey'           : 'showCustomOnKeyup'
   },
 
   initialize: function(){},
-
-  showPetStore: function(){
-    this.trigger('update-swagger-ui', {
-      url:'http://petstore.swagger.io/v2/swagger.json'
-    });
-  },
-
-  showWordnikDev: function(){
-    this.trigger('update-swagger-ui', {
-      url: 'http://api.wordnik.com/v4/resources.json'
-    });
-  },
 
   showCustomOnKeyup: function(e){
     if (e.keyCode === 13) {
