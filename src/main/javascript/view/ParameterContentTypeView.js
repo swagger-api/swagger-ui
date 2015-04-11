07584cb54f7d0b7970192227b7334e1ed7411c3f
Joe Wolf <joewolf@gmail.com>
2015-04-10 21:59:39 -0400

    Minor formatting fixes from previous commit

--- src/main/javascript/view/ParameterContentTypeView.js
+++ src/main/javascript/view/ParameterContentTypeView.js
@@ -4,7 +4,7 @@ SwaggerUi.Views.ParameterContentTypeView = Backbone.View.extend({
   initialize: function  () {},
 
   render: function(){
-  	this.model.parameterContentTypeId = 'pct' + Math.random();
+    this.model.parameterContentTypeId = 'pct' + Math.random();
     $(this.el).html(Handlebars.templates.parameter_content_type(this.model));
     return this;
   }
--- src/main/javascript/view/ResponseContentTypeView.js
+++ src/main/javascript/view/ResponseContentTypeView.js
@@ -4,7 +4,7 @@ SwaggerUi.Views.ResponseContentTypeView = Backbone.View.extend({
   initialize: function(){},
 
   render: function(){
-  	this.model.responseContentTypeId = 'rct' + Math.random();
+    this.model.responseContentTypeId = 'rct' + Math.random();
     $(this.el).html(Handlebars.templates.response_content_type(this.model));
     return this;
   }
