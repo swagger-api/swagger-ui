3c39a89bda23f3c1fe9a912996566e2eadae0555
Joe Wolf <joewolf@gmail.com>
2015-04-13 12:17:34 -0400

    508 fixes
    Addresses #1021 mostly by adding label tags, generating unique element
    ids as needed.
    
    In the process, moved the label text that gets set in the
    *ContentTypeView.js files to the respective handlebar templates; the
    text was static as far as I could tell.

--- src/main/javascript/view/ResponseContentTypeView.js
+++ src/main/javascript/view/ResponseContentTypeView.js
@@ -4,10 +4,8 @@ SwaggerUi.Views.ResponseContentTypeView = Backbone.View.extend({
   initialize: function(){},
 
   render: function(){
+    this.model.responseContentTypeId = 'rct' + Math.random();
     $(this.el).html(Handlebars.templates.response_content_type(this.model));
-
-    $('label[for=responseContentType]', $(this.el)).text('Response Content Type');
-
     return this;
   }
 });
