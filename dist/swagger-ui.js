a7d708593cd04b4c1d6b615a260ea243286be0db
Mohsen Azimi <msn.azi@gmail.com>
2015-04-14 13:30:55 -0700

    Fix issue #1160 "piKey is not set when Explore button is clicked "

--- dist/swagger-ui.js
+++ dist/swagger-ui.js
@@ -20692,7 +20692,7 @@ SwaggerUi.Views.MainView = Backbone.View.extend({
       } else {
 
         // Default validator
-        this.model.validatorUrl = 'http://online.swagger.io/validator';
+        this.model.validatorUrl = window.location.protocol + '//online.swagger.io/validator';
       }
     }
   },
