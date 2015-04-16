569830b4ac97cd3ebb12186212a8ef9a9e49d682
Björn Rochel <bjoern@bjro.de>
2015-04-16 20:37:19 +0200

    Use jQuery for trimming

--- src/main/javascript/view/OperationView.js
+++ src/main/javascript/view/OperationView.js
@@ -549,7 +549,7 @@ SwaggerUi.Views.OperationView = Backbone.View.extend({
       url = response.request.url;
     }
     var headers = response.headers;
-    content = content.trim();
+    content = jQuery.trim(content);
 
     // if server is nice, and sends content-type back, we can use it
     var contentType = null;
