f16c2650871595421c0963a3166dd2d5265b0f4a
Laurent Lepinay <laurent.lepinay@thomsonreuters.com>
2015-05-18 14:26:55 +0200

    disabled edit json button

--- dist/swagger-ui.js
+++ dist/swagger-ui.js
@@ -21567,7 +21567,7 @@ SwaggerUi.Views.ParameterView = Backbone.View.extend({
       this.model.jsonEditor = 
         /* global JSONEditor */
         new JSONEditor($('.editor_holder', $self)[0],
-                       {schema: this.model.schema, startval : this.model.default, ajax:true, disable_properties:true });
+                       {schema: this.model.schema, startval : this.model.default, ajax:true, disable_properties:true, disable_edit_json:true });
       // This is so that the signature can send back the sample to the json editor
       // TODO: SignatureView should expose an event "onSampleClicked" instead
       signatureModel.jsonEditor = this.model.jsonEditor;
