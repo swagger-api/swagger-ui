058f959669fcbb2d281d3b4140649fb878ac50fa
Tony Tam <fehguy@gmail.com>
2015-09-19 09:51:23 -0700

    extended timeouts

--- test/e2e/v2.js
+++ test/e2e/v2.js
@@ -17,10 +17,10 @@ var elements = [
 ];
 
 describe('swagger 2.0 spec tests', function () {
-  this.timeout(10 * 1000);
+  this.timeout(10 * 3000);
 
   before(function (done) {
-    this.timeout(25 * 1000);
+    this.timeout(25 * 3000);
     servers.start('/v2/petstore.json', done);
   });
 
