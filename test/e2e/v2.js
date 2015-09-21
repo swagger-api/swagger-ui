7e82b3d66a0f9af9e9b14c3078f7aaaa906cf31b
Laurent Lepinay <laurent.lepinay@thomsonreuters.com>
2015-09-21 15:36:59 +0200

    jshint

--- test/e2e/v2.js
+++ test/e2e/v2.js
@@ -110,7 +110,6 @@ describe('swagger 2.0 spec tests', function () {
     });
   });
 
-<<<<<<< HEAD
   ['root.id','root.username','root.firstName','root.lastName', 'root.email', 'root.password', 'root.phone', 'root.userStatus']
   .forEach(function (id) {
     it('should find a jsoneditor for user post with field: ' + id, function (done) {
