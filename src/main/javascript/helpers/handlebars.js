e5bf68fd502ca4bb083e2339bceb0f6e0473fb73
jruckert <jonathan.ruckert@oconics.com>
2015-10-27 16:22:40 +1030

    Better error checking if the param.type is null, changed the behavior so that operations are only loaded when clicked on for the first time.

--- src/main/javascript/helpers/handlebars.js
+++ src/main/javascript/helpers/handlebars.js
@@ -8,7 +8,7 @@ Handlebars.registerHelper('sanitize', function(html) {
 
 Handlebars.registerHelper('renderTextParam', function(param) {
     var result, type = 'text', idAtt = '';
-    var isArray = param.type.toLowerCase() === 'array' || param.allowMultiple;
+    var isArray = typeof param.type === 'undefined' ? false : param.type.toLowerCase() === 'array' || param.allowMultiple;
     var defaultValue = isArray && Array.isArray(param.default) ? param.default.join('\n') : param.default;
 
     var dataVendorExtensions = Object.keys(param).filter(function(property) {
