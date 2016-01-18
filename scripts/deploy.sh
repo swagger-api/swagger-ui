88cc0b9ba1db3f361f4fbdbf9e6a1ce105ed63b1
Seth Baxter <sethbaxter@prominentedge.com>
2016-01-18 08:55:20 -0500

    Change deploy target

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,6 +5,6 @@ base=$(pwd -P)
 popd > /dev/null
 
 src=$base/../dist/
-dest=s3://piazzageo.io
+dest=s3://swagger.piazzageo.io
 
 aws s3 sync --delete --exclude '*.swp' --exclude '.git*' --exclude '.DS_STORE' $src $dest
