86a7a85d98e97b3997e184a5334c8b5f1d2774ba
Anna Bodnia <anna.bodnia@gmail.com>
2016-02-23 20:56:09 +0200

    Put authorizations into popup

---
+++ src/main/javascript/view/PopupView.js
@@ -0,0 +1,44 @@
+'use strict';
+
+SwaggerUi.Views.PopupView = Backbone.View.extend({
+    events: {
+        'click .api-popup-cancel': 'cancelClick'
+    },
+
+    template: Handlebars.templates.popup,
+    className: 'api-popup-dialog',
+
+    initialize: function(){},
+
+    render: function () {
+        var $win, dw, dh, st, dlgWd, dlgHt, top, left;
+        $win = $(window);
+        dw = $win.width();
+        dh = $win.height();
+        st = $win.scrollTop();
+        this.$el.html(this.template(this.model));
+        $(document.body).append(this.el);
+        dlgWd = this.$el.outerWidth();
+        dlgHt = this.$el.outerHeight();
+        top = (dh -dlgHt)/2 + st;
+
+        left = (dw - dlgWd)/2;
+
+        this.$el.css({
+            top: (top < 0? 0 : top) + 'px',
+            left: (left < 0? 0 : left) + 'px'
+        });
+        this.showPopup();
+
+        return this;
+    },
+
+    showPopup: function () {
+        this.$el.show();
+    },
+
+    cancelClick: function () {
+        this.remove();
+    }
+
+});
