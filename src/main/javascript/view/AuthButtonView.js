a268d007a2407ef098adf5d4299124baf4b25bfb
Anna Bodnia <anna.bodnia@gmail.com>
2016-03-04 20:55:57 +0200

    [auth] Added api key and basic authorization

--- src/main/javascript/view/AuthButtonView.js
+++ src/main/javascript/view/AuthButtonView.js
@@ -23,6 +23,7 @@ SwaggerUi.Views.AuthButtonView = Backbone.View.extend({
 
     authorizeBtnClick: function (e) {
         var authsModel;
+
         e.preventDefault();
 
         authsModel = {
@@ -35,30 +36,14 @@ SwaggerUi.Views.AuthButtonView = Backbone.View.extend({
     },
 
     renderAuths: function (auths) {
-        var name, authEl, authModel;
-        var el = $('<div>');
-        var authz = window.swaggerUi.api.clientAuthorizations.authz;
+        var $el = $('<div>');
 
-        for (name in auths) {
-            authModel = _.extend({}, auths[name]);
+        auths.forEach(function (auth) {
+            var authEl = new SwaggerUi.Views.AuthView({data: auth, router: this.router}).render().el;
+            $el.append(authEl);
+        }, this);
 
-            if (authz[name]) {
-                _.extend(authModel, {
-                    isLogout: true,
-                    value: authz[name].value
-                });
-            }
-
-            if (authModel.type === 'apiKey') {
-                authEl = new SwaggerUi.Views.ApiKeyButton({model: authModel, router: this.router}).render().el;
-                el.append(authEl);
-            } else if (authModel.type === 'basic' && el.find('.basic_auth_container').length === 0) {
-                authEl = new SwaggerUi.Views.BasicAuthButton({model: authModel, router: this.router}).render().el;
-                el.append(authEl);
-            }
-        }
-
-        return el;
+        return $el;
     }
 
 });
--- src/main/javascript/view/MainView.js
+++ src/main/javascript/view/MainView.js
@@ -84,13 +84,19 @@ SwaggerUi.Views.MainView = Backbone.View.extend({
 
   render: function () {
     // Render the outer container for resources
-    var authsModel;
+    var authsModel, parsedDefinitions;
 
     $(this.el).html(Handlebars.templates.main(this.model));
     this.model.securityDefinitions = this.model.securityDefinitions || {};
 
     if (!_.isEmpty(this.model.securityDefinitions)) {
-      authsModel = { auths: this.model.securityDefinitions };
+      parsedDefinitions = _.map(this.model.securityDefinitions, function (auth, name) {
+        var result = {};
+        result[name] = auth;
+        return result;
+      });
+
+      authsModel = { auths: parsedDefinitions };
 
       authsModel.isLogout = !_.isEmpty(window.swaggerUi.api.clientAuthorizations.authz);
       this.authView = new SwaggerUi.Views.AuthButtonView({model: authsModel, router: this.router});
