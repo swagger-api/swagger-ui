d82bad414b7954a1745b40f315b1cbbd468ff02e
Xiaomin Yu <yuxiaomin@gmail.com>
2016-06-09 11:20:34 -0500

    Replace the formatXml with a new implementation.
    The new implementation is adapted from
    https://github.com/vkiryukhin/vkBeautify/blob/master/vkbeautify.js

--- src/main/javascript/view/OperationView.js
+++ src/main/javascript/view/OperationView.js
@@ -573,80 +573,102 @@ SwaggerUi.Views.OperationView = Backbone.View.extend({
     parent.showStatus(data);
   },
 
-  // Adapted from http://stackoverflow.com/a/2893259/454004
-  // Note: directly ported from CoffeeScript
-  // TODO: Cleanup CoffeeScript artifacts
+  // Adapted from https://github.com/vkiryukhin/vkBeautify/blob/master/vkbeautify.js
   formatXml: function(xml) {
-    var contexp, fn, formatted, indent, l, lastType, len, lines, ln, pad, reg, transitions, wsexp;
-    reg = /(>)(<)(\/*)/g;
-    wsexp = /[ ]*(.*)[ ]+\n/g;
-    contexp = /(<.+>)(.+\n)/g;
-    xml = xml.replace(/\r\n/g, '\n').replace(reg, '$1\n$2$3').replace(wsexp, '$1\n').replace(contexp, '$1\n$2');
-    pad = 0;
-    formatted = '';
-    lines = xml.split('\n');
-    indent = 0;
-    lastType = 'other';
-    transitions = {
-      'single->single': 0,
-      'single->closing': -1,
-      'single->opening': 0,
-      'single->other': 0,
-      'closing->single': 0,
-      'closing->closing': -1,
-      'closing->opening': 0,
-      'closing->other': 0,
-      'opening->single': 1,
-      'opening->closing': 0,
-      'opening->opening': 1,
-      'opening->other': 1,
-      'other->single': 0,
-      'other->closing': -1,
-      'other->opening': 0,
-      'other->other': 0
-    };
-    fn = function(ln) {
-      var fromTo, j, key, padding, type, types, value;
-      types = {
-        single: Boolean(ln.match(/<.+\/>/)),
-        closing: Boolean(ln.match(/<\/.+>/)),
-        opening: Boolean(ln.match(/<[^!?].*>/))
-      };
-      type = ((function() {
-        var results;
-        results = [];
-        for (key in types) {
-          value = types[key];
-          if (value) {
-            results.push(key);
-          }
+    var createShiftArr = function(step) {
+      var space = '    ';
+      
+      if ( isNaN(parseInt(step)) ) {  // argument is string
+        space = step;
+      } else { // argument is integer
+        switch(step) {
+          case 1: space = ' '; break;
+          case 2: space = '  '; break;
+          case 3: space = '   '; break;
+          case 4: space = '    '; break;
+          case 5: space = '     '; break;
+          case 6: space = '      '; break;
+          case 7: space = '       '; break;
+          case 8: space = '        '; break;
+          case 9: space = '         '; break;
+          case 10: space = '          '; break;
+          case 11: space = '           '; break;
+          case 12: space = '            '; break;
         }
-        return results;
-      })())[0];
-      type = type === void 0 ? 'other' : type;
-      fromTo = lastType + '->' + type;
-      lastType = type;
-      padding = '';
-      indent += transitions[fromTo];
-      padding = ((function() {
-        var m, ref1, results;
-        results = [];
-        for (j = m = 0, ref1 = indent; 0 <= ref1 ? m < ref1 : m > ref1; j = 0 <= ref1 ? ++m : --m) {
-          results.push('  ');
-        }
-        return results;
-      })()).join('');
-      if (fromTo === 'opening->closing') {
-        formatted = formatted.substr(0, formatted.length - 1) + ln + '\n';
-      } else {
-        formatted += padding + ln + '\n';
       }
-    };
-    for (l = 0, len = lines.length; l < len; l++) {
-      ln = lines[l];
-      fn(ln);
+
+      var shift = ['\n']; // array of shifts
+      for(ix=0;ix<100;ix++){
+        shift.push(shift[ix]+space); 
+      }
+      return shift;
     }
-    return formatted;
+
+
+    var ar = text.replace(/>\s{0,}</g,"><")
+           .replace(/</g,"~::~<")
+           .replace(/\s*xmlns\:/g,"~::~xmlns:")
+           .replace(/\s*xmlns\=/g,"~::~xmlns=")
+           .split('~::~'),
+      len = ar.length,
+      inComment = false,
+      deep = 0,
+      str = '',
+      ix = 0,
+      shift = createShiftArr(2);
+
+    for(ix=0;ix<len;ix++) {
+      // start comment or <![CDATA[...]]> or <!DOCTYPE //
+      if(ar[ix].search(/<!/) > -1) { 
+        str += shift[deep]+ar[ix];
+        inComment = true; 
+        // end comment  or <![CDATA[...]]> //
+        if(ar[ix].search(/-->/) > -1 || ar[ix].search(/\]>/) > -1 || ar[ix].search(/!DOCTYPE/) > -1 ) { 
+          inComment = false; 
+        }
+      } else 
+      // end comment  or <![CDATA[...]]> //
+      if(ar[ix].search(/-->/) > -1 || ar[ix].search(/\]>/) > -1) { 
+        str += ar[ix];
+        inComment = false; 
+      } else 
+      // <elm></elm> //
+      if( /^<\w/.exec(ar[ix-1]) && /^<\/\w/.exec(ar[ix]) &&
+        /^<[\w:\-\.\,]+/.exec(ar[ix-1]) == /^<\/[\w:\-\.\,]+/.exec(ar[ix])[0].replace('/','')) { 
+        str += ar[ix];
+        if(!inComment) deep--;
+      } else
+       // <elm> //
+      if(ar[ix].search(/<\w/) > -1 && ar[ix].search(/<\//) == -1 && ar[ix].search(/\/>/) == -1 ) {
+        str = !inComment ? str += shift[deep++]+ar[ix] : str += ar[ix];
+      } else 
+       // <elm>...</elm> //
+      if(ar[ix].search(/<\w/) > -1 && ar[ix].search(/<\//) > -1) {
+        str = !inComment ? str += shift[deep]+ar[ix] : str += ar[ix];
+      } else 
+      // </elm> //
+      if(ar[ix].search(/<\//) > -1) { 
+        str = !inComment ? str += shift[--deep]+ar[ix] : str += ar[ix];
+      } else 
+      // <elm/> //
+      if(ar[ix].search(/\/>/) > -1 ) { 
+        str = !inComment ? str += shift[deep]+ar[ix] : str += ar[ix];
+      } else 
+      // <? xml ... ?> //
+      if(ar[ix].search(/<\?/) > -1) { 
+        str += shift[deep]+ar[ix];
+      } else 
+      // xmlns //
+      if( ar[ix].search(/xmlns\:/) > -1  || ar[ix].search(/xmlns\=/) > -1) { 
+        str += shift[deep]+ar[ix];
+      } 
+      
+      else {
+        str += ar[ix];
+      }
+    }
+      
+    return  (str[0] == '\n') ? str.slice(1) : str;
   },
 
   // puts the response data in UI
