0a26dad9f05e4fe60c1fce3e12f4f15deb75d360
Aleksandr <mogu-i-budu@hotmail.com>
2016-08-12 17:11:13 +0300

    ICLD-1427 (fix scroll top)

--- src/main/javascript/app.js
+++ src/main/javascript/app.js
@@ -8,4 +8,19 @@
 
         Backbone.history.start(new window.SwaggerUiRouter({app: this}));
     };
+
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},800);
+        return false;
+    });
 })();
