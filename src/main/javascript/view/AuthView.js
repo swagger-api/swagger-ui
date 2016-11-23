3d43b93957ddc07f4e50d36f52b9e099fc6700e3
Frederic <fred@sketchfab.com>
2016-11-23 10:44:50 +0100

    [#WEB-6602] Fixes token auth

--- src/main/javascript/view/AuthView.js
+++ src/main/javascript/view/AuthView.js
@@ -64,7 +64,7 @@ SwaggerUi.Views.AuthView = Backbone.View.extend({
             if (type === 'apiKey') {
                 keyAuth = new SwaggerClient.ApiKeyAuthorization(
                     auth.get('name'),
-                    'Token ' + auth.get('value'),
+                    auth.get('value'),
                     auth.get('in')
                 );
 
