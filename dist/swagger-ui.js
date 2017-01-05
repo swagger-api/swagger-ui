ae4961ded9dfe0965a596ad6b5384b62d0956d2e
Tony Tam <fehguy@gmail.com>
2017-01-05 00:55:17 -0800

    updated versions

--- dist/swagger-ui.js
+++ dist/swagger-ui.js
@@ -1,6 +1,6 @@
 /**
  * swagger-ui - Swagger UI is a dependency-free collection of HTML, JavaScript, and CSS assets that dynamically generate beautiful documentation from a Swagger-compliant API
- * @version v2.2.9
+ * @version v2.2.10
  * @link http://swagger.io
  * @license Apache-2.0
  */
@@ -3175,7 +3175,7 @@ state.pendingcb++;ret=writeOrBuffer(this,state,chunk,encoding,cb)}return ret};Wr
 
 /**
  * swagger-client - swagger-client is a javascript client for use with swaggering APIs.
- * @version v2.1.28
+ * @version v2.1.29
  * @link http://swagger.io
  * @license Apache-2.0
  */
