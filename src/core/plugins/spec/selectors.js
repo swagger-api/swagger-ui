8b78d9bd134b6103c18d82bc7eeba6b395f223d9
Kyle Shockey <shockey@users.noreply.github.com>
2017-04-18 18:13:52 -0700

    Default to no scheme if we can't find an assigned scheme

--- src/core/plugins/spec/selectors.js
+++ src/core/plugins/spec/selectors.js
@@ -294,8 +294,8 @@ export const operationScheme = ( state, path, method ) => {
   let url = state.get("url")
   let matchResult = url.match(/^([a-z][a-z0-9+\-.]*):/)
   let urlScheme = Array.isArray(matchResult) ? matchResult[1] : null
-  
-  return state.getIn(["scheme", path, method]) || state.getIn(["scheme", "_defaultScheme"]) || urlScheme || "http"
+
+  return state.getIn(["scheme", path, method]) || state.getIn(["scheme", "_defaultScheme"]) || urlScheme || ""
 }
 
 export const canExecuteScheme = ( state, path, method ) => {
