06c82bb325d1334dda904eab71cf68235effa821
Kyle Shockey <shockey@users.noreply.github.com>
2017-04-20 17:11:54 -0700

    Add message that reveals commit that SwaggerUI was derived from

--- src/core/index.js
+++ src/core/index.js
@@ -6,6 +6,8 @@ import * as AllPlugins from "core/plugins/all"
 import { filterConfigs } from "plugins/configs"
 import { parseSeach } from "core/utils"
 
+console.log(`This Swagger-UI was built from ${process.env.COMMITHASH}.`)
+
 module.exports = function SwaggerUI(opts) {
 
   const defaults = {
