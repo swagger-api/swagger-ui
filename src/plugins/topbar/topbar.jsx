ca808e9d181885d4e322a1fdb31e7f92ff5e71b1
Minasokoni <minasokoni@gmail.com>
2017-04-28 08:36:32 -0700

    updated searchbar sizing

--- src/plugins/topbar/topbar.jsx
+++ src/plugins/topbar/topbar.jsx
@@ -43,10 +43,10 @@ export default class Topbar extends React.Component {
                 <img height="30" width="30" src={ Logo } alt="Swagger UX"/>
                 <span>swagger</span>
               </Link>
-              <div className="download-url-wrapper">
+              <form className="download-url-wrapper" onSubmit={this.downloadUrl}>
                 <input className="download-url-input" type="text" onChange={ this.onUrlChange } value={this.state.url} disabled={isLoading} style={inputStyle} />
                 <Button className="download-url-button" onClick={ this.downloadUrl }>Explore</Button>
-              </div>
+              </form>
             </div>
           </div>
         </div>
