31abbb7641256297af67e550a6e7cff423f4033c
Kyle Shockey <shockey@users.noreply.github.com>
2017-05-23 15:29:58 -0700

    Finish rewiring Markdown provider

--- src/core/components/providers/markdown.jsx
+++ src/core/components/providers/markdown.jsx
@@ -2,7 +2,7 @@ import Remarkable from "react-remarkable"
 import React from "react"
 
 
-export default ({ source }) => {
+export default function Markdown({ source }) {
   return <Remarkable
     options={{html: true, typographer: true, linkify: true, linkTarget: "_blank"}}
     source={source}
