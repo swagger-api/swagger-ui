5511aa9a6adafb1c39e03261520e11ec3112f6a4
Kyle Shockey <shockey@users.noreply.github.com>
2017-05-31 18:49:46 -0700

    Fix react-markdown empty prop error

--- src/core/plugins/oas3/wrap-components/markdown.js
+++ src/core/plugins/oas3/wrap-components/markdown.js
@@ -2,4 +2,4 @@ import React from "react"
 import ReactMarkdown from "react-markdown"
 import { OAS3ComponentWrapFactory } from "../helpers"
 
-export default OAS3ComponentWrapFactory(({ source }) => <ReactMarkdown source={source} />)
+export default OAS3ComponentWrapFactory(({ source }) => { return source ? <ReactMarkdown source={source} /> : null})
