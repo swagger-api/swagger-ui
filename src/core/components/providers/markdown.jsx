af2b1e20f1406277e0d7474febcaa37680407a80
Kyle Shockey <shockey@users.noreply.github.com>
2017-06-01 22:14:44 -0700

    Sanitize Markdown inputs

--- src/core/components/providers/markdown.jsx
+++ src/core/components/providers/markdown.jsx
@@ -1,10 +1,11 @@
-import Remarkable from "react-remarkable"
 import React from "react"
-
+import Remarkable from "react-remarkable"
+import sanitize from "sanitize-html"
 
 export default function Markdown({ source }) {
+  const sanitized = sanitize(source)
   return <Remarkable
     options={{html: true, typographer: true, linkify: true, linkTarget: "_blank"}}
-    source={source}
+    source={sanitized}
     ></Remarkable>
 }
