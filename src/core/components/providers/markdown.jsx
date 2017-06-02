bec97cfaf51e572a7b603340363e72fad941bc0c
Kyle Shockey <shockey@users.noreply.github.com>
2017-06-01 22:24:31 -0700

    Fix style error

--- src/core/components/providers/markdown.jsx
+++ src/core/components/providers/markdown.jsx
@@ -1,11 +1,17 @@
-import React from "react"
+import React, { PropTypes } from "react"
 import Remarkable from "react-remarkable"
 import sanitize from "sanitize-html"
 
-export default function Markdown({ source }) {
+function Markdown({ source }) {
   const sanitized = sanitize(source)
   return <Remarkable
     options={{html: true, typographer: true, linkify: true, linkTarget: "_blank"}}
     source={sanitized}
     ></Remarkable>
 }
+
+Markdown.propTypes = {
+  source: PropTypes.string.isRequired
+}
+
+export default Markdown
