234e6d5d57291db1e7fd03e18bb3acc08e02fc86
Dylan <dwood@walmart.com>
2017-06-09 07:54:42 -0700

    add `absolutePath` to swagger-ui-dist

--- swagger-ui-dist-package/index.js
+++ swagger-ui-dist-package/index.js
@@ -1,2 +1,6 @@
+const path = require('path')
+
 module.exports.SwaggerUIBundle = require('./swagger-ui-bundle.js')
 module.exports.SwaggerUIStandalonePreset = require('./swagger-ui-standalone-preset.js')
+
+module.exports.absolutePath = path.resolve(__dirname)
