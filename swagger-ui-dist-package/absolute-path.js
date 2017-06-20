ab960e8c4571ea67dc1ae0cfd1006e821d7cb162
Johann Wagner <elkonqi@gmail.com>
2017-06-20 18:45:30 +0200

    don't use ES6 syntax in a dist package
    using ES6 syntax will cause trouble unless you transpile before use (which is something you should not be required to do in a dist package)

--- swagger-ui-dist-package/absolute-path.js
+++ swagger-ui-dist-package/absolute-path.js
@@ -3,7 +3,7 @@
  * @return {string} When run in NodeJS env, returns the absolute path to the current directory
  *                  When run outside of NodeJS, will return an error message
  */
-const getAbsoluteFSPath = () => {
+const getAbsoluteFSPath = function () {
   // detect whether we are running in a browser or nodejs
   if (typeof module !== "undefined" && module.exports) {
     return require("path").resolve(__dirname)
