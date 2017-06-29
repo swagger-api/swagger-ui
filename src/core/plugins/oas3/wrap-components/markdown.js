2b37e7c2e44118e732a66808ffa5c4ef2eb40d0b
Kyle Shockey <shockey@users.noreply.github.com>
2017-06-28 19:28:30 -0700

    Render markdown more normally

--- src/core/plugins/oas3/wrap-components/markdown.js
+++ src/core/plugins/oas3/wrap-components/markdown.js
@@ -2,4 +2,9 @@ import React from "react"
 import ReactMarkdown from "react-markdown"
 import { OAS3ComponentWrapFactory } from "../helpers"
 
-export default OAS3ComponentWrapFactory(({ source }) => { return source ? <ReactMarkdown source={source} /> : null})
+export default OAS3ComponentWrapFactory(({ source }) => { return source ? (
+  <ReactMarkdown
+    source={source}
+    className={"renderedMarkdown"}
+    />
+) : null})
