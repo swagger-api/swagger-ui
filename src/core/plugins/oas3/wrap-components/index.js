1970b2c41033a5c9aea4ad151987911b417ac68f
Kyle Shockey <shockey@users.noreply.github.com>
2017-07-03 19:02:03 -0700

    Disable Online Validation badge for OAS3

--- src/core/plugins/oas3/wrap-components/index.js
+++ src/core/plugins/oas3/wrap-components/index.js
@@ -1,11 +1,13 @@
 import Markdown from "./markdown"
 import parameters from "./parameters"
 import VersionStamp from "./version-stamp"
+import OnlineValidatorBadge from "./online-validator-badge"
 import Model from "./model"
 
 export default {
   Markdown,
   parameters,
   VersionStamp,
-  model: Model
+  model: Model,
+  onlineValidatorBadge: OnlineValidatorBadge
 }
---
+++ src/core/plugins/oas3/wrap-components/online-validator-badge.js
@@ -0,0 +1,6 @@
+import React from "react"
+import { OAS3ComponentWrapFactory } from "../helpers"
+
+// We're disabling the Online Validator Badge until the online validator
+// can handle OAS3 specs.
+export default OAS3ComponentWrapFactory(() => null)
