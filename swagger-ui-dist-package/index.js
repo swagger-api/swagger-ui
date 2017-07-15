7462d069ccf66ad06943d23804521f2785e29ed6
shockey <shockey@users.noreply.github.com>
2017-07-14 19:11:53 -0700

    Maintain backwards compatibility with absolutePath

--- swagger-ui-dist-package/index.js
+++ swagger-ui-dist-package/index.js
@@ -10,4 +10,8 @@ try {
   // for more information.
 }
 
+// `absolutePath` and `getAbsoluteFSPath` are both here because at one point,
+// we documented having one and actually implemented the other.
+// They were both retained so we don't break anyone's code.
+module.exports.absolutePath = require("./absolute-path.js")
 module.exports.getAbsoluteFSPath = require("./absolute-path.js")
