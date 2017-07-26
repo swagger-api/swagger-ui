8bbe706918beccad0d74a3e6a41ebfdc84a7e220
Kyle Shockey <shockey@users.noreply.github.com>
2017-07-25 21:55:32 -0700

    Disable Try-It-Out button in OAS3
    CC: #3436

--- src/core/plugins/oas3/wrap-components/index.js
+++ src/core/plugins/oas3/wrap-components/index.js
@@ -3,11 +3,13 @@ import parameters from "./parameters"
 import VersionStamp from "./version-stamp"
 import OnlineValidatorBadge from "./online-validator-badge"
 import Model from "./model"
+import TryItOutButton from "./try-it-out-button"
 
 export default {
   Markdown,
   parameters,
   VersionStamp,
   model: Model,
-  onlineValidatorBadge: OnlineValidatorBadge
+  onlineValidatorBadge: OnlineValidatorBadge,
+  TryItOutButton
 }
---
+++ src/core/plugins/oas3/wrap-components/try-it-out-button.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+import { OAS3ComponentWrapFactory } from "../helpers"
+
+export default OAS3ComponentWrapFactory(() => {
+  return null
+})
