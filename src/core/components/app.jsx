0d903055506a598839c40cfb22bee259aaf8d3fc
Owen Conti <owen@ohseemedia.com>
2017-08-20 08:06:59 -0600

    Remove unnecessary div in app.jsx

--- src/core/components/app.jsx
+++ src/core/components/app.jsx
@@ -14,9 +14,7 @@ export default class App extends React.Component {
     const Layout = this.getLayout()
 
     return (
-      <div>
-        <Layout/>
-      </div>
+      <Layout/>
     )
   }
 }
