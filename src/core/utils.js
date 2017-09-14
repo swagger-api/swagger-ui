840912cb567011d5967fe977197063aa4e7fc40b
Owen Conti <owen@ohseemedia.com>
2017-09-13 21:38:07 -0600

    Handle null value in createDeepLinkPath

--- src/core/utils.js
+++ src/core/utils.js
@@ -652,5 +652,5 @@ export const shallowEqualKeys = (a,b, keys) => {
   })
 }
 
-export const createDeepLinkPath = (str) => str.replace(/\s/g, "_")
+export const createDeepLinkPath = (str) => str ? str.replace(/\s/g, "_") : ""
 export const escapeDeepLinkPath = (str) => cssEscape( createDeepLinkPath(str) )
