87fa8f10f0db78741ebc2be140933d1dfd94e9ba
Vadim Barylo <vadimb@intapp.net>
2017-09-18 17:29:47 +0300

    load tenant configuration

--- src/main/javascript/app.js
+++ src/main/javascript/app.js
@@ -1,21 +1,27 @@
 (function() {
     'use strict';
 
+    //TMS IDD url
+    var _tmsUrl = 'proxy/tms.platformdev.intapp.com/tms/idd/v1/platform';
+    var _keycloakTenantIdDefault = 'intapp';
+    var _keycloakClientIdDefault = 'ADMIN-UI';
+
     window.SwaggerApp = function() {
-        hljs.configure({
-            highlightSizeThreshold: 5000
+        hljs.configure({ highlightSizeThreshold: 5000 });
+
+        //get tenant configuration
+        $.get(_tmsUrl + '/alias/' + getTenantFromUrl()).then(function(configuration) {
+            var keycloak = window.KC = Keycloak({ url: configuration.auth_url, realm: configuration.name, clientId: 'ADMIN-UI' });
+
+            keycloak.init({ onLoad: 'login-required' }).success(function(authenticated) {
+                if(authenticated) {
+                    //start history
+                    Backbone.history.start(new window.SwaggerUiRouter({app: this}));
+                } else {
+                    alert('not authenticated');
+                }
+            });
         });
-
-        //set main open URL
-        /*global Intapp */
-        if((typeof window.Intapp !== 'undefined' && window.Intapp.Config.Url)) {
-            $('#main-app').attr('href', Intapp.Config.Url);
-        } else {
-            $('#main-app').attr('href', location.href.replace('/api/swagger', ''));
-        }
-
-        //start history
-        Backbone.history.start(new window.SwaggerUiRouter({app: this}));
     };
 
     //Check to see if the window is top if not then display button
@@ -32,4 +38,14 @@
         $('html, body').animate({scrollTop : 0},800);
         return false;
     });
+
+    //get tenant configuration
+    function getTenantConfiguration() {
+        $.get(_tmsUrl + '/alias/' + getTenantFromUrl());
+    }
+
+    function getTenantFromUrl() {
+        var urlParts = window.location.href.replace('https://', '').replace('http://', '').replace('localhost:4200', _keycloakTenantIdDefault).split('.');
+        return (urlParts.length > 1) ? urlParts[0] : _keycloakTenantIdDefault;
+    }
 })();
