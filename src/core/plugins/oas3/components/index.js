e61800c35e0e2e759b08f8f5fe3d562265489c64
Xiaoyu Guo <biggates2010@gmail.com>
2017-09-28 16:11:09 +0800

    Register commonent "Servers" in OAS3
    So this branch will work with 3.2.2

--- src/core/plugins/oas3/components/index.js
+++ src/core/plugins/oas3/components/index.js
@@ -2,10 +2,12 @@ import Callbacks from "./callbacks"
 import RequestBody from "./request-body"
 import OperationLink from "./operation-link.jsx"
 import ExternalValue from "./external-value.jsx"
+import Servers from "./Servers.jsx"
 
 export default {
   Callbacks,
   RequestBody,
   operationLink: OperationLink,
-  ExternalValue
+  ExternalValue,
+  Servers
 }
