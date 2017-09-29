0a49263e506d37c05cff2bea90eab600eb9fd5a2
Xiaoyu Guo <biggates2010@gmail.com>
2017-09-29 15:58:46 +0800

    Fix typo
    which causes 'There are multiple modules with names that only differ in
    casing.'

--- src/core/plugins/oas3/components/index.js
+++ src/core/plugins/oas3/components/index.js
@@ -2,7 +2,7 @@ import Callbacks from "./callbacks"
 import RequestBody from "./request-body"
 import OperationLink from "./operation-link.jsx"
 import ExternalValue from "./external-value.jsx"
-import Servers from "./Servers.jsx"
+import Servers from "./servers.jsx"
 
 export default {
   Callbacks,
