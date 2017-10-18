f31a3cb0fd7a23d4254de8ec1c587daca59619d7
Liam Shaw <liam.shaw@smartbear.com>
2017-10-18 12:40:06 +0100

    deep linking for models, operations and tags

--- src/core/components/model-wrapper.jsx
+++ src/core/components/model-wrapper.jsx
@@ -1,21 +1,31 @@
 import React, { Component, } from "react"
 import PropTypes from "prop-types"
+//import layoutActions from "actions/layout"
 
-export default class ModelComponent extends Component {
+
+export default class ModelWrapper extends Component {
+
+
   static propTypes = {
     schema: PropTypes.object.isRequired,
     name: PropTypes.string,
     getComponent: PropTypes.func.isRequired,
     specSelectors: PropTypes.object.isRequired,
-    expandDepth: PropTypes.number
+    expandDepth: PropTypes.number,
+    layoutActions: PropTypes.object,
+    layoutSelectors: PropTypes.object.isRequired
+  }
+
+  onToggle = (name,isShown) => {
+      this.props.layoutActions.show(["models", name],isShown)
   }
 
   render(){
     let { getComponent } = this.props
     const Model = getComponent("Model")
-
+    const expanded = this.props.layoutSelectors.isShown(["models",this.props.name])
     return <div className="model-box">
-      <Model { ...this.props } depth={ 1 } expandDepth={ this.props.expandDepth || 0 }/>
+      <Model { ...this.props } expanded={expanded} depth={ 1 } onToggle={ this.onToggle} expandDepth={ this.props.expandDepth || 0 }/>
     </div>
   }
 }
