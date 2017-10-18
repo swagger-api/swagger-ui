6da1828cd35206c6c7c867f228ca328b7fcb67ab
Josh Ponelat <jponelat@gmail.com>
2017-10-18 13:06:33 +0200

    Create a configs plugin
    This starts to migrate configs to state

--- src/plugins/configs/index.js
+++ src/plugins/configs/index.js
@@ -1,56 +1,66 @@
 import YAML from "js-yaml"
 import yamlConfig from "../../../swagger-config.yaml"
+import * as actions from "./actions"
+import * as selectors from "./selectors"
+import reducers from "./reducers"
 
 const parseYamlConfig = (yaml, system) => {
-    try {
-        return YAML.safeLoad(yaml)
-    } catch(e) {
-        if (system) {
-            system.errActions.newThrownErr( new Error(e) )
-        }
-        return {}
+  try {
+    return YAML.safeLoad(yaml)
+  } catch(e) {
+    if (system) {
+      system.errActions.newThrownErr( new Error(e) )
     }
+    return {}
+  }
 }
 
 
-export default function configPlugin (toolbox) {
-    let { fn } = toolbox
+const specActions = {
+  downloadConfig: (url) => ({fn}) => {
+    let {fetch} = fn
+    return fetch(url)
+  },
 
-    const actions = {
-        downloadConfig: (url) => () => {
-            let {fetch} = fn
-            return fetch(url)
-        },
-
-        getConfigByUrl: (configUrl, cb)=> ({ specActions }) => {
-            if (configUrl) {
-                return specActions.downloadConfig(configUrl).then(next, next)
-            }
-
-            function next(res) {
-                if (res instanceof Error || res.status >= 400) {
-                    specActions.updateLoadingStatus("failedConfig")
-                    specActions.updateLoadingStatus("failedConfig")
-                    specActions.updateUrl("")
-                    console.error(res.statusText + " " + configUrl)
-                    cb(null)
-                } else {
-                    cb(parseYamlConfig(res.text))
-                }
-            }
-        }
-
+  getConfigByUrl: (configUrl, cb)=> ({ specActions }) => {
+    if (configUrl) {
+      return specActions.downloadConfig(configUrl).then(next, next)
     }
 
-    const selectors = {
-        getLocalConfig: () => {
-            return parseYamlConfig(yamlConfig)
-        }
+    function next(res) {
+      if (res instanceof Error || res.status >= 400) {
+        specActions.updateLoadingStatus("failedConfig")
+        specActions.updateLoadingStatus("failedConfig")
+        specActions.updateUrl("")
+        console.error(res.statusText + " " + configUrl)
+        cb(null)
+      } else {
+        cb(parseYamlConfig(res.text))
+      }
     }
+  }
+}
 
-    return {
-        statePlugins: {
-            spec: { actions, selectors }
-        }
+const specSelectors = {
+  getLocalConfig: () => {
+    return parseYamlConfig(yamlConfig)
+  }
+}
+
+
+export default function configsPlugin() {
+
+  return {
+    statePlugins: {
+      spec: {
+        actions: specActions,
+        selectors: specSelectors,
+      },
+      configs: {
+        reducers,
+        actions,
+        selectors,
+      }
     }
+  }
 }
