50b318c5d9247e918f2c6996a5ee31e76c2fff91
Josh Ponelat <jponelat@gmail.com>
2017-10-18 13:06:33 +0200

    Create a configs plugin
    This starts to migrate configs to state

--- src/core/presets/base.js
+++ src/core/presets/base.js
@@ -10,6 +10,7 @@ import auth from "core/plugins/auth"
 import util from "core/plugins/util"
 import SplitPaneModePlugin from "core/plugins/split-pane-mode"
 import downloadUrlPlugin from "core/plugins/download-url"
+import configsPlugin from "plugins/configs"
 import deepLinkingPlugin from "core/plugins/deep-linking"
 
 import App from "core/components/app"
@@ -122,6 +123,7 @@ export default function() {
   }
 
   return [
+    configsPlugin,
     util,
     logs,
     view,
