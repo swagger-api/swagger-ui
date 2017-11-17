d28bf861297ed1a45e44f8628d0dcd4d2942ae66
Kyle Shockey <shockey@users.noreply.github.com>
2017-11-16 21:38:41 -0800

    Skip toJS() call in default mapStateToProps function

--- src/core/plugins/view/root-injects.js
+++ src/core/plugins/view/root-injects.js
@@ -22,7 +22,7 @@ const RootWrapper = (reduxStore, ComponentToWrap) => class extends Component {
 const makeContainer = (getSystem, component, reduxStore) => {
   const mapStateToProps = function(state, ownProps) {
     const propsForContainerComponent = Object.assign({}, ownProps, getSystem())
-    const ori = component.prototype.mapStateToProps || (state => state.toJS())
+    const ori = component.prototype.mapStateToProps || (state => { return {state} })
     return ori(state, propsForContainerComponent)
   }
 
