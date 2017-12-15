a2f8d289a0b22b6ff91d5b2b70d38663ddf8b5d7
Kyle Shockey <shockey@users.noreply.github.com>
2017-12-15 14:17:12 -0800

    Fix error transformer imports

--- src/core/plugins/err/error-transformers/hook.js
+++ src/core/plugins/err/error-transformers/hook.js
@@ -1,7 +1,7 @@
 import reduce from "lodash/reduce"
-import { transform as NotOfType } from "./transformers/not-of-type"
-import { transform as ParameterOneOf } from "./transformers/parameter-oneof"
-import { transform as StripInstance } from "./transformers/strip-instance"
+import * as NotOfType from "./transformers/not-of-type"
+import * as ParameterOneOf from "./transformers/parameter-oneof"
+import * as StripInstance from "./transformers/strip-instance"
 
 const errorTransformers = [
   NotOfType,
