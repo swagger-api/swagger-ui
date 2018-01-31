2c68bb93c27e783ea455bb1cbf2818ee06c51fc2
srpalomino <salvatpalvat@gmail.com>
2018-01-31 14:07:16 -0500

    Create and export makeDeeplinkClickEvent()

--- src/core/plugins/deep-linking/helpers.js
+++ src/core/plugins/deep-linking/helpers.js
@@ -1,7 +1,25 @@
-export const setHash = (value) => {
+const setHash = (value) => {
   if(value) {
     return history.pushState(null, null, `#${value}`)
   } else {
     return window.location.hash = ""
   }
 }
+
+const makeDeeplinkClickEvent = () => {
+  const createEvent = function() {
+    var deeplinks = document.querySelectorAll("[name=deeplink]")
+    if (deeplinks.length === 0) return
+    deeplinks.forEach((link) => {
+        link.addEventListener("click", function() {
+            const deeplinkClickEvent = new Event("deeplinkClick", {bubbles: true})
+            link.dispatchEvent(deeplinkClickEvent)
+        })
+    })
+  }
+  setTimeout(createEvent, 2000)
+}
+
+
+
+export {setHash, makeDeeplinkClickEvent}
