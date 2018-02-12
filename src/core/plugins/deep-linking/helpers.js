f0105f29bd1591caecdebe8b24f488d178a1bdc7
srpalomino <salvatpalvat@gmail.com>
2018-02-11 22:15:37 -0500

    Improve the documentation of makeDeeplinks() and create a comment for the operation component's componentDidMount()

--- src/core/plugins/deep-linking/helpers.js
+++ src/core/plugins/deep-linking/helpers.js
@@ -6,18 +6,27 @@ const setHash = (value) => {
   }
 }
 
+
+
 const makeDeeplinks = (rootNode) => {
   /**
-      Given an element instance, some of its descendant anchor elements
-      are converted into deeplinks. Atleast in the context of implemenation,
-      a deeplink is any anchor element that dispatches a deeplink click event
-      when it is clicked.
-  **/
+    Converts an element's qualified descendants into deep links. A qualified
+    descendant is an anchor element whose href attribute is assigned the hash of an
+    operation or tag, and whose name attribute is assigned `deeplink`. A qualified
+    descendant becomes a deep link when it can dispatch a deeplinkClick event whenever
+    the descendant is clicked.
+
+    Arguments:
+      a) rootNode: element object
+
+**/
   const callback = function() {
     const deeplinks = rootNode.querySelectorAll("[name=deeplink]")
     if (deeplinks.length === 0) return
     deeplinks.forEach((link) => {
-        link.addEventListener("click", function() {
+        link.addEventListener("click", function(event) {
+            event.preventDefault()
+            window.location.hash = event.target.getAttribute("href")
             const deeplinkClickEvent = new Event("deeplinkClick", {bubbles: true})
             link.dispatchEvent(deeplinkClickEvent)
         })
