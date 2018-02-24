1263b2ed83bcbfa67f31c5bb5e4561f79199ea3c
davidvujic <david@vujic.se>
2018-02-24 15:40:11 +0100

    fix(edgeCases): added unit tests after PR feedback

--- test/core/plugins/filter/OpsFilter.js
+++ test/core/plugins/filter/OpsFilter.js
@@ -3,11 +3,23 @@ import OpsFilter from "corePlugins/filter/OpsFilter"
 import expect from "expect"
 
 describe("OpsFilter", function() {
-  it("should filter taggedOps by tag name", function () {
-    const taggedOps = Map([["pet"], ["store"], ["user"]])
+  const taggedOps = Map([["pet"], ["store"], ["user"]])
 
+  it("should filter taggedOps by tag name", function () {
     const filtered = OpsFilter(taggedOps, "sto")
 
     expect(filtered.size).toEqual(1)
+  })
+
+  it("should return all taggedOps when search phrase is empty", function () {
+    const filtered = OpsFilter(taggedOps, "")
+
+    expect(filtered.size).toEqual(taggedOps.size)
+  })
+
+  it("should return empty result when there is no match", function () {
+    const filtered = OpsFilter(taggedOps, "NoMatch")
+
+    expect(filtered.size).toEqual(0)
   })
 })
