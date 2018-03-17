b214abfbe617264a10de93ab9282f03f81f9d94a
Kyle Shockey <shockey@users.noreply.github.com>
2018-03-16 21:54:23 -0700

    tests: add cases for oas3 preauthorization

--- test/core/plugins/auth/preauthorize.js
+++ test/core/plugins/auth/preauthorize.js
@@ -28,7 +28,34 @@ const S2_SYSTEM = {
   }
 }
 
-describe.only("auth plugin - preauthorizers", () => {
+const OAI3_SYSTEM = {
+  authActions: {
+    authorize
+  },
+  specSelectors: {
+    isOAS3: () => true,
+    specJson: () => {
+      return fromJS({
+        openapi: "3.0.0",
+        components: {
+          securitySchemes: {
+            basicAuth: {
+              type: "http",
+              scheme: "basic"
+            },
+            APIKeyHeader: {
+              type: "apiKey",
+              in: "header",
+              name: "X-API-Key"
+            }
+          }
+        }
+      })
+    }
+  }
+}
+
+describe("auth plugin - preauthorizers", () => {
   describe("preauthorizeBasic", () => {
     it("should return a valid authorize action in Swagger 2", () => {
       const res = preauthorizeBasic(S2_SYSTEM, "basicAuth", "user", "pass")
@@ -48,8 +75,32 @@ describe.only("auth plugin - preauthorizers", () => {
         }
       })
     })
-    it("should return null when the authorization name is invalid", () => {
+    it("should return a valid authorize action in OpenAPI 3", () => {
+      const res = preauthorizeBasic(OAI3_SYSTEM, "basicAuth", "user", "pass")
+
+      expect(res).toEqual({
+        type: "authorize",
+        payload: {
+          basicAuth: {
+            schema: {
+              type: "http",
+              scheme: "basic"
+            },
+            value: {
+              username: "user",
+              password: "pass"
+            }
+          }
+        }
+      })
+    })
+    it("should return null when the authorization name is invalid in Swagger 2", () => {
       const res = preauthorizeBasic(S2_SYSTEM, "fakeBasicAuth", "user", "pass")
+
+      expect(res).toEqual(null)
+    })
+    it("should return null when the authorization name is invalid in OpenAPI 3", () => {
+      const res = preauthorizeBasic(OAI3_SYSTEM, "fakeBasicAuth", "user", "pass")
 
       expect(res).toEqual(null)
     })
@@ -72,8 +123,30 @@ describe.only("auth plugin - preauthorizers", () => {
         }
       })
     })
-    it("should return null when the authorization name is invalid", () => {
+    it("should return a valid authorize action in OpenAPI 3", () => {
+      const res = preauthorizeApiKey(OAI3_SYSTEM, "APIKeyHeader", "Asdf1234")
+
+      expect(res).toEqual({
+        type: "authorize",
+        payload: {
+          APIKeyHeader: {
+            schema: {
+              type: "apiKey",
+              "in": "header",
+              name: "X-API-Key"
+            },
+            value: "Asdf1234"
+          }
+        }
+      })
+    })
+    it("should return null when the authorization name is invalid in Swagger 2", () => {
       const res = preauthorizeApiKey(S2_SYSTEM, "FakeAPIKeyHeader", "Asdf1234")
+
+      expect(res).toEqual(null)
+    })
+    it("should return null when the authorization name is invalid in OpenAPI 3", () => {
+      const res = preauthorizeApiKey(OAI3_SYSTEM, "FakeAPIKeyHeader", "Asdf1234")
 
       expect(res).toEqual(null)
     })
