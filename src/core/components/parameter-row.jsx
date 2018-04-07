a77e5ccf83804dcbe6f0de66f52d2376c252e7c7
David DE CARVALHO <dedece35@gmail.com>
2018-04-08 00:20:47 +0200

    Display example value in Swagger ReadOnly documentation

--- src/core/components/parameter-row.jsx
+++ src/core/components/parameter-row.jsx
@@ -177,6 +177,11 @@ export default class ParameterRow extends Component {
             : null
           }
 
+          { (bodyParam || !isExecute) && paramExample !== undefined ?
+            <Markdown source={"<i>Example</i> : " + paramExample}/>
+            : null
+          }
+
           {(isFormData && !isFormDataSupported) && <div>Error: your browser does not support FormData</div>}
 
           { bodyParam || !isExecute ? null
