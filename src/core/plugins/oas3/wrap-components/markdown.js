aaabe7c32b3fec623001e2791a9cd6ba6b5628fd
Kyle Shockey <shockey@users.noreply.github.com>
2018-07-18 18:53:49 -0700

    trim Markdown HTML string output before rendering

--- src/core/plugins/oas3/wrap-components/markdown.js
+++ src/core/plugins/oas3/wrap-components/markdown.js
@@ -12,10 +12,16 @@ export const Markdown = ({ source, className = "" }) => {
     const html = parser.render(source)
     const sanitized = sanitizer(html)
 
+    let trimmed
+
+    if(typeof sanitized === "string") {
+      trimmed = sanitized.trim()
+    }
+
     return (
       <div
         dangerouslySetInnerHTML={{
-          __html: sanitized
+          __html: trimmed
         }}
         className={cx(className, "renderedMarkdown")}
       />
