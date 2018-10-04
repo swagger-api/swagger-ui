0d1605179491c4d8b6a4b192426b7a6d7373b7f9
Kyle Shockey <shockey@users.noreply.github.com>
2018-10-04 17:48:31 -0500

    break plugin object inheritance in combinePlugins

--- src/core/system.js
+++ src/core/system.js
@@ -4,6 +4,7 @@ import Im, { fromJS, Map } from "immutable"
 import deepExtend from "deep-extend"
 import { combineReducers } from "redux-immutable"
 import serializeError from "serialize-error"
+import assignDeep from "@kyleshockey/object-assign-deep"
 import { NEW_THROWN_ERR } from "corePlugins/err/actions"
 import win from "core/window"
 
@@ -310,8 +311,9 @@ export default class Store {
 }
 
 function combinePlugins(plugins, toolbox) {
-  if(isObject(plugins) && !isArray(plugins))
-    return plugins
+  if(isObject(plugins) && !isArray(plugins)) {
+    return assignDeep({}, plugins)
+  }
 
   if(isFunc(plugins))
     return combinePlugins(plugins(toolbox), toolbox)
