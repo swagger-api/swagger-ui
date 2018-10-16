3b5028051fb369eaa705f6bd414681356f37cb3f
Kyle Shockey <shockey@users.noreply.github.com>
2018-10-16 17:44:16 -0500

    add deep linking tests for tags/operationIds with underscores

--- test/e2e-cypress/tests/deep-linking.js
+++ test/e2e-cypress/tests/deep-linking.js
@@ -72,6 +72,40 @@ describe("Deep linking feature", () => {
       })
     })
 
+    describe("Operation with underscores in tag+id", () => {
+      const elementToGet = ".opblock-patch"
+      const correctElementId = "operations-underscore_Tag-underscore_Operation"
+      const correctFragment = "#/underscore_Tag/underscore_Operation"
+
+      it("should generate a correct element ID", () => {
+        cy.get(elementToGet)
+          .should("have.id", correctElementId)
+      })
+
+      it("should add the correct element fragment to the URL when expanded", () => {
+        cy.get(elementToGet)
+          .click()
+          .window()
+          .should("have.deep.property", "location.hash", correctFragment)
+      })
+
+      it("should provide an anchor link that has the correct fragment as href", () => {
+        cy.get(elementToGet)
+          .find("a")
+          .should("have.attr", "href", correctFragment)
+          .click()
+          .should("have.attr", "href", correctFragment) // should be valid after expanding
+
+      })
+
+      it("should expand the operation when reloaded", () => {
+        cy.visit(`${baseUrl}${correctFragment}`)
+          .reload()
+          .get(`${elementToGet}.is-open`)
+          .should("exist")
+      })
+    })
+
     describe("Operation with no operationId", () => {
       const elementToGet = ".opblock-put"
       const correctElementId = "operations-tagTwo-put_noOperationId"
@@ -162,6 +196,40 @@ describe("Deep linking feature", () => {
       const elementToGet = ".opblock-post"
       const correctElementId = "operations-my_Tag-my_Operation"
       const correctFragment = "#/my_Tag/my_Operation"
+
+      it("should generate a correct element ID", () => {
+        cy.get(elementToGet)
+          .should("have.id", correctElementId)
+      })
+
+      it("should add the correct element fragment to the URL when expanded", () => {
+        cy.get(elementToGet)
+          .click()
+          .window()
+          .should("have.deep.property", "location.hash", correctFragment)
+      })
+
+      it("should provide an anchor link that has the correct fragment as href", () => {
+        cy.get(elementToGet)
+          .find("a")
+          .should("have.attr", "href", correctFragment)
+          .click()
+          .should("have.attr", "href", correctFragment) // should be valid after expanding
+
+      })
+
+      it("should expand the operation when reloaded", () => {
+        cy.visit(`${baseUrl}${correctFragment}`)
+          .reload()
+          .get(`${elementToGet}.is-open`)
+          .should("exist")
+      })
+    })
+
+    describe("Operation with underscores in tag+id", () => {
+      const elementToGet = ".opblock-patch"
+      const correctElementId = "operations-underscore_Tag-underscore_Operation"
+      const correctFragment = "#/underscore_Tag/underscore_Operation"
 
       it("should generate a correct element ID", () => {
         cy.get(elementToGet)
