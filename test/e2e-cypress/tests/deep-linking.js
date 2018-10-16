feb69817224f77bf819a03a867d352ed99b53cbc
Kyle Shockey <shockey@users.noreply.github.com>
2018-10-16 17:22:53 -0500

    add tests for operation lacking an operationId

--- test/e2e-cypress/tests/deep-linking.js
+++ test/e2e-cypress/tests/deep-linking.js
@@ -72,6 +72,40 @@ describe("Deep linking feature", () => {
       })
     })
 
+    describe("Operation with no operationId", () => {
+      const elementToGet = ".opblock-put"
+      const correctElementId = "operations-tagTwo-put_noOperationId"
+      const correctFragment = "#/tagTwo/put_noOperationId"
+
+      it("should generate a correct element ID", () => {
+        cy.get(elementToGet)
+          .should("have.id", correctElementId)
+      })
+
+      it("should add the correct element fragment to the URL when expanded", () => {
+        cy.get(elementToGet)
+          .click()
+          .window()
+          .should("have.deep.property", "location.hash", correctFragment)
+      })
+
+      it("should provide an anchor link that has the correct fragment as href", () => {
+        cy.get(elementToGet)
+          .find("a")
+          .should("have.attr", "href", correctFragment)
+          .click()
+          .should("have.attr", "href", correctFragment) // should be valid after expanding
+
+      })
+
+      it("should expand the operation when reloaded", () => {
+        cy.visit(`${baseUrl}${correctFragment}`)
+          .reload()
+          .get(`${elementToGet}.is-open`)
+          .should("exist")
+      })
+    })
+
     describe("regular Operation with `docExpansion: none` enabled", function() {
       it("should expand a tag", () => {
         cy.visit(`${baseUrl}&docExpansion=none#/myTag`)
@@ -128,6 +162,40 @@ describe("Deep linking feature", () => {
       const elementToGet = ".opblock-post"
       const correctElementId = "operations-my_Tag-my_Operation"
       const correctFragment = "#/my_Tag/my_Operation"
+
+      it("should generate a correct element ID", () => {
+        cy.get(elementToGet)
+          .should("have.id", correctElementId)
+      })
+
+      it("should add the correct element fragment to the URL when expanded", () => {
+        cy.get(elementToGet)
+          .click()
+          .window()
+          .should("have.deep.property", "location.hash", correctFragment)
+      })
+
+      it("should provide an anchor link that has the correct fragment as href", () => {
+        cy.get(elementToGet)
+          .find("a")
+          .should("have.attr", "href", correctFragment)
+          .click()
+          .should("have.attr", "href", correctFragment) // should be valid after expanding
+
+      })
+
+      it("should expand the operation when reloaded", () => {
+        cy.visit(`${baseUrl}${correctFragment}`)
+          .reload()
+          .get(`${elementToGet}.is-open`)
+          .should("exist")
+      })
+    })
+
+    describe("Operation with no operationId", () => {
+      const elementToGet = ".opblock-put"
+      const correctElementId = "operations-tagTwo-put_noOperationId"
+      const correctFragment = "#/tagTwo/put_noOperationId"
 
       it("should generate a correct element ID", () => {
         cy.get(elementToGet)
