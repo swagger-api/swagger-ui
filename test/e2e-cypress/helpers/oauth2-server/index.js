627f9fa723878026d1b2a5a43399d5798465bb20
Kyle Shockey <shockey@users.noreply.github.com>
2018-10-18 16:10:30 -0500

    continue implementing OAuth tests

--- test/e2e-cypress/helpers/oauth2-server/index.js
+++ test/e2e-cypress/helpers/oauth2-server/index.js
@@ -1,10 +1,15 @@
 // from https://github.com/pedroetb/node-oauth2-server-example
 
+var Http = require("http")
+var path = require("path")
 var express = require("express")
 var bodyParser = require("body-parser")
 var oauthserver = require("oauth2-server")
+var cors = require("cors")
 
 var app = express()
+
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -18,10 +23,28 @@ app.oauth = oauthserver({
 
 app.all("/oauth/token", app.oauth.grant())
 
-app.get("/", app.oauth.authorise(), function (req, res) {
+app.get('/swagger.yaml', function (req, res) {
+  res.sendFile(path.join(__dirname, "swagger.yaml"));
+});
+
+app.get("*", app.oauth.authorise(), function (req, res) {
   res.send("Secret secrets are no fun, secret secrets hurt someone.")
 })
 
 app.use(app.oauth.errorHandler())
 
-app.listen(3000)
+function startServer() {
+  var httpServer = Http.createServer(app)
+  httpServer.listen("3231")
+
+  return function stopServer() {
+    httpServer.close()
+  }
+}
+
+module.exports = startServer
+
+if (require.main === module) {
+  // for debugging
+  startServer()
+}
