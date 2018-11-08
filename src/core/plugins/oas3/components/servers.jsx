86552867671e1234dd311119f784e27f68098530
Sven Hesse <git@svenhesse.de>
2018-11-08 23:22:15 +0100

    Showing server description in dropdown

--- src/core/plugins/oas3/components/servers.jsx
+++ src/core/plugins/oas3/components/servers.jsx
@@ -110,7 +110,7 @@ export default class Servers extends React.Component {
               <option
                 value={ server.get("url") }
                 key={ server.get("url") }>
-                { server.get("url") }
+                { server.get("url") } { server.get("description") ? "("+server.get("description")+")" : "" }
               </option>
             ).toArray()}
           </select>
