2db63e2c08ca60e5ac34d0d9af81c42ce04dbc07
kyle <shockey@users.noreply.github.com>
2019-01-07 13:40:17 -0600

    fix: Immutable property access pattern (via #5112)

--- src/core/plugins/err/error-transformers/transformers/strip-instance.js
+++ src/core/plugins/err/error-transformers/transformers/strip-instance.js
@@ -1,7 +1,7 @@
 export function transform(errors) {
   return errors
     .map(err => {
-      if(err.type === "schema") {
+      if(err.get("type") === "schema") {
         return err.set("message", removeSubstring(err.get("message"), "instance\\."))
       } else {
         return err
