5a954c868612d758b5ffd0b266853ae6a446dd9c
Kyle Shockey <shockey@users.noreply.github.com>
2019-01-12 00:53:40 -0600

    add failing tests

--- test/core/plugins/spec/selectors.js
+++ test/core/plugins/spec/selectors.js
@@ -3,6 +3,7 @@ import expect from "expect"
 import { fromJS } from "immutable"
 import { fromJSOrdered } from "core/utils"
 import {
+  definitions,
   parameterValues,
   contentTypeValues,
   operationScheme,
@@ -20,6 +21,70 @@ import {
 } from "../../../../src/core/plugins/spec/selectors"
 
 describe("spec plugin - selectors", function(){
+
+  describe("definitions", function(){
+    it("should return definitions by default", function(){
+
+      // Given
+      const spec = fromJS({
+        json: {
+          swagger: "2.0",
+          definitions: {
+            a: {
+              type: "string"
+            },
+            b: {
+              type: "string"
+            }
+          }
+        }
+      })
+
+      // When
+      let res = definitions(spec)
+
+      // Then
+      expect(res.toJS()).toEqual({
+        a: {
+          type: "string"
+        },
+        b: {
+          type: "string"
+        }
+      })
+    })
+    it("should return an empty Map when missing definitions", function(){
+
+      // Given
+      const spec = fromJS({
+        json: {
+          swagger: "2.0"
+        }
+      })
+
+      // When
+      let res = definitions(spec)
+
+      // Then
+      expect(res.toJS()).toEqual({})
+    })
+    it("should return an empty Map when given non-object definitions", function(){
+
+      // Given
+      const spec = fromJS({
+        json: {
+          swagger: "2.0",
+          definitions: "..."
+        }
+      })
+
+      // When
+      let res = definitions(spec)
+
+      // Then
+      expect(res.toJS()).toEqual({})
+    })
+  })
 
   describe("parameterValue", function(){
 
