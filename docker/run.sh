28d2413b023a96dc372d56368578ebfba91b3d1a
Alex Vasilenko <aa.vasilenko@gmail.com>
2019-02-13 16:40:02 +0100

    copy whole directory with swagger definitions
    To be able to resolve relative references as described in #5008

--- docker/run.sh
+++ docker/run.sh
@@ -30,7 +30,9 @@ fi
 replace_in_index myApiKeyXXXX123456789 $API_KEY
 
 if [[ -f $SWAGGER_JSON ]]; then
-  cp -s $SWAGGER_JSON $NGINX_ROOT
+  cp -s $(dirname $SWAGGER_JSON)/*.yml $NGINX_ROOT
+  cp -s $(dirname $SWAGGER_JSON)/*.yaml $NGINX_ROOT
+  cp -s $(dirname $SWAGGER_JSON)/*.json $NGINX_ROOT
   REL_PATH="./$(basename $SWAGGER_JSON)"
   sed -i "s|https://petstore.swagger.io/v2/swagger.json|$REL_PATH|g" $INDEX_FILE
   sed -i "s|http://example.com/api|$REL_PATH|g" $INDEX_FILE
