d73839ff532d49057d95ca3218a3443a91733248
Kyle Shockey <shockey@users.noreply.github.com>
2019-02-22 12:02:38 -0800

    WIP: `displayOperationId` support

--- flavors/swagger-ui-react/index.js
+++ flavors/swagger-ui-react/index.js
@@ -10,7 +10,8 @@ export class SwaggerUI extends React.Component {
   
   componentDidMount() {
     const ui = swaggerUIConstructor({
-      url: this.props.url
+      url: this.props.url,
+      displayOperationId: this.props.displayOperationId
     })
 
     this.system = ui
@@ -21,5 +22,11 @@ export class SwaggerUI extends React.Component {
   
   render() {
     return this.SwaggerUIComponent ? <this.SwaggerUIComponent /> : null
+  }
+
+  componentDidUpdate(prevProps) {
+    if(this.props.displayOperationId !== prevProps.displayOperationId) {
+      this.system.getConfigs().displayOperationId = this.props.displayOperationId  // XXX: this is bad
+    }
   }
 }
