ff496fa5bd1f3d5afacdcd186016ca1f671f7579
kyle <shockey@users.noreply.github.com>
2019-02-23 17:00:29 -0800

    fix: type safety for operation summary handling (via #5191)
    * fix: gracefully handle non-string operation summaries
    
    * use lodash for more summary type safety

--- test/e2e-cypress/tests/bugs/5188.js
+++ test/e2e-cypress/tests/bugs/5188.js
@@ -2,9 +2,17 @@ describe("#5188: non-string operation summary value", () => {
   it("should gracefully handle an object value for an operation summary", () => {
     cy
       .visit("?url=/documents/bugs/5188.yaml")
-      .get("#operations-pet-addPet")
+      .get("#operations-default-objectSummary")
       .click()
       .get(".opblock-summary-description")
       .contains("[object Object]")
-    })
+  })
+  it("should gracefully handle a missing value for an operation summary", () => {
+    cy
+      .visit("?url=/documents/bugs/5188.yaml")
+      .get("#operations-default-noSummary")
+      .click()
+      // check for response rendering; makes sure the Operation itself rendered
+      .contains("Invalid input")
+  })
 })
