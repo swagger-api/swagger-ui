7d97cfb4f5797a912e37a6f21b1f7487d08ce20d
Kyle Shockey <shockey@users.noreply.github.com>
2019-02-28 16:46:03 -0800

    remove independent manifest; build releasable exclusively from template

--- flavors/swagger-ui-react/release/create-manifest.js
+++ flavors/swagger-ui-react/release/create-manifest.js
@@ -1,5 +1,5 @@
 var jsonMerger = require("json-merger")
 var fs = require("fs")
-var result = jsonMerger.mergeFiles(["../../../package.json", "../package.json", "template.json"])
+var result = jsonMerger.mergeFiles(["../../../package.json", "template.json"])
 
 process.stdout.write(JSON.stringify(result, null, 2))
