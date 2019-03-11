d0ece2e8d97b601662507e4c8f7f4415de340f1a
Peter Goldthorp <peter.goldthorp@oracle.com>
2019-03-11 16:45:57 -0400

    Remove react-addons-perf dependency

--- src/core/index.js
+++ src/core/index.js
@@ -7,10 +7,6 @@ import ApisPreset from "core/presets/apis"
 import * as AllPlugins from "core/plugins/all"
 import { parseSearch } from "core/utils"
 
-if (process.env.NODE_ENV !== "production" && typeof window !== "undefined") {
-  win.Perf = require("react-addons-perf")
-}
-
 // eslint-disable-next-line no-undef
 const { GIT_DIRTY, GIT_COMMIT, PACKAGE_VERSION, HOSTNAME, BUILD_TIME } = buildInfo
 
