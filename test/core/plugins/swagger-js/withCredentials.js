89e456d53bb41852e8046a743c5ba227d2c421bf
kyle <shockey@users.noreply.github.com>
2019-03-19 20:33:58 -0700

    add more test cases

--- test/core/plugins/swagger-js/withCredentials.js
+++ test/core/plugins/swagger-js/withCredentials.js
@@ -2,7 +2,7 @@ import expect, { createSpy } from "expect"
 import { loaded } from "corePlugins/swagger-js/configs-wrap-actions"
 
 describe("swagger-js plugin - withCredentials", () => {
-  it("should default to false", () => {
+  it("should have no effect by default", () => {
     const system = {
       fn: {
         fetch: createSpy().andReturn(Promise.resolve())
@@ -15,10 +15,10 @@ describe("swagger-js plugin - withCredentials", () => {
     loadedFn()
 
     expect(oriExecute.calls.length).toBe(1)
-    expect(system.fn.fetch.withCredentials).toBe(false)
+    expect(system.fn.fetch.withCredentials).toBe(undefined)
   })
 
-  it("should allow settings it to true via config", () => {
+  it("should allow setting flag to true via config", () => {
     const system = {
       fn: {
         fetch: createSpy().andReturn(Promise.resolve())
@@ -34,5 +34,61 @@ describe("swagger-js plugin - withCredentials", () => {
 
     expect(oriExecute.calls.length).toBe(1)
     expect(system.fn.fetch.withCredentials).toBe(true)
+  })
+  
+  it("should allow setting flag to false via config", () => {
+    const system = {
+      fn: {
+        fetch: createSpy().andReturn(Promise.resolve())
+      },
+      getConfigs: () => ({
+        withCredentials: false
+      })
+    }
+    const oriExecute = createSpy()
+
+    const loadedFn = loaded(oriExecute, system)
+    loadedFn()
+
+    expect(oriExecute.calls.length).toBe(1)
+    expect(system.fn.fetch.withCredentials).toBe(false)
+  })
+  
+   it("should allow setting flag to true via config as string", () => {
+    // for query string config
+    const system = {
+      fn: {
+        fetch: createSpy().andReturn(Promise.resolve())
+      },
+      getConfigs: () => ({
+        withCredentials: "true"
+      })
+    }
+    const oriExecute = createSpy()
+
+    const loadedFn = loaded(oriExecute, system)
+    loadedFn()
+
+    expect(oriExecute.calls.length).toBe(1)
+    expect(system.fn.fetch.withCredentials).toBe(true)
+  })
+  
+  it("should allow setting flag to false via config as string", () => {
+    // for query string config
+    const system = {
+      fn: {
+        fetch: createSpy().andReturn(Promise.resolve())
+      },
+      getConfigs: () => ({
+        withCredentials: "false"
+      })
+    }
+    const oriExecute = createSpy()
+
+    const loadedFn = loaded(oriExecute, system)
+    loadedFn()
+
+    expect(oriExecute.calls.length).toBe(1)
+    expect(system.fn.fetch.withCredentials).toBe(false)
   })
 })
