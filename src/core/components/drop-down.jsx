bb13e906eeee13c199484d8dfc7f12e3f79910ad
taraokelly <taraokelly.dev@gmail.com>
2019-05-27 11:53:35 +0100

    Dropdown id & custom class props

--- src/core/components/drop-down.jsx
+++ src/core/components/drop-down.jsx
@@ -1,20 +1,25 @@
 import React, { PureComponent } from "react"
 import PropTypes from "prop-types"
+import cx from "classnames"
 
 export default class DropDown extends PureComponent {
   static propTypes = {
+    id: PropTypes.string,
+    className: PropTypes.string,
     children: PropTypes.node.isRequired
   }
 
+  classHandler = (className) => cx(className, {[`${className}--${this.props.className}`] : this.props.className})
   
   render() {
+    const { id, children } = this.props
     const isOpen = true
     const isDisabled = false
 
     return (
-			<div className="dropdown">
+			<div id={id} className={this.classHandler("dropdown")}>
         <button
-          className="dropdown__button"
+          className={this.classHandler("dropdown__button")}
           role="button"
           aria-haspopup={true}
           aria-expanded={isOpen}
@@ -23,10 +28,10 @@ export default class DropDown extends PureComponent {
           I am a dropdown
         </button>
         <div
-          className="dropdown__menu"
+          className={this.classHandler("dropdown__menu")}
           role="menu"
         >
-        {this.props.children}
+          {children}
         </div>
       </div>
 			)
