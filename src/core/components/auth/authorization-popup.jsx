bc10e149d7317507a434899c14e8f583c963f361
taraokelly <taraokelly.dev@gmail.com>
2019-06-07 16:20:18 +0100

    Cancel icon

--- src/core/components/auth/authorization-popup.jsx
+++ src/core/components/auth/authorization-popup.jsx
@@ -13,43 +13,38 @@ export default class AuthorizationPopup extends React.Component {
     let definitions = authSelectors.shownDefinitions()
     const Auths = getComponent("auths")
     const Button = getComponent("Button")
+    const Icon = getComponent("Icon")
 
     return (
       <div className="dialog-ux">
         <div className="backdrop-ux"></div>
         <div className="modal-ux">
-          <div className="modal-dialog-ux">
-            <div className="modal-ux-inner">
-              <div className="modal-ux-header">
-                <h3>Available authorizations</h3>
-                <Button
-                  type="button"
-                  className="sui-btn-transparent close-modal"
-                  onClick={ this.close }
-                  unstyled
-                >
-                  <svg width="20" height="20">
-                    <use href="#close" xlinkHref="#close" />
-                  </svg>
-                </Button>
-              </div>
-              <div className="modal-ux-content">
-
-                {
-                  definitions.valueSeq().map(( definition, key ) => {
-                    return <Auths key={ key }
-                                  AST={AST}
-                                  definitions={ definition }
-                                  getComponent={ getComponent }
-                                  errSelectors={ errSelectors }
-                                  authSelectors={ authSelectors }
-                                  authActions={ authActions }
-                                  specSelectors={ specSelectors }/>
-                  })
-                }
-              </div>
-            </div>
+          <div className="modal-ux-header">
+            <h3>Available authorizations</h3>
+            <Button
+              type="button"
+              className="sui-btn-transparent modal-close"
+              onClick={ this.close }
+              unstyled
+            >
+              <Icon icon="cancel"/>
+            </Button>
           </div>
+          <div className="modal-ux-content">
+
+              {
+                definitions.valueSeq().map(( definition, key ) => {
+                  return <Auths key={ key }
+                                AST={AST}
+                                definitions={ definition }
+                                getComponent={ getComponent }
+                                errSelectors={ errSelectors }
+                                authSelectors={ authSelectors }
+                                authActions={ authActions }
+                                specSelectors={ specSelectors }/>
+                })
+              }
+            </div>
         </div>
       </div>
     )
