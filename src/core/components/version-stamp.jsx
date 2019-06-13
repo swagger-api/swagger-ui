c71b377a7e8716d46207821275cbc3d15bd0bad0
Rody Kirwan <Rody.Kirwan@smartbear.com>
2019-06-13 12:35:14 +0100

    Update version-stamps so that oas3 stamp is wrapped in Ori to avoid awkard nesting

--- src/core/components/version-stamp.jsx
+++ src/core/components/version-stamp.jsx
@@ -1,14 +1,16 @@
 import React from "react"
 import PropTypes from "prop-types"
 
-const VersionStamp = ({ version }) => (
-  <div>
+const VersionStamp = ({ version, children }) => (
+  <div className="info__version-stamp">
     <small><pre className="version"> { version } </pre></small>
+    { children }
   </div>
 )
 
 VersionStamp.propTypes = {
-  version: PropTypes.string.isRequired
+  version: PropTypes.string.isRequired,
+  children: PropTypes.node
 }
 
 export default VersionStamp
