ce4d512fb66412d4d5559d4042e6e51ed236f144
Rody Kirwan <Rody.Kirwan@smartbear.com>
2019-06-17 11:40:30 +0100

    Remove duplicate propType keys

--- src/core/components/layout-utils/drop-down.jsx
+++ src/core/components/layout-utils/drop-down.jsx
@@ -261,8 +261,6 @@ export class DropDownItem extends Component {
 
   static propTypes = {
     id: PropTypes.string,
-    selected: PropTypes.bool.isRequired, 
-    initialSelect: PropTypes.func.isRequired,
     mod: PropTypes.string,
     value: PropTypes.string,
     optionKey: PropTypes.any,
