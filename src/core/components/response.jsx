258cc86f19380a6aea33bac1edb61b055a9c2acd
Kyle Shockey <shockey@users.noreply.github.com>
2019-06-18 14:12:03 +0100

    prettier reformat

--- src/core/components/response.jsx
+++ src/core/components/response.jsx
@@ -146,44 +146,50 @@ export default class Response extends React.Component {
             <Markdown source={ response.get( "description" ) } />
           </div>
 
-          {
-            isOAS3 ? (
-              <section className="response-controls">
-                <div
-                  className={cx("response-control-media-type", {
-                    "response-control-media-type--accept-controller": controlsAcceptHeader
-                  })}
-                >
-                  <small className="response-control-media-type__title">Media type</small>
-                  <ContentType
-                    value={this.state.responseContentType}
-                    contentTypes={
-                      response.get("content") ? response.get("content").keySeq() : Seq()
-                    }
-                    onChange={this._onContentTypeChange}
-                  />
-                  {controlsAcceptHeader ? (
-                    <small className="response-control-media-type__accept-message">
-                      Controls <code>Accept</code> header.
-                    </small>
-                  ) : null}
-                </div>
-                {examplesForMediaType ? (
-                  <div className="response-control-examples">
-                    <small className="response-control-examples__title">Examples</small>
-                    <ExamplesSelect
-                      examples={examplesForMediaType}
-                      currentValue={examplesForMediaType.getIn([
-                        this.getTargetExamplesKey(),
-                        "value"
-                      ])}
-                      onSelect={(v, key) => this.setState({ activeExamplesKey: key })}
-                    />
-                  </div>
+          {isOAS3 ? (
+            <section className="response-controls">
+              <div
+                className={cx("response-control-media-type", {
+                  "response-control-media-type--accept-controller": controlsAcceptHeader
+                })}
+              >
+                <small className="response-control-media-type__title">
+                  Media type
+                </small>
+                <ContentType
+                  value={this.state.responseContentType}
+                  contentTypes={
+                    response.get("content")
+                      ? response.get("content").keySeq()
+                      : Seq()
+                  }
+                  onChange={this._onContentTypeChange}
+                />
+                {controlsAcceptHeader ? (
+                  <small className="response-control-media-type__accept-message">
+                    Controls <code>Accept</code> header.
+                  </small>
                 ) : null}
-              </section>
-            ) : null
-          }
+              </div>
+              {examplesForMediaType ? (
+                <div className="response-control-examples">
+                  <small className="response-control-examples__title">
+                    Examples
+                  </small>
+                  <ExamplesSelect
+                    examples={examplesForMediaType}
+                    currentValue={examplesForMediaType.getIn([
+                      this.getTargetExamplesKey(),
+                      "value"
+                    ])}
+                    onSelect={(v, key) =>
+                      this.setState({ activeExamplesKey: key })
+                    }
+                  />
+                </div>
+              ) : null}
+            </section>
+          ) : null}
 
           { example ? (
             <ModelExample
