9935ea332858b5b4abdeba75cec52bf8873eec75
kyle <shockey@users.noreply.github.com>
2019-07-09 23:25:34 -0500

    housekeeping: upgrade to babel@7 (via #5450)
    * Babel 7
    
    * preserve module output; straighten out ES/CJS syntax
    
    * add webpack TODOs
    
    * remove unused Babel modules
    
    * Update index.js
    
    * install @babel/register

--- src/core/index.js
+++ src/core/index.js
@@ -1,11 +1,15 @@
-import deepExtend from "deep-extend"
+// This file uses CommonJS require/exports syntax in order to export the SwaggerUI
+// function directly, instead of `{ default: fn SwaggerUI }`, which Babel would
+// generate if we used ESM syntax.
 
-import System from "core/system"
-import win from "core/window"
-import ApisPreset from "core/presets/apis"
+const deepExtend = require("deep-extend")
 
-import * as AllPlugins from "core/plugins/all"
-import { parseSearch } from "core/utils"
+const System = require("core/system").default
+const ApisPreset = require("core/presets/apis").default
+const AllPlugins = require("core/plugins/all").default
+const { parseSearch } = require("core/utils")
+const win = require("core/window")
+
 
 if (process.env.NODE_ENV !== "production" && typeof window !== "undefined") {
   win.Perf = require("react-dom/lib/ReactPerf")
