61aaa2948c0696051e2629c0626e409989fdb80e
Kyle Shockey <shockey@users.noreply.github.com>
2019-07-10 18:25:34 -0500

    add bundle config

---
+++ webpack/bundle.babel.js
@@ -0,0 +1,21 @@
+import configBuilder from "./_config-builder"
+
+const result = configBuilder({
+  minimize: true,
+  mangle: true,
+  sourcemaps: true,
+  includeDependencies: true,
+}, {
+  entry: {
+    "swagger-ui-bundle": [
+      "./src/polyfills.js", // TODO: remove?
+      "./src/core/index.js",
+    ]
+  },
+
+  output: {
+    library: "SwaggerUIBundle",
+  }
+})
+
+export default result
