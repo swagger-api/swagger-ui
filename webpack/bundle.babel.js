d0db9d7967341aeac5d9755bcd5d1e62d212ee22
Kyle Shockey <shockey@users.noreply.github.com>
2019-07-10 19:11:26 -0500

    prettier...

--- webpack/bundle.babel.js
+++ webpack/bundle.babel.js
@@ -1,21 +1,28 @@
+/**
+ * @prettier
+ */
+
 import configBuilder from "./_config-builder"
 
-const result = configBuilder({
-  minimize: true,
-  mangle: true,
-  sourcemaps: true,
-  includeDependencies: true,
-}, {
-  entry: {
-    "swagger-ui-bundle": [
-      "./src/polyfills.js", // TODO: remove?
-      "./src/core/index.js",
-    ]
+const result = configBuilder(
+  {
+    minimize: true,
+    mangle: true,
+    sourcemaps: true,
+    includeDependencies: true,
   },
+  {
+    entry: {
+      "swagger-ui-bundle": [
+        "./src/polyfills.js", // TODO: remove?
+        "./src/core/index.js",
+      ],
+    },
 
-  output: {
-    library: "SwaggerUIBundle",
+    output: {
+      library: "SwaggerUIBundle",
+    },
   }
-})
+)
 
 export default result
--- webpack/core.babel.js
+++ webpack/core.babel.js
@@ -1,22 +1,28 @@
-import path from "path"
+/**
+ * @prettier
+ */
+
 import configBuilder from "./_config-builder"
 
-const result = configBuilder({
-  minimize: true,
-  mangle: true,
-  sourcemaps: true,
-  includeDependencies: false,
-}, {
-  entry: {
-    "swagger-ui": [
-      "./src/polyfills.js", // TODO: remove?
-      "./src/core/index.js",
-    ]
+const result = configBuilder(
+  {
+    minimize: true,
+    mangle: true,
+    sourcemaps: true,
+    includeDependencies: false,
   },
+  {
+    entry: {
+      "swagger-ui": [
+        "./src/polyfills.js", // TODO: remove?
+        "./src/core/index.js",
+      ],
+    },
 
-  output:  {
-    library: "SwaggerUICore",
+    output: {
+      library: "SwaggerUICore",
+    },
   }
-})
+)
 
 export default result
