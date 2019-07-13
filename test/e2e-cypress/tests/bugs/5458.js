91b9dc46ae0dbe165bb31477d2c210ee168ba78f
Kyle Shockey <shockey@users.noreply.github.com>
2019-07-13 00:11:45 -0500

    fix: Swagger 2.0 `Response.examples`

---
+++ test/e2e-cypress/tests/bugs/5458.js
@@ -0,0 +1,18 @@
+// http://github.com/swagger-api/swagger-ui/issues/5458
+
+describe("#5458: Swagger 2.0 `Response.examples` mappings", () => {
+  it("should render a custom example when a schema is not defined", () => {
+    cy.visit("/?url=/documents/bugs/5458.yaml")
+      .get("#operations-default-get_foo1")
+      .click()
+      .get(".model-example .highlight-code")
+      .contains("custom value")
+  })
+  it("should render a custom example when a schema is defined", () => {
+    cy.visit("/?url=/documents/bugs/5458.yaml")
+      .get("#operations-default-get_foo2")
+      .click()
+      .get(".model-example .highlight-code")
+      .contains("custom value")
+  })
+})
