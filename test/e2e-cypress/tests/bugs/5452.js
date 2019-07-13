85acb6c66b3475910f9266700c8aaf5b3054b4e0
Kyle Shockey <shockey@users.noreply.github.com>
2019-07-12 22:52:16 -0500

    add failing tests

---
+++ test/e2e-cypress/tests/bugs/5452.js
@@ -0,0 +1,34 @@
+/**
+ * @prettier
+ */
+
+describe("#5452: <Select /> crashing in Parameters", function() {
+  describe("in OpenAPI 3", () => {
+    it("should not result in a render error", function() {
+      cy.visit("http://localhost:3230/?url=/documents/bugs/5452/openapi.yaml")
+        .get("#operations-default-get_endpoint")
+        .click()
+        .get(".parameters > tbody > tr > .col > select")
+        .select("")
+        .get(".parameters > tbody > tr > .col > select")
+        .should("exist")
+        .select("fruit")
+        .get(".parameters > tbody > tr > .col > select")
+        .should("exist")
+    })
+  })
+  describe("in Swagger 2", () => {
+    it("should not result in a render error", function() {
+      cy.visit("http://localhost:3230/?url=/documents/bugs/5452/swagger.yaml")
+        .get("#operations-default-get_endpoint")
+        .click()
+        .get(".parameters > tbody > tr > .col > select")
+        .select("")
+        .get(".parameters > tbody > tr > .col > select")
+        .should("exist")
+        .select("fruit")
+        .get(".parameters > tbody > tr > .col > select")
+        .should("exist")
+    })
+  })
+})
