be5e057bfa896909c831cd7c987a546258719a59
kyle <shockey@users.noreply.github.com>
2019-07-18 22:02:24 -0500

    housekeeping: npm audit resolutions (via #5457)
    * add `security-audit` script
    
    * npm audit fix
    
    * remove nyc
    
    * nightwatch@1
    
    this breaks the test suite, but it appears to have already regressed. leaving it for another day, TODO: open a backlog ticket
    
    * add `security-audit` script
    
    * disable mocha exclusivity
    
    * update package-lock.json
    
    * cypress@3.4.0
    
    * `npm audit fix`
    
    * @release-it/conventional-changelog@1.1.0
    
    * release-it@12

--- test/e2e-selenium/scenarios/bugs/4196.js
+++ test/e2e-selenium/scenarios/bugs/4196.js
@@ -6,11 +6,9 @@ describe("bug #4196: HTTP basic auth credential retention", function () {
             .page.main()
 
         client.waitForElementVisible(".download-url-input:not([disabled])", 5000)
-            .pause(80)
             .clearValue(".download-url-input")
             .setValue(".download-url-input", "http://localhost:3230/test-specs/bugs/4196.yaml")
             .click("button.download-url-button")
-            .pause(1000)
 
         done()
     })
@@ -36,7 +34,7 @@ describe("bug #4196: HTTP basic auth credential retention", function () {
             .waitForElementVisible(`section>input[type="password"]`, 5000)
             .setValue(`section>input[type="password"]`, "bbb") // Set password
             .click(".auth-btn-wrapper button:nth-child(1)") // Click Authorize
-            .pause(5000)
+            .pause(1000)
             .assert.containsText("div.wrapper:nth-child(4)>code", "bbb")
 
         client.end()
