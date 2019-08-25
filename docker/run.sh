28aa46950285992c1e2eb17a2b7dad8055d8d65c
Wesley Schwengle <wesley@schwengle.net>
2019-08-25 12:14:56 +0200

    Move nginx.conf to variable in run.sh
    Signed-off-by: Wesley Schwengle <wesley@schwengle.net>

--- docker/run.sh
+++ docker/run.sh
@@ -4,6 +4,7 @@ set -e
 BASE_URL=${BASE_URL:-/}
 NGINX_ROOT=/usr/share/nginx/html
 INDEX_FILE=$NGINX_ROOT/index.html
+NGINX_CONF=/etc/nginx/nginx.conf
 
 node /usr/share/nginx/configurator $INDEX_FILE
 
@@ -24,7 +25,7 @@ replace_or_delete_in_index () {
 }
 
 if [ "${BASE_URL}" ]; then
-  sed -i "s|location / {|location $BASE_URL {|g" /etc/nginx/nginx.conf
+  sed -i "s|location / {|location $BASE_URL {|g" $NGINX_CONF
 fi
 
 replace_in_index myApiKeyXXXX123456789 $API_KEY
@@ -38,7 +39,7 @@ fi
 
 # replace the PORT that nginx listens on if PORT is supplied
 if [[ -n "${PORT}" ]]; then
-    sed -i "s|8080|${PORT}|g" /etc/nginx/nginx.conf
+    sed -i "s|8080|${PORT}|g" $NGINX_CONF
 fi
 
 find $NGINX_ROOT -type f -regex ".*\.\(html\|js\|css\)" -exec sh -c "gzip < {} > {}.gz" \;
