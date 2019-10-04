898bfc26bedaade09b334d213f23c8a4879e925b
Kyle Shockey <shockey@users.noreply.github.com>
2019-10-04 12:50:03 -0700

    use target-method spies

--- test/mocha/core/oauth2-authorize.js
+++ test/mocha/core/oauth2-authorize.js
@@ -1,5 +1,5 @@
 /* eslint-env mocha */
-import expect, { createSpy } from "expect"
+import expect, { spyOn } from "expect"
 import win from "core/window"
 import oauth2Authorize from "core/oauth2-authorize"
 import * as utils from "core/utils"
@@ -20,43 +20,34 @@ describe("oauth2", function () {
   }
 
   describe("authorize redirect", function () {
-    let initialGenerateCodeVerifier,
-        initialCreateCodeChallenge
-
-    beforeEach(() => {
-      initialGenerateCodeVerifier = utils.generateCodeVerifier
-      initialCreateCodeChallenge = utils.createCodeChallenge
-    })
-
-    afterEach(() => {
-      utils.generateCodeVerifier = initialGenerateCodeVerifier
-      utils.createCodeChallenge = initialCreateCodeChallenge
-    })
-
     it("should build authorize url", function() {
-      win.open = createSpy()
+      const windowOpenSpy = spyOn(win, "open")
       oauth2Authorize(authConfig)
-      expect(win.open.calls.length).toEqual(1)
-      expect(win.open.calls[0].arguments[0]).toMatch("https://testAuthorizationUrl?response_type=code&redirect_uri=&state=")
+      expect(windowOpenSpy.calls.length).toEqual(1)
+      expect(windowOpenSpy.calls[0].arguments[0]).toMatch("https://testAuthorizationUrl?response_type=code&redirect_uri=&state=")
+
+      windowOpenSpy.restore()
     })
 
     it("should append query parameters to authorizeUrl with query parameters", function() {
-      win.open = createSpy()
+      const windowOpenSpy = spyOn(win, "open")
       mockSchema.authorizationUrl = "https://testAuthorizationUrl?param=1"
       oauth2Authorize(authConfig)
-      expect(win.open.calls.length).toEqual(1)
-      expect(win.open.calls[0].arguments[0]).toMatch("https://testAuthorizationUrl?param=1&response_type=code&redirect_uri=&state=")
+      expect(windowOpenSpy.calls.length).toEqual(1)
+      expect(windowOpenSpy.calls[0].arguments[0]).toMatch("https://testAuthorizationUrl?param=1&response_type=code&redirect_uri=&state=")
+      
+      windowOpenSpy.restore()
     })
 
     it("should send code_challenge when using authorizationCode flow with usePkceWithAuthorizationCodeGrant enabled", function () {
-      win.open = createSpy()
+      const windowOpenSpy = spyOn(win, "open")
       mockSchema.flow = "authorizationCode"
 
       const expectedCodeVerifier = "mock_code_verifier"
       const expectedCodeChallenge = "mock_code_challenge"
       
-      utils.generateCodeVerifier = createSpy().andReturn(expectedCodeVerifier)
-      utils.createCodeChallenge = createSpy().andReturn(expectedCodeChallenge)
+      const generateCodeVerifierSpy = spyOn(utils, "generateCodeVerifier").andReturn(expectedCodeVerifier)
+      const createCodeChallengeSpy = spyOn(utils, "createCodeChallenge").andReturn(expectedCodeChallenge)
 
       authConfig.authConfigs.usePkceWithAuthorizationCodeGrant = true
 
@@ -67,12 +58,17 @@ describe("oauth2", function () {
       expect(actualUrl.get("code_challenge")).toBe(expectedCodeChallenge)
       expect(actualUrl.get("code_challenge_method")).toBe("S256")
 
-      expect(utils.createCodeChallenge.calls.length).toEqual(1)
-      expect(utils.createCodeChallenge.calls[0].arguments[0]).toBe(expectedCodeVerifier)
+      expect(createCodeChallengeSpy.calls.length).toEqual(1)
+      expect(createCodeChallengeSpy.calls[0].arguments[0]).toBe(expectedCodeVerifier)
 
       // The code_verifier should be stored to be able to send in
       // on the TokenUrl call
       expect(authConfig.auth.codeVerifier).toBe(expectedCodeVerifier)
+
+      // Restore spies
+      windowOpenSpy.restore()
+      generateCodeVerifierSpy.restore()
+      createCodeChallengeSpy.restore()
     })    
   })
 })
