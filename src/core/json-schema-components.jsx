7d638c9675515b1929ca6685c49843211987d588
Roman Melnyk <r.melnyk@smartrockstars.com>
2019-11-16 01:41:56 +0200

    #1515 Fix for array of enums doesn't allow you to specify order. Use not used property uniqueItems. If uniqueItems === true than render regular multiselect otherwise list of single select.

--- src/core/json-schema-components.jsx
+++ src/core/json-schema-components.jsx
@@ -163,7 +163,7 @@ export class JsonSchema_array extends PureComponent {
     let enumValue = itemSchema["enum"]
     let value = this.state.value
 
-    if ( enumValue ) {
+    if ( enumValue && itemSchema["uniqueItems"]) {
       const Select = getComponent("Select")
       return (<Select className={ errors.length ? "invalid" : ""}
                       title={ errors.length ? errors : ""}
@@ -186,7 +186,7 @@ export class JsonSchema_array extends PureComponent {
             }
           return (
             <div key={i} className="json-schema-form-item">
-              <JsonSchemaForm 
+              <JsonSchemaForm
                 fn={fn}
                 getComponent={getComponent}
                 value={item}
