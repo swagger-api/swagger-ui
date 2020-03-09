b6f0a04b0b944c2bab217ab9fc1cfce7b28e7be5
Timothy Lai <timothy.lai@gmail.com>
2020-03-09 13:27:11 -0700

    fix: enable online validator badge, without flag

--- src/core/plugins/oas3/wrap-components/online-validator-badge.js
+++ src/core/plugins/oas3/wrap-components/online-validator-badge.js
@@ -1,24 +1,5 @@
 import { OAS3ComponentWrapFactory } from "../helpers"
 import OnlineValidatorBadge from "core/components/online-validator-badge"
 
-// We're disabling the Online Validator Badge until the online validator
-// can handle OAS3 specs.
-// export default OAS3ComponentWrapFactory(() => null)
-// OAS3 spec is now supported. Return OnlineValidatorBadge.
-// export default OAS3ComponentWrapFactory(OnlineValidatorBadge)
-
-
-const VALIDATOR_CAN_HANDLE_OAS3_SPECS = true
-
-export const handleFlag = (wrapped) => {
-  if (VALIDATOR_CAN_HANDLE_OAS3_SPECS) {
-    return (
-      wrapped(OnlineValidatorBadge)
-    )
-  }
-  return (
-    // renders but console error: setState(...): Can only update a mounted or mounting component. This usually means you called setState() on an unmounted component. This is a no-op. Please check the code for the ValidatorImage component.
-    wrapped(() => null)
-  )
-}
-export default handleFlag(OAS3ComponentWrapFactory)
+// OAS3 spec is now supported by the online validator.
+export default OAS3ComponentWrapFactory(OnlineValidatorBadge)
