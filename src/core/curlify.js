96c7b4cbf5e7208e74e18a6cf2d9a0260756914b
Tim Lai <tim-lai@users.noreply.github.com>
2020-05-13 16:59:37 -0700

    fix: curl array support within multipart/form-data (#3838) (#5999)
    ft: utils.createObjWithHashedKeys
    ft: curlify.extractKey
    test: curlify with array representation

--- src/core/curlify.js
+++ src/core/curlify.js
@@ -1,5 +1,19 @@
 import win from "./window"
 
+/**
+ * if duplicate key name existed from FormData entries,
+ * we mutated the key name by appending a hashIdx
+ * @param {String} k - possibly mutated key name
+ * @return {String} - src key name
+ */
+const extractKey = (k) => {
+  const hashIdx = "_**[]"
+  if (k.indexOf(hashIdx) < 0) {
+    return k
+  }
+  return k.split(hashIdx)[0].trim()
+}
+
 export default function curl( request ){
   let curlified = []
   let type = ""
@@ -21,11 +35,12 @@ export default function curl( request ){
 
     if(type === "multipart/form-data" && request.get("method") === "POST") {
       for( let [ k,v ] of request.get("body").entrySeq()) {
+        let extractedKey = extractKey(k)
         curlified.push( "-F" )
         if (v instanceof win.File) {
-          curlified.push( `"${k}=@${v.name}${v.type ? `;type=${v.type}` : ""}"` )
+          curlified.push(`"${extractedKey}=@${v.name}${v.type ? `;type=${v.type}` : ""}"` )
         } else {
-          curlified.push( `"${k}=${v}"` )
+          curlified.push(`"${extractedKey}=${v}"` )
         }
       }
     } else {
