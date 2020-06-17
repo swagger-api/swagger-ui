b43e2e25ed4f5adac5e30b1834b2d4e44cc0d5cf
AdrieanKhisbe <adriean.khisbe@live.fr>
2020-06-17 22:54:37 +0200

    Rework curl clipboard to be similar to #5278 :clipboard:

--- src/core/components/curl.jsx
+++ src/core/components/curl.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import curlify from "core/curlify"
-import {copyToClipboard} from "core/copy-to-clipboard"
+import { CopyToClipboard } from "react-copy-to-clipboard"
 import {SyntaxHighlighter, styles} from "core/syntax-highlighting"
 
 export default class Curl extends React.Component {
@@ -9,19 +9,18 @@ export default class Curl extends React.Component {
     request: PropTypes.object.isRequired
   }
 
-  copy(curlCommand) {
-    return () => copyToClipboard(curlCommand)
-  }
-
   render() {
     let { request } = this.props
     let curl = curlify(request)
 
     return (
-      <div>
-        <h4>Curl <i onClick={this.copy(curl)}>(copyCommand)</i></h4>
+      <div className="curl-command">
+        <h4>Curl</h4>
+        <div className="copy-to-clipboard">
+            <CopyToClipboard text={curl}><button/></CopyToClipboard>
+        </div>
         <div>
-          <SyntaxHighlighter language="bash" className="curl" style={styles.agate}>{curl}</SyntaxHighlighter>
+          <SyntaxHighlighter language="bash" className="curl microlight" style={styles.agate}>{curl}</SyntaxHighlighter>
         </div>
       </div>
     )
