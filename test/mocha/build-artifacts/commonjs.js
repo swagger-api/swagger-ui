5449900befeddccc2ee0cd5ba9188fa8823992f3
Timothy Lai <timothy.lai@gmail.com>
2020-08-07 17:07:40 -0700

    wip: test for build artifacts

---
+++ test/mocha/build-artifacts/commonjs.js
@@ -0,0 +1,9 @@
+// import expect from "expect"
+// // commonjs import is here by design to test native node.js require function on artifact
+// const SwaggerUI = require("../../../lib/commonjs")
+
+// describe("babel commonjs artifact", () => {
+//   it("should export a function", () => {
+//     expect(SwaggerUI).toBeA(Function)
+//   })
+// })
---
+++ test/mocha/build-artifacts/es.js
@@ -0,0 +1,9 @@
+// import "@babel/runtime-corejs2/regenerator"
+// import expect from "expect"
+// import SwaggerUI from "../../../es"
+
+// describe("babel es artifact", () => {
+//   it("should export a function", () => {
+//     expect(SwaggerUI).toBeA(Function)
+//   })
+// })
