d8822d40402f549ef8fb6ced45cc6c227a5abd1f
Timothy Lai <timothy.lai@gmail.com>
2020-08-31 10:57:48 -0700

    config(jest): update testMatch to include jsx files

--- config/jest/jest.unit.config.js
+++ config/jest/jest.unit.config.js
@@ -3,8 +3,14 @@ const path = require('path');
 module.exports = {
   rootDir: path.join(__dirname, '..', '..'),
   testEnvironment: 'jsdom',
-  testMatch: ['**/test/mocha/*.js', '**/test/mocha/**/*.js'],
-  // testMatch: ['**/test/mocha/core/curlify.js'],
+  testMatch: [
+    '**/test/mocha/*.js',
+    '**/test/mocha/**/*.js',
+    '**/test/mocha/core/system/*.jsx',
+    '**/test/mocha/xss/*.jsx'
+  ],
+  // testMatch: ['**/test/mocha/core/plugins/auth/selectors.js'],
+  // testMatch: [ '**/test/mocha/xss/**/*.jsx'],
   setupFilesAfterEnv: ['<rootDir>/test/mocha/setup.js'],
   testPathIgnorePatterns: [
     '<rootDir>/node_modules/',
