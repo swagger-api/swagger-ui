c81144a7d2cadf6a330afe95ad2f59796fba0f03
Timothy Lai <timothy.lai@gmail.com>
2020-10-12 15:50:30 -0700

    feat(oas3): reset requestBodyValue action/reducer

--- src/core/plugins/oas3/reducers.js
+++ src/core/plugins/oas3/reducers.js
@@ -10,6 +10,7 @@ import {
   UPDATE_RESPONSE_CONTENT_TYPE,
   SET_REQUEST_BODY_VALIDATE_ERROR,
   CLEAR_REQUEST_BODY_VALIDATE_ERROR,
+  RESET_REQUEST_BODY_VALUE,
 } from "./actions"
 
 export default {
@@ -94,4 +95,8 @@ export default {
       }, bodyValues)
     })
   },
+  [RESET_REQUEST_BODY_VALUE]: (state, { payload: { pathMethod }}) => {
+    let [path, method] = pathMethod
+    return state.setIn(["requestData", path, method, "bodyValue"], Map())
+  }
 }
