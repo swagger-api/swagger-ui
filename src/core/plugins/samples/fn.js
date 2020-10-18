2daf1c68a1b23c7132dcd354090ea6549a946bcf
mathis-m <mathis.michel@outlook.de>
2020-10-18 16:29:26 +0200

    xml should override

--- src/core/plugins/samples/fn.js
+++ src/core/plugins/samples/fn.js
@@ -30,15 +30,14 @@ const primitive = (schema) => {
   return "Unknown Type: " + schema.type
 }
 
+// do a couple of quick sanity tests to ensure the value
+// looks like a $$ref that swagger-client generates.
+const sanitizeRef = (value) => deeplyStripKey(value, "$$ref", (val) =>
+  typeof val === "string" && val.indexOf("#") > -1)
 
 export const sampleFromSchema = (schema, config={}, exampleOverride = undefined) => {
   let { type, example, properties, additionalProperties, items } = objectify(schema)
   let { includeReadOnly, includeWriteOnly } = config
-
-  // do a couple of quick sanity tests to ensure the value
-  // looks like a $$ref that swagger-client generates.
-  const sanitizeRef = (value) => deeplyStripKey(value, "$$ref", (val) =>
-    typeof val === "string" && val.indexOf("#") > -1)
 
   if(exampleOverride !== undefined) {
     return sanitizeRef(exampleOverride)
@@ -135,6 +134,13 @@ export const sampleXmlFromSchema = (schema, config={}, exampleOverride = undefin
   let enumValue = objectifySchema.enum
   let displayName, value
 
+  if(exampleOverride !== undefined) {
+    return sanitizeRef(exampleOverride)
+  }
+  if(example !== undefined) {
+    return sanitizeRef(example)
+  }
+
   if(!type) {
     if(properties || additionalProperties) {
       type = "object"
@@ -161,12 +167,7 @@ export const sampleXmlFromSchema = (schema, config={}, exampleOverride = undefin
 
       if (xml.wrapped) {
         res[displayName] = []
-        if (Array.isArray(example)) {
-          example.forEach((v)=>{
-            items.example = v
-            res[displayName].push(sampleXmlFromSchema(items, config))
-          })
-        } else if (Array.isArray(defaultValue)) {
+        if (Array.isArray(defaultValue)) {
           defaultValue.forEach((v)=>{
             items.default = v
             res[displayName].push(sampleXmlFromSchema(items, config))
@@ -183,13 +184,7 @@ export const sampleXmlFromSchema = (schema, config={}, exampleOverride = undefin
 
       let _res = []
 
-      if (Array.isArray(example)) {
-        example.forEach((v)=>{
-          items.example = v
-          _res.push(sampleXmlFromSchema(items, config))
-        })
-        return _res
-      } else if (Array.isArray(defaultValue)) {
+      if (Array.isArray(defaultValue)) {
         defaultValue.forEach((v)=>{
           items.default = v
           _res.push(sampleXmlFromSchema(items, config))
@@ -204,7 +199,6 @@ export const sampleXmlFromSchema = (schema, config={}, exampleOverride = undefin
   if (type === "object") {
     let props = objectify(properties)
     res[displayName] = []
-    example = example || {}
 
     for (let propName in props) {
       if (!props.hasOwnProperty(propName)) {
@@ -224,13 +218,10 @@ export const sampleXmlFromSchema = (schema, config={}, exampleOverride = undefin
         let attrExample = props[propName].example
         let attrDefault = props[propName].default
         _attr[props[propName].xml.name || propName] = attrExample!== undefined && attrExample
-          || example[propName] !== undefined && example[propName] || attrDefault !== undefined && attrDefault
+          || attrDefault !== undefined && attrDefault
           || enumAttrVal || primitive(props[propName])
       } else {
         props[propName].xml.name = props[propName].xml.name || propName
-        if(props[propName].example === undefined && example[propName] !== undefined) {
-          props[propName].example = example[propName]
-        }
         let t = sampleXmlFromSchema(props[propName])
         if (Array.isArray(t)) {
           res[displayName] = res[displayName].concat(t)
@@ -253,9 +244,7 @@ export const sampleXmlFromSchema = (schema, config={}, exampleOverride = undefin
     return res
   }
 
-  if (example !== undefined) {
-    value = example
-  } else if (defaultValue !== undefined) {
+  if (defaultValue !== undefined) {
     //display example if exists
     value = defaultValue
   } else if (Array.isArray(enumValue)) {
