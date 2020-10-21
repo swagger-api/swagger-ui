c21cc6ac26edcd0dd79453228deda77681b8d605
Illia Kliuiev <72439852+kliuiev-io@users.noreply.github.com>
2020-10-21 12:14:08 +0300

    feat: added easygenerator repos dropdowns (#1)
    * Начал работать над EgTopbar
    
    - Добавил необходимые поля в state
    - Сделал теоретическую загрузку конфига
    - Частично сделал внутреннюю логику
    
    * Заменил Topbar на EgTopbar
    
    * Теоретически готовый компонент
    
    * Исправил опечатку в задании propTypes
    
    * Удалён некоторый неиспользуемый код
    
    * Закинул тестовый конфиг
    
    * Успешная подгрузка списка репозиториев из конфига
    
    * Исправил ошибки
    
    * Убрал неиспользуемый код
    
    * Финальные правки
    
    - Исправлена загрузка Pet Store при запуске страницы
    - Убраны неиспользуемые поля в state
    
    * Подготовка к сборке
    
    - eg_config -> egConfig
    
    * Собрал билд
    
    * Удалён текущий билд
    
    * Перебилдил

---
+++ src/plugins/topbar/egtopbar.jsx
@@ -0,0 +1,135 @@
+import React, { cloneElement } from "react"
+import PropTypes from "prop-types"
+
+import Logo from "./logo_small.svg"
+
+export default class EgTopbar extends React.Component {
+
+  static propTypes = {
+    layoutActions: PropTypes.object.isRequired
+  }
+
+  constructor(props, context) {
+    super(props, context)
+    this.state = {
+        urls: {},
+        currentRepository: null,
+        currentBranch: null,
+    }
+  }
+
+  onRepositoryChange = e => {
+    const repository = e.target.value
+
+    this.setState({
+      currentRepository: repository,
+      currentBranch: this.state.urls[repository][0]
+    }, this.loadLocalSpec)
+  }
+
+  onBranchChange = e => {
+    const branch = e.target.value
+
+    this.setState({
+      currentBranch: branch,
+    }, this.loadLocalSpec)
+  }
+
+  fetchUrls = async () => {
+      const egConfig = this.props.getConfigs().egConfig
+
+      const request = await fetch(egConfig)
+
+      if (!request.ok) throw new Error(request.status)
+
+      const urls = await request.json()
+
+      this.setState({urls})
+  }
+
+  loadSpec = (url) => {
+    this.props.specActions.updateUrl(url)
+    this.props.specActions.download(url)
+  }
+
+  loadLocalSpec = () => {
+    const {currentRepository, currentBranch} = this.state
+
+    this.loadSpec(`https://test-swagger-api.s3.eu-central-1.amazonaws.com/${currentRepository}/${currentBranch}/swagger.json`)
+  }
+
+  async componentDidMount() {
+    await this.fetchUrls()
+
+    const {urls} = this.state
+
+    const [defaultRepository] = Object.keys(urls)
+
+    this.setState({
+      currentRepository: defaultRepository,
+      currentBranch: urls[defaultRepository][0]
+    }, this.loadLocalSpec)
+  }
+
+  render() {
+    let { getComponent, specSelectors } = this.props
+    const Link = getComponent("Link")
+
+    let isLoading = specSelectors.loadingStatus() === "loading"
+    let isFailed = specSelectors.loadingStatus() === "failed"
+
+    const classNames = ["download-url-input"]
+    if (isFailed) classNames.push("failed")
+    if (isLoading) classNames.push("loading")
+    
+    const { urls, currentBranch, currentRepository } = this.state
+
+    if (!Object.keys(urls).length) return <span>No urls found.</span>
+    if (!currentRepository || !currentBranch) return <span>Not initialized.</span>
+
+    let control = []
+    let formOnSubmit = null
+
+
+    const repos = Object.keys(urls).map(repo => <option key={repo} value={repo}>{repo}</option>)
+    const branches = urls[currentRepository].map(branch => <option key={branch} value={branch}>{branch}</option>)
+
+    control.push(
+      <label className="select-label" htmlFor="select-repo"><span>Select a repository</span>
+        <select id="select-repo" disabled={isLoading} onChange={ this.onRepositoryChange } value={currentRepository}>
+          {repos}
+        </select>
+      </label>
+    )
+
+    control.push(
+      <label className="select-label" htmlFor="select-branch"><span>Select a branch</span>
+        <select id="select-branch" disabled={isLoading} onChange={ this.onBranchChange } value={currentBranch}>
+          {branches}
+        </select>
+      </label>
+    )
+
+    return (
+      <div className="topbar">
+        <div className="wrapper">
+          <div className="topbar-wrapper">
+            <Link>
+              <img height="40" src={ Logo } alt="Swagger UI (Easygenerator)"/>
+            </Link>
+            <form className="download-url-wrapper" onSubmit={formOnSubmit}>
+              {control.map((el, i) => cloneElement(el, { key: i }))}
+            </form>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+EgTopbar.propTypes = {
+  specSelectors: PropTypes.object.isRequired,
+  specActions: PropTypes.object.isRequired,
+  getComponent: PropTypes.func.isRequired,
+  getConfigs: PropTypes.func.isRequired
+}
