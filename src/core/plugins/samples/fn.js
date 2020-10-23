fcbe7f9498fd19592502fe608871910b509c1846
mathis-m <mathis.michel@outlook.de>
2020-10-23 14:20:27 +0200

    merge json and xml logic because it isn't that different

--- src/core/plugins/samples/fn.js
+++ src/core/plugins/samples/fn.js
@@ -1,7 +1,6 @@
 import { objectify, isFunc, normalizeArray, deeplyStripKey } from "core/utils"
 import XML from "@kyleshockey/xml"
 import memoizee from "memoizee"
-import deepAssign from "@kyleshockey/object-assign-deep"
 
 const primitives = {
   "string": () => "string",
@@ -35,19 +34,49 @@ const primitive = (schema) => {
 const sanitizeRef = (value) => deeplyStripKey(value, "$$ref", (val) =>
   typeof val === "string" && val.indexOf("#") > -1)
 
-export const sampleFromSchema = (schema, config={}, exampleOverride = undefined) => {
+const liftSampleHelper = (oldSchema, target) => {
+  if(target.example === undefined && oldSchema.example !== undefined) {
+    target.example = oldSchema.example
+  }
+  if(target.default === undefined && oldSchema.default !== undefined) {
+    target.default = oldSchema.default
+  }
+  if(target.enum === undefined && oldSchema.enum !== undefined) {
+    target.enum = oldSchema.enum
+  }
+}
+
+export const sampleFromSchemaGeneric = (schema, config={}, exampleOverride = undefined, respectXML = false) => {
   let { type, example, properties, additionalProperties, items } = objectify(schema)
   let { includeReadOnly, includeWriteOnly } = config
+  let { xml } = schema
+  let _attr = {}
+  xml = xml || {}
+  let { name, prefix, namespace } = xml
+  let displayName
 
-  if(exampleOverride !== undefined) {
-    return sanitizeRef(exampleOverride)
-  }
-  if(example !== undefined) {
-    return sanitizeRef(example)
+  // set xml naming and attributes
+  if(respectXML) {
+    name = name || "notagname"
+    // add prefix to name if exists
+    displayName = (prefix ? prefix + ":" : "") + name
+    if ( namespace ) {
+      //add prefix to namespace if exists
+      let namespacePrefix = prefix ? ( "xmlns:" + prefix ) : "xmlns"
+      _attr[namespacePrefix] = namespace
+    }
   }
 
-  if(!type) {
-    if(properties) {
+  // init response sample obj
+  let res = {}
+  // init xml default response sample obj
+  if(respectXML) {
+    res[displayName] = []
+  }
+
+  // try recover missing type
+  if(schema && !type) {
+    if(properties || additionalProperties) {
       type = "object"
     } else if(items) {
       type = "array"
@@ -56,61 +85,207 @@ export const sampleFromSchema = (schema, config={}, exampleOverride = undefined)
     }
   }
 
+  // add to result helper init for xml or json
+  let props = objectify(properties)
+  let addResult
+  if(respectXML) {
+    addResult = (propName, overrideE = undefined) => {
+      if(schema) {
+        // case it is an xml attribute
+        props[propName].xml = props[propName].xml || {}
+
+        if (props[propName].xml.attribute) {
+          let enumAttrVal = Array.isArray(props[propName].enum) && props[propName].enum[0]
+          let attrExample = props[propName].example
+          let attrDefault = props[propName].default
+          _attr[props[propName].xml.name || propName] = attrExample!== undefined && attrExample
+            || attrDefault !== undefined && attrDefault
+            || enumAttrVal
+            || primitive(props[propName])
+          return
+        }
+        props[propName].xml.name = props[propName].xml.name || propName
+      }
+
+      let t = sampleFromSchemaGeneric(schema && props[propName] || undefined, config, overrideE, respectXML)
+      if (Array.isArray(t)) {
+        res[displayName] = res[displayName].concat(t)
+      } else {
+        res[displayName].push(t)
+      }
+    }
+  } else {
+    addResult = (propName, overrideE) =>
+      res[propName] = sampleFromSchemaGeneric(props[propName], config, overrideE, respectXML)
+  }
+
+  // check for example override
+  if(exampleOverride !== undefined || example !== undefined || schema.default !== undefined) {
+    let sample = exampleOverride !== undefined
+      ? sanitizeRef(exampleOverride)
+      : sanitizeRef(example !== undefined ? example : schema.default)
+
+    // if json just return
+    if(!respectXML) {
+      return sample
+    }
+    // recover missing type
+    if(!schema) {
+      type = Array.isArray(sample) ? "array" : typeof sample
+    }
+    // generate xml sample recursively for array case
+    if(type === "array") {
+      if (!Array.isArray(sample)) {
+        sample = [sample]
+      }
+      const itemSchema = schema
+        ? schema.items
+        : undefined
+      if(itemSchema) {
+        itemSchema.xml = itemSchema.xml || xml || {}
+        itemSchema.xml.name = itemSchema.xml.name || xml.name
+      }
+      const itemSamples = sample
+        .map(s => sampleFromSchemaGeneric(itemSchema, config, s, respectXML))
+      if(xml.wrapped) {
+        res[displayName] = itemSamples
+        if (_attr) {
+          res[displayName].push({_attr: _attr})
+        }
+      }
+      else {
+        res = itemSamples
+      }
+      return res
+    }
+
+    // generate xml sample recursively for object case
+    if(type === "object") {
+      for (let propName in sample) {
+        if (!sample.hasOwnProperty(propName)) {
+          continue
+        }
+        if (schema && props[propName] && props[propName].readOnly && !includeReadOnly) {
+          continue
+        }
+        if (schema && props[propName] && props[propName].writeOnly && !includeWriteOnly) {
+          continue
+        }
+        if (schema && props[propName] && props[propName].xml && props[propName].xml.attribute) {
+          _attr[props[propName].xml.name || propName] = example[propName]
+          continue
+        }
+        addResult(propName, sample[propName])
+      }
+      if (_attr) {
+        res[displayName].push({_attr: _attr})
+      }
+
+      return res
+    }
+
+    res[displayName] = _attr ? [{_attr: _attr}, sample] : sample
+    return res
+  }
+
+
+
   if(type === "object") {
-    let props = objectify(properties)
-    let obj = {}
-    for (let name in props) {
-      if (!props.hasOwnProperty(name)) {
+    for (let propName in props) {
+      if (!props.hasOwnProperty(propName)) {
         continue
       }
-      if ( props[name] && props[name].deprecated ) {
+      if ( props[propName] && props[propName].deprecated ) {
         continue
       }
-      if ( props[name] && props[name].readOnly && !includeReadOnly ) {
+      if ( props[propName] && props[propName].readOnly && !includeReadOnly ) {
         continue
       }
-      if ( props[name] && props[name].writeOnly && !includeWriteOnly ) {
+      if ( props[propName] && props[propName].writeOnly && !includeWriteOnly ) {
         continue
       }
-      obj[name] = sampleFromSchema(props[name], config)
+      addResult(propName)
     }
 
     if ( additionalProperties === true ) {
-      obj.additionalProp1 = {}
+      if(respectXML) {
+        res[displayName].push({additionalProp: "Anything can be here"})
+      } else {
+        res.additionalProp1 = {}
+      }
     } else if ( additionalProperties ) {
-      let additionalProps = objectify(additionalProperties)
-      let additionalPropVal = sampleFromSchema(additionalProps, config)
+      const additionalProps = objectify(additionalProperties)
+      const additionalPropSample = sampleFromSchemaGeneric(additionalProps, config, undefined, respectXML)
 
-      for (let i = 1; i < 4; i++) {
-        obj["additionalProp" + i] = additionalPropVal
+
+      if(respectXML && additionalProps.xml && additionalProps.xml.name && additionalProps.xml.name !== "notagname")
+      {
+        res[displayName].push(additionalPropSample)
+      } else {
+        for (let i = 1; i < 4; i++) {
+          if(respectXML) {
+            const temp = {}
+            temp["additionalProp" + i] = additionalPropSample["notagname"]
+            res[displayName].push(temp)
+          } else {
+            res["additionalProp" + i] = additionalPropSample
+          }
+        }
       }
     }
-    return obj
+    if (respectXML && _attr) {
+      res[displayName].push({_attr: _attr})
+    }
+
+    return res
   }
 
   if(type === "array") {
+    let sampleArray
+    if(respectXML) {
+      items.xml = items.xml || xml || {}
+      items.xml.name = items.xml.name || xml.name
+    }
+
     if(Array.isArray(items.anyOf)) {
-      return items.anyOf.map(i => sampleFromSchema(i, config))
+      liftSampleHelper(items, items.anyOf)
+      sampleArray = items.anyOf.map(i => sampleFromSchemaGeneric(i, config, undefined, respectXML))
+    } else if(Array.isArray(items.oneOf)) {
+      liftSampleHelper(items, items.oneOf)
+      sampleArray = items.oneOf.map(i => sampleFromSchemaGeneric(i, config, undefined, respectXML))
+    } else if(!respectXML || respectXML && xml.wrapped) {
+      sampleArray = [sampleFromSchemaGeneric(items, config, undefined, respectXML)]
+    } else {
+      return sampleFromSchemaGeneric(items, config, undefined, respectXML)
     }
-
-    if(Array.isArray(items.oneOf)) {
-      return items.oneOf.map(i => sampleFromSchema(i, config))
+    if(respectXML && xml.wrapped) {
+      res[displayName] = sampleArray
+      if (_attr) {
+        res[displayName].push({_attr: _attr})
+      }
+      return res
     }
-
-    return [ sampleFromSchema(items, config) ]
+    return sampleArray
   }
 
-  if(schema["enum"]) {
-    if(schema["default"])
-      return schema["default"]
-    return normalizeArray(schema["enum"])[0]
+  let value
+  if (Array.isArray(schema.enum)) {
+    //display enum first value
+    value = normalizeArray(schema.enum)[0]
+  } else {
+    // display schema default
+    value = primitive(schema)
   }
-
   if (type === "file") {
     return
   }
 
-  return primitive(schema)
+  if(respectXML) {
+    res[displayName] = _attr ? [{_attr: _attr}, value] : value
+    return res
+  }
+
+  return value
 }
 
 export const inferSchema = (thing) => {
@@ -124,162 +299,15 @@ export const inferSchema = (thing) => {
   return thing // Hopefully this will have something schema like in it... `type` for example
 }
 
-
-export const sampleXmlFromSchema = (schema, config={}) => {
-  let objectifySchema = deepAssign({}, objectify(schema))
-  let { type, properties, additionalProperties, items, example } = objectifySchema
-  let { includeReadOnly, includeWriteOnly } = config
-  let defaultValue = objectifySchema.default
-  let res = {}
-  let _attr = {}
-  let { xml } = schema
-  let { name, prefix, namespace } = xml
-  let enumValue = objectifySchema.enum
-  let displayName, value
-
-  if(!type) {
-    if(properties || additionalProperties) {
-      type = "object"
-    } else if(items) {
-      type = "array"
-    } else {
-      return
-    }
-  }
-
-  name = name || "notagname"
-  // add prefix to name if exists
-  displayName = (prefix ? prefix + ":" : "") + name
-  if ( namespace ) {
-    //add prefix to namespace if exists
-    let namespacePrefix = prefix ? ( "xmlns:" + prefix ) : "xmlns"
-    _attr[namespacePrefix] = namespace
-  }
-
-  if (type === "array") {
-    if (items) {
-      items.xml = items.xml || xml || {}
-      items.xml.name = items.xml.name || xml.name
-
-      if (xml.wrapped) {
-        res[displayName] = []
-        if (Array.isArray(example)) {
-          example.forEach((v)=>{
-            items.example = v
-            res[displayName].push(sampleXmlFromSchema(items, config))
-          })
-        } else if (Array.isArray(defaultValue)) {
-          defaultValue.forEach((v)=>{
-            items.default = v
-            res[displayName].push(sampleXmlFromSchema(items, config))
-          })
-        } else {
-          res[displayName] = [sampleXmlFromSchema(items, config)]
-        }
-
-        if (_attr) {
-          res[displayName].push({_attr: _attr})
-        }
-        return res
-      }
-
-      let _res = []
-
-      if (Array.isArray(example)) {
-        example.forEach((v)=>{
-          items.example = v
-          _res.push(sampleXmlFromSchema(items, config))
-        })
-        return _res
-      } else if (Array.isArray(defaultValue)) {
-        defaultValue.forEach((v)=>{
-          items.default = v
-          _res.push(sampleXmlFromSchema(items, config))
-        })
-        return _res
-      }
-
-      return sampleXmlFromSchema(items, config)
-    }
-  }
-
-  if (type === "object") {
-    let props = objectify(properties)
-    res[displayName] = []
-    example = example || {}
-
-    for (let propName in props) {
-      if (!props.hasOwnProperty(propName)) {
-        continue
-      }
-      if ( props[propName].readOnly && !includeReadOnly ) {
-        continue
-      }
-      if ( props[propName].writeOnly && !includeWriteOnly ) {
-        continue
-      }
-
-      props[propName].xml = props[propName].xml || {}
-
-      if (props[propName].xml.attribute) {
-        let enumAttrVal = Array.isArray(props[propName].enum) && props[propName].enum[0]
-        let attrExample = props[propName].example
-        let attrDefault = props[propName].default
-        _attr[props[propName].xml.name || propName] = attrExample!== undefined && attrExample
-          || example[propName] !== undefined && example[propName] || attrDefault !== undefined && attrDefault
-          || enumAttrVal || primitive(props[propName])
-      } else {
-        props[propName].xml.name = props[propName].xml.name || propName
-        if(props[propName].example === undefined && example[propName] !== undefined) {
-          props[propName].example = example[propName]
-        }
-        let t = sampleXmlFromSchema(props[propName])
-        if (Array.isArray(t)) {
-          res[displayName] = res[displayName].concat(t)
-        } else {
-          res[displayName].push(t)
-        }
-
-      }
-    }
-
-    if (additionalProperties === true) {
-      res[displayName].push({additionalProp: "Anything can be here"})
-    } else if (additionalProperties) {
-      res[displayName].push({additionalProp: primitive(additionalProperties)})
-    }
-
-    if (_attr) {
-      res[displayName].push({_attr: _attr})
-    }
-    return res
-  }
-
-  if (example !== undefined) {
-    value = example
-  } else if (defaultValue !== undefined) {
-    //display example if exists
-    value = defaultValue
-  } else if (Array.isArray(enumValue)) {
-    //display enum first value
-    value = enumValue[0]
-  } else {
-    //set default value
-    value = primitive(schema)
-  }
-
-  res[displayName] = _attr ? [{_attr: _attr}, value] : value
-
-  return res
-}
-
-export function createXMLExample(schema, config) {
-  let json = sampleXmlFromSchema(schema, config)
+export const createXMLExample = (schema, config={}, o) => {
+  const json = sampleFromSchemaGeneric(schema, config, o, true)
   if (!json) { return }
 
   return XML(json, { declaration: true, indent: "\t" })
 }
-
+export const sampleFromSchema = (schema, config={}, o) => {
+  return sampleFromSchemaGeneric(schema, config, o, false)
+}
 export const memoizedCreateXMLExample = memoizee(createXMLExample)
 
 export const memoizedSampleFromSchema = memoizee(sampleFromSchema)
