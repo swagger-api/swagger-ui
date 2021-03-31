bf6114af452e2eded4ff313b0e2d7471ebfc2ba8
Timothy Lai <timothy.lai@gmail.com>
2021-03-31 10:38:01 -0700

    feat: markdown sanitization of form tag

--- test/unit/xss/markdown-script-sanitization.jsx
+++ test/unit/xss/markdown-script-sanitization.jsx
@@ -16,6 +16,12 @@ describe("Markdown Script Sanitization", function() {
       const el = render(<Markdown source={str} />)
       expect(el.html()).toEqual(`<div class="markdown"><p><img src="x"></p>\n</div>`)
     })
+
+    it("sanitizes <form> elements", function() {
+      const str = `"<form action='https://do.not.use.url/fake' method='post' action='java'><input type='email' id='email' placeholder='Email-address' name='email' value=''><button type='submit'>Login</button>"`
+      const el = render(<Markdown source={str} />)
+      expect(el.html()).toEqual(`<div class="markdown"><p>&quot;</p><input value name="email" placeholder="Email-address" id="email" type="email"><button type="submit">Login</button>&quot;<p></p>\n</div>`)
+    })
   })
 
   describe("OAS 3", function() {
@@ -29,6 +35,12 @@ describe("Markdown Script Sanitization", function() {
       const str = `<img src=x onerror="alert('img-in-description')">`
       const el = render(<OAS3Markdown source={str} />)
       expect(el.html()).toEqual(`<div class="renderedMarkdown"><p><img src="x"></p></div>`)
+    })
+
+    it("sanitizes <form> elements", function () {
+      const str = `"<form action='https://do.not.use.url/fake' method='post' action='java'><input type='email' id='email' placeholder='Email-address' name='email' value=''><button type='submit'>Login</button>"`
+      const el = render(<OAS3Markdown source={str} />)
+      expect(el.html()).toEqual(`<div class="renderedMarkdown"><p>&quot;</p><input value name="email" placeholder="Email-address" id="email" type="email"><button type="submit">Login</button>&quot;<p></p></div>`)
     })
   })
 })
