a54c40c3ad6b7def849812895208238dd7e447d2
Calvin Gonzalez <calvin.gonzalez@oddball.io>
2021-08-16 11:02:11 -0400

    [BUG-7463]: Response tabs not keyboard navigable Update tabs to use <button> elements instead of <a> Add aria roles for tablist, tabs, and tabpanel Add aria attributes for additional a11y compliance and screen reader accessibility

--- src/core/components/model-example.jsx
+++ src/core/components/model-example.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import ImPropTypes from "react-immutable-proptypes"
+import cx from "classnames"
 
 export default class ModelExample extends React.Component {
   static propTypes = {
@@ -31,11 +32,11 @@ export default class ModelExample extends React.Component {
     }
 
     this.state = {
-      activeTab: activeTab
+      activeTab,
     }
   }
 
-  activeTab =( e ) => {
+  activeTab = ( e ) => {
     let { target : { dataset : { name } } } = e
 
     this.setState({
@@ -61,39 +62,62 @@ export default class ModelExample extends React.Component {
 
     let isOAS3 = specSelectors.isOAS3()
 
-    return <div className="model-example">
-      <ul className="tab">
-        <li className={ "tabitem" + ( this.state.activeTab === "example" ? " active" : "") }>
-          <a className="tablinks" data-name="example" onClick={ this.activeTab }>{isExecute ? "Edit Value" : "Example Value"}</a>
-        </li>
-        { schema ? <li className={ "tabitem" + ( this.state.activeTab === "model" ? " active" : "") }>
-          <a className={ "tablinks" + ( isExecute ? " inactive" : "" )} data-name="model" onClick={ this.activeTab }>
-            {isOAS3 ? "Schema" : "Model" }
-          </a>
-        </li> : null }
-      </ul>
-      <div>
-        {
-          this.state.activeTab === "example" ? (
-            example ? example : (
+    return (
+      <div className="model-example">
+        <ul className="tab" role="tablist">
+          <li className={cx("tabitem", { "active": this.state.activeTab === "example" })} role="presentation">
+            <button
+              aria-controls="examplePanel"
+              aria-selected={this.state.activeTab === "example"}
+              className="tablinks"
+              data-name="example"
+              id="exampleTab"
+              onClick={ this.activeTab }
+              role="tab"
+            >
+              {isExecute ? "Edit Value" : "Example Value"}
+            </button>
+          </li>
+          { schema && (
+            <li className={cx("tabitem", { "active": this.state.activeTab === "model" })} role="presentation">
+              <button
+                aria-controls={isOAS3 ? "schemaPanel" : "modelPanel" }
+                aria-selected={this.state.activeTab === "model"}
+                className={ "tablinks" + ( isExecute ? " inactive" : "" )}
+                data-name="model"
+                id={isOAS3 ? "schemaTab" : "modelTab" }
+                onClick={ this.activeTab }
+                role="tab"
+              >
+                {isOAS3 ? "Schema" : "Model" }
+              </button>
+            </li>
+          )}
+        </ul>
+        {this.state.activeTab === "example" && (
+          <div id="examplePanel" role="tabpanel" aria-labelledby="exampleTab" aria-hidden={this.state.activeTab === "model"} tabIndex="0">
+            {example ? example : (
               <HighlightCode value="(no example available)" getConfigs={ getConfigs } />
-            )
-          ) : null
-        }
-        {
-          this.state.activeTab === "model" && <ModelWrapper schema={ schema }
-                                                     getComponent={ getComponent }
-                                                     getConfigs={ getConfigs }
-                                                     specSelectors={ specSelectors }
-                                                     expandDepth={ defaultModelExpandDepth }
-                                                     specPath={specPath}
-                                                     includeReadOnly = {includeReadOnly}
-                                                     includeWriteOnly = {includeWriteOnly}/>
+            )}
+          </div>
+        )}
 
-
-        }
+        {this.state.activeTab === "model" && (
+          <div id={isOAS3 ? "schemaPanel" : "modelPanel" } role="tabpanel" aria-labelledby={isOAS3 ? "schemaTab" : "modelTab" } aria-hidden={this.state.activeTab === "example"} tabIndex="0">
+            <ModelWrapper
+              schema={ schema }
+              getComponent={ getComponent }
+              getConfigs={ getConfigs }
+              specSelectors={ specSelectors }
+              expandDepth={ defaultModelExpandDepth }
+              specPath={specPath}
+              includeReadOnly = {includeReadOnly}
+              includeWriteOnly = {includeWriteOnly}
+            />
+          </div>
+        )}
       </div>
-    </div>
+    )
   }
 
 }
