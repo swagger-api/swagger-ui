89ab965714710a4cfda2f6da2ba5be49dcdb07ef
mathis-m <mathis.michel@outlook.de>
2021-09-24 03:30:48 +0200

    feat(react): allow displayRequestDuration via react

--- flavors/swagger-ui-react/index.jsx
+++ flavors/swagger-ui-react/index.jsx
@@ -24,6 +24,7 @@ export default class SwaggerUI extends React.Component {
       defaultModelExpandDepth: this.props.defaultModelExpandDepth,
       displayOperationId: this.props.displayOperationId,
       tryItOutEnabled: this.props.tryItOutEnabled,
+      displayRequestDuration: this.props.displayRequestDuration,
       showMutatedRequest: typeof this.props.showMutatedRequest === "boolean" ? this.props.showMutatedRequest : true,
       deepLinking: typeof this.props.deepLinking === "boolean" ? this.props.deepLinking : false,
     })
@@ -102,7 +103,8 @@ SwaggerUI.propTypes = {
   defaultModelsExpandDepth: PropTypes.number,
   presets: PropTypes.arrayOf(PropTypes.func),
   deepLinking: PropTypes.bool,
-  tryItOutEnabled: PropTypes.bool
+  tryItOutEnabled: PropTypes.bool,
+  displayRequestDuration: PropTypes.bool,
 }
 
 SwaggerUI.defaultProps = {
@@ -112,4 +114,5 @@ SwaggerUI.defaultProps = {
   defaultModelsExpandDepth: 1,
   presets: [],
   deepLinking: false,
+  displayRequestDuration: false,
 }
