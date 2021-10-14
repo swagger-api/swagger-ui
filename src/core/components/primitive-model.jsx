400633e89bf44dc9dc3a6d05ed0d41678f4e0982
twjasa <twjasa@gmail.com>
2021-10-14 17:54:29 -0300

    feat: add modelcollapse to primitive models

--- src/core/components/primitive-model.jsx
+++ src/core/components/primitive-model.jsx
@@ -11,11 +11,12 @@ export default class Primitive extends Component {
     getConfigs: PropTypes.func.isRequired,
     name: PropTypes.string,
     displayName: PropTypes.string,
-    depth: PropTypes.number
+    depth: PropTypes.number,
+    expandDepth: PropTypes.number
   }
 
   render(){
-    let { schema, getComponent, getConfigs, name, displayName, depth } = this.props
+    let { schema, getComponent, getConfigs, name, displayName, depth, expandDepth } = this.props
 
     const { showExtensions } = getConfigs()
 
@@ -37,33 +38,39 @@ export default class Primitive extends Component {
     const Markdown = getComponent("Markdown", true)
     const EnumModel = getComponent("EnumModel")
     const Property = getComponent("Property")
+    const ModelCollapse = getComponent("ModelCollapse")
+    const titleEl = title &&
+      <span className="model-title">
+        <span className="model-title__text">{ title }</span>
+      </span>
 
     return <span className="model">
-      <span className="prop">
-        { name && <span className={`${depth === 1 && "model-title"} prop-name`}>{ title }</span> }
-        <span className="prop-type">{ type }</span>
-        { format && <span className="prop-format">(${format})</span>}
-        {
-          properties.size ? properties.entrySeq().map( ( [ key, v ] ) => <Property key={`${key}-${v}`} propKey={ key } propVal={ v } propClass={ propClass } />) : null
-        }
-        {
-          showExtensions && extensions.size ? extensions.entrySeq().map( ( [ key, v ] ) => <Property key={`${key}-${v}`} propKey={ key } propVal={ v } propClass={ propClass } />) : null
-        }
-        {
-          !description ? null :
-            <Markdown source={ description } />
-        }
-        {
-          xml && xml.size ? (<span><br /><span className={ propClass }>xml:</span>
-            {
-              xml.entrySeq().map( ( [ key, v ] ) => <span key={`${key}-${v}`} className={ propClass }><br/>&nbsp;&nbsp;&nbsp;{key}: { String(v) }</span>).toArray()
-            }
-          </span>): null
-        }
-        {
-          enumArray && <EnumModel value={ enumArray } getComponent={ getComponent } />
-        }
-      </span>
+      <ModelCollapse title={titleEl} expanded={depth <= expandDepth} collapsedContent=" ">
+        <span className="prop">
+          <span className="prop-type">{ type }</span>
+          { format && <span className="prop-format">(${format})</span>}
+          {
+            properties.size ? properties.entrySeq().map( ( [ key, v ] ) => <Property key={`${key}-${v}`} propKey={ key } propVal={ v } propClass={ propClass } />) : null
+          }
+          {
+            showExtensions && extensions.size ? extensions.entrySeq().map( ( [ key, v ] ) => <Property key={`${key}-${v}`} propKey={ key } propVal={ v } propClass={ propClass } />) : null
+          }
+          {
+            !description ? null :
+              <Markdown source={ description } />
+          }
+          {
+            xml && xml.size ? (<span><br /><span className={ propClass }>xml:</span>
+              {
+                xml.entrySeq().map( ( [ key, v ] ) => <span key={`${key}-${v}`} className={ propClass }><br/>&nbsp;&nbsp;&nbsp;{key}: { String(v) }</span>).toArray()
+              }
+            </span>): null
+          }
+          {
+            enumArray && <EnumModel value={ enumArray } getComponent={ getComponent } />
+          }
+        </span>
+      </ModelCollapse>
     </span>
   }
 }
