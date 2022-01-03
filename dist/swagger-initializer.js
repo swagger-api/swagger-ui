8c946a02e73ef877d73b7635de27924418ba50f3
akop <akop@ppi.de>
2022-01-03 10:08:05 +0100

    fix code, so that configuration can be injected in docker-environment

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -1,4 +1,7 @@
 window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     url: "https://petstore.swagger.io/v2/swagger.json",
     dom_id: '#swagger-ui',
@@ -12,4 +15,6 @@ window.onload = function() {
     ],
     layout: "StandaloneLayout"
   });
+
+  //</editor-fold>
 };
