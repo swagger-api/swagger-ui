eb35158e4381b266a2a1972deee13de97ffe987e
live-wire <dhruv.life@hotmail.com>
2022-07-11 23:28:51 +0200

    Add support for bigints in response body

--- src/core/components/response-body.jsx
+++ src/core/components/response-body.jsx
@@ -5,6 +5,7 @@ import toLower from "lodash/toLower"
 import { extractFileNameFromContentDispositionHeader } from "core/utils"
 import { getKnownSyntaxHighlighterLanguage } from "core/utils/jsonParse"
 import win from "core/window"
+var JSONbig = require('json-bigint');
 
 export default class ResponseBody extends React.PureComponent {
   state = {
@@ -23,11 +24,11 @@ export default class ResponseBody extends React.PureComponent {
   updateParsedContent = (prevContent) => {
     const { content } = this.props
 
-    if(prevContent === content) {
+    if (prevContent === content) {
       return
     }
 
-    if(content && content instanceof Blob) {
+    if (content && content instanceof Blob) {
       var reader = new FileReader()
       reader.onload = () => {
         this.setState({
@@ -51,7 +52,7 @@ export default class ResponseBody extends React.PureComponent {
   }
 
   render() {
-    let { content, contentType, url, headers={}, getConfigs, getComponent } = this.props
+    let { content, contentType, url, headers = {}, getConfigs, getComponent } = this.props
     const { parsedContent } = this.state
     const HighlightCode = getComponent("highlightCode")
     const downloadName = "response_" + new Date().getTime()
@@ -68,7 +69,7 @@ export default class ResponseBody extends React.PureComponent {
 
       if ("Blob" in window) {
         let type = contentType || "text/html"
-        let blob = (content instanceof Blob) ? content : new Blob([content], {type: type})
+        let blob = (content instanceof Blob) ? content : new Blob([content], { type: type })
         let href = window.URL.createObjectURL(blob)
         let fileName = url.substr(url.lastIndexOf("/") + 1)
         let download = [type, fileName, href].join(":")
@@ -83,10 +84,10 @@ export default class ResponseBody extends React.PureComponent {
           }
         }
 
-        if(win.navigator && win.navigator.msSaveOrOpenBlob) {
-            bodyEl = <div><a href={ href } onClick={() => win.navigator.msSaveOrOpenBlob(blob, download)}>{ "Download file" }</a></div>
+        if (win.navigator && win.navigator.msSaveOrOpenBlob) {
+          bodyEl = <div><a href={href} onClick={() => win.navigator.msSaveOrOpenBlob(blob, download)}>{"Download file"}</a></div>
         } else {
-            bodyEl = <div><a href={ href } download={ download }>{ "Download file" }</a></div>
+          bodyEl = <div><a href={href} download={download}>{"Download file"}</a></div>
         }
       } else {
         bodyEl = <pre className="microlight">Download headers detected but your browser does not support downloading binary via XHR (Blob).</pre>
@@ -101,12 +102,12 @@ export default class ResponseBody extends React.PureComponent {
         language = "json"
       }
       try {
-        body = JSON.stringify(JSON.parse(content), null, "  ")
+        body = JSONbig.stringify(JSONbig.parse(content), null, "  ")
       } catch (error) {
         body = "can't parse JSON.  Raw result:\n\n" + content
       }
 
-      bodyEl = <HighlightCode language={language} downloadable fileName={`${downloadName}.json`} value={ body } getConfigs={ getConfigs } canCopy />
+      bodyEl = <HighlightCode language={language} downloadable fileName={`${downloadName}.json`} value={body} getConfigs={getConfigs} canCopy />
 
       // XML
     } else if (/xml/i.test(contentType)) {
@@ -114,39 +115,39 @@ export default class ResponseBody extends React.PureComponent {
         textNodesOnSameLine: true,
         indentor: "  "
       })
-      bodyEl = <HighlightCode downloadable fileName={`${downloadName}.xml`} value={ body } getConfigs={ getConfigs } canCopy />
+      bodyEl = <HighlightCode downloadable fileName={`${downloadName}.xml`} value={body} getConfigs={getConfigs} canCopy />
 
       // HTML or Plain Text
     } else if (toLower(contentType) === "text/html" || /text\/plain/.test(contentType)) {
-      bodyEl = <HighlightCode downloadable fileName={`${downloadName}.html`} value={ content } getConfigs={ getConfigs } canCopy />
+      bodyEl = <HighlightCode downloadable fileName={`${downloadName}.html`} value={content} getConfigs={getConfigs} canCopy />
 
       // CSV
     } else if (toLower(contentType) === "text/csv" || /text\/csv/.test(contentType)) {
-      bodyEl = <HighlightCode downloadable fileName={`${downloadName}.csv`} value={ content } getConfigs={ getConfigs } canCopy />
+      bodyEl = <HighlightCode downloadable fileName={`${downloadName}.csv`} value={content} getConfigs={getConfigs} canCopy />
 
       // Image
     } else if (/^image\//i.test(contentType)) {
-      if(contentType.includes("svg")) {
-        bodyEl = <div> { content } </div>
+      if (contentType.includes("svg")) {
+        bodyEl = <div> {content} </div>
       } else {
-        bodyEl = <img src={ window.URL.createObjectURL(content) } />
+        bodyEl = <img src={window.URL.createObjectURL(content)} />
       }
 
       // Audio
     } else if (/^audio\//i.test(contentType)) {
-      bodyEl = <pre className="microlight"><audio controls><source src={ url } type={ contentType } /></audio></pre>
+      bodyEl = <pre className="microlight"><audio controls><source src={url} type={contentType} /></audio></pre>
     } else if (typeof content === "string") {
-      bodyEl = <HighlightCode downloadable fileName={`${downloadName}.txt`} value={ content } getConfigs={ getConfigs } canCopy />
-    } else if ( content.size > 0 ) {
+      bodyEl = <HighlightCode downloadable fileName={`${downloadName}.txt`} value={content} getConfigs={getConfigs} canCopy />
+    } else if (content.size > 0) {
       // We don't know the contentType, but there was some content returned
-      if(parsedContent) {
+      if (parsedContent) {
         // We were able to squeeze something out of content
         // in `updateParsedContent`, so let's display it
         bodyEl = <div>
           <p className="i">
             Unrecognized response type; displaying content as text.
           </p>
-          <HighlightCode downloadable fileName={`${downloadName}.txt`} value={ parsedContent } getConfigs={ getConfigs } canCopy />
+          <HighlightCode downloadable fileName={`${downloadName}.txt`} value={parsedContent} getConfigs={getConfigs} canCopy />
         </div>
 
       } else {
@@ -160,10 +161,10 @@ export default class ResponseBody extends React.PureComponent {
       bodyEl = null
     }
 
-    return ( !bodyEl ? null : <div>
-        <h5>Response body</h5>
-        { bodyEl }
-      </div>
+    return (!bodyEl ? null : <div>
+      <h5>Response body</h5>
+      {bodyEl}
+    </div>
     )
   }
 }
