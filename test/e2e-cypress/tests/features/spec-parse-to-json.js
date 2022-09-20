4546fb1957184f8379b74b1b09d087f9f706625c
twjasa <twjasa@gmail.com>
2022-09-20 02:11:25 -0300

    fix: click primitive model to expand and check if has text

--- test/e2e-cypress/tests/features/spec-parse-to-json.js
+++ test/e2e-cypress/tests/features/spec-parse-to-json.js
@@ -9,15 +9,23 @@ describe("Parse YAML as YAML@1.2 with json_schema for all JSON-supported types",
       .click()
       // Responses -> example value tab
       .get(".language-json > :nth-child(3)")
-      .should("have.text", "\"without-quotes\"")
+      .should("have.text", '"without-quotes"')
       .get(".language-json > :nth-child(5)")
-      .should("have.text", "\"1999-11-31\"")
+      .should("have.text", '"1999-11-31"')
       // Responses -> schema tab
       .get(".model-example > .tab > :nth-child(2)")
       .click()
-      .get(":nth-child(1) > :nth-child(2) > .model > :nth-child(1) > .prop > .property") // first element, without-quotes
+      .get(":nth-child(1) > :nth-child(2) > .model > :nth-child(1)")
+      .click()
+      .get(
+        ":nth-child(1) > :nth-child(2) > .model > :nth-child(1) > .prop > .property"
+      ) // first element, without-quotes
       .should("have.text", "example: 1999-11-31")
-      .get(":nth-child(2) > :nth-child(2) > .model > :nth-child(1) > .prop > .property") // second element, with quotes
+      .get(":nth-child(2) > :nth-child(2) > .model > :nth-child(1)")
+      .click()
+      .get(
+        ":nth-child(2) > :nth-child(2) > .model > :nth-child(1) > .prop > .property"
+      ) // second element, with quotes
       .should("have.text", "example: 1999-12-31")
   })
 })
