ba530be9b43e60b86e78227aa33c7d02546db3b6
Timothy Lai <timothy.lai@smartbear.com>
2023-01-26 11:35:33 -0800

    test(oas31): add anchor target safety check for License object

--- test/e2e-cypress/tests/features/license.js
+++ test/e2e-cypress/tests/features/license.js
@@ -1,27 +1,29 @@
 describe("Render License", () => {
-  it("should render License URL from Swagger2 definition", () => {
+  describe("OpenAPI 2", () =>{
     const baseUrl = "/?url=/documents/features/license-openAPI2.yaml"
-    cy.visit(baseUrl)
-      .get(".info__license")
-      .should("exist")
-      .should("contains.text", "Apache 2.0")
-      .should("not.contains.text", "SPDX License")
-      .get(".info__license__identifier")
-      .should("not.exist")
+    it("should render License URL", () => {
+      cy.visit(baseUrl)
+        .get(".info__license")
+        .should("exist")
+        .should("contains.text", "Apache 2.0")
+        .should("not.contains.text", "SPDX License")
+        .get(".info__license__identifier")
+        .should("not.exist")
+    })
+    it("should render License URL anchor target xss link with safe `rel` attributes ", () => {
+      cy.visit(baseUrl)
+        .get(".info__license > .link")
+        .should("have.attr", "rel")
+        .and("include", "noopener")
+        .and("include", "noreferrer")
+        .get(".info .main > a")
+        .should("have.attr", "target")
+        .and("equal", "_blank")
+    })
   })
-  it("should render License URL from OpenAPI 3.0 definition", () => {
+  describe("OpenAPI 3.0.x", () => {
     const baseUrl = "/?url=/documents/features/license-openAPI30.yaml"
-    cy.visit(baseUrl)
-      .get(".info__license__url")
-      .should("exist")
-      .should("contains.text", "Apache 2.0")
-      .should("not.contains.text", "SPDX License")
-      .get(".info__license__identifier")
-      .should("not.exist")
-   })
-  describe("should render License from OpenAPI 3.1 definition", () => { 
-    it("should render only URL", () => {
-      const baseUrl = "/?url=/documents/features/license-openAPI31-url.yaml"
+    it("should render License URL", () => {
       cy.visit(baseUrl)
         .get(".info__license__url")
         .should("exist")
@@ -30,23 +32,74 @@ describe("Render License", () => {
         .get(".info__license__identifier")
         .should("not.exist")
      })
-    it("should render only SPDX", () => {
+    it("should render URL anchor target xss link with safe `rel` attributes ", () => {
+      cy.visit(baseUrl)
+        .get(".info__license__url > a")
+        .should("have.attr", "rel")
+        .and("include", "noopener")
+        .and("include", "noreferrer")
+        .get(".info .main > a")
+        .should("have.attr", "target")
+        .and("equal", "_blank")
+    })
+  })
+  describe("OpenAPI 3.1.x", () => { 
+    describe("only URL", () => {
+      const baseUrl = "/?url=/documents/features/license-openAPI31-url.yaml"
+      it("should render URL", () => {
+        cy.visit(baseUrl)
+          .get(".info__license__url")
+          .should("exist")
+          .should("contains.text", "Apache 2.0")
+          .should("not.contains.text", "SPDX License")
+          .get(".info__license__identifier")
+          .should("not.exist")
+       })
+      it("should render URL anchor target xss link with safe `rel` attributes ", () => {
+        cy.visit(baseUrl)
+          .get(".info__license__url > a")
+          .should("have.attr", "rel")
+          .and("include", "noopener")
+          .and("include", "noreferrer")
+          .get(".info .main > a")
+          .should("have.attr", "target")
+          .and("equal", "_blank")
+      })
+    })
+    describe("only SPDX Identifier", () => {
       const baseUrl = "/?url=/documents/features/license-openAPI31-identifier.yaml"
-      cy.visit(baseUrl)
-        .get(".info__license__identifier")
-        .should("exist")
-        .should("contains.text", "Apache-2.0")
-        .should("contains.text", "SPDX License")
-        .get(".info__license__url")
-        .should("not.exist")
-     })
-    it("should render nothing if both URL & SPDX exists", () => {
-      const baseUrl = "/?url=/documents/features/license-openAPI31-error-both-identifier-and-url.yaml"
-      cy.visit(baseUrl)
-        .get(".info__license__identifier")
-        .should("not.exist")
-        .get(".info__license__url")
-        .should("not.exist")
-     })
+      it("should render SPDX Identifier", () => {
+        cy.visit(baseUrl)
+          .get(".info__license__identifier")
+          .should("exist")
+          .should("contains.text", "Apache-2.0")
+          .should("contains.text", "SPDX License")
+          .get(".info__license__url")
+          .should("not.exist")
+       })
+      it("should render SPDX and Identifier anchor target xss links with safe `rel` attributes ", () => {
+        cy.visit(baseUrl)
+          .get(".info__license__identifier > a")
+          .each(($el) => {
+            cy.get($el)
+              .should("have.attr", "rel")
+              .and("include", "noopener")
+              .and("include", "noreferrer")
+            cy.get($el)
+              .should("have.attr", "target")
+              .and("equal", "_blank")
+          })
+      })
+    })
+    describe("URL and SPX are mutually exclusive", () => {
+      it("should render nothing if both URL & SPDX exists", () => {
+        const baseUrl = "/?url=/documents/features/license-openAPI31-error-both-identifier-and-url.yaml"
+        cy.visit(baseUrl)
+          .get(".info__license__identifier")
+          .should("not.exist")
+          .get(".info__license__url")
+          .should("not.exist")
+       })
+    })
   })
 })
