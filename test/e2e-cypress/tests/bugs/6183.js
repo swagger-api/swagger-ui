21cfda392f9f7e018a45b6cf98bf8d93392334dc
Vladimír Gorej <vladimir.gorej@gmail.com>
2023-02-23 16:15:44 +0100

    test(cypress): fix tests flakiness by stubbing httpbin.org requests (#8410)
    Closes #8409

--- test/e2e-cypress/tests/bugs/6183.js
+++ test/e2e-cypress/tests/bugs/6183.js
@@ -1,5 +1,29 @@
 describe("When trying it out", () => {
   it("should render the response headers as comma separated lists", () => {
+    cy.intercept(
+      {
+        method: "GET",
+        url: /^\/response-headers/,
+        hostname: "httpbin.org",
+      },
+      {
+        body: {
+          "Access-Control-Expose-Headers": "X-Header1, X-Header2, X-Header3, Access-Control-Expose-Headers",
+          "Content-Length": "289",
+          "Content-Type": "application/json",
+          "X-Header1": "value1,value2",
+          "X-Header2": "value3,value4",
+          "X-Header3": ["value5", "value6"]
+        },
+        headers: {
+          "access-control-expose-headers": "X-Header1,X-Header2,X-Header3,Access-Control-Expose-Headers",
+          "content-type": "application/json",
+          "x-header1": "value1,value2",
+          "x-header2": "value3,value4",
+          "x-header3": "value5,value6",
+        }
+    })
+
     cy.visit("/?url=/documents/bugs/6183.yaml")
       .get("#operations-default-get_response_headers")
       .click()
