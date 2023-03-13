b0063ddefe28899960150bfac83290acababbcd6
Vladimír Gorej <vladimir.gorej@gmail.com>
2023-03-13 11:54:41 +0100

    style: rewrite test to be more readable

--- test/unit/core/helpers/get-model-name.js
+++ test/unit/core/helpers/get-model-name.js
@@ -5,21 +5,35 @@
 import Model from "../../../../src/core/components/model"
 
 describe("getModelName", () => {
-    let m = new Model()
-    it("Should decode JSON Pointer and URI encoding for OpenAPI v3 refs", () => {
-        expect(m.getModelName("#/components/schemas/a~1b%2Bc"))
-            .toEqual("a/b+c")
-    })
-    it("Should decode JSON Pointer and URI encoding for Swagger v2 refs", () => {
-        expect(m.getModelName("#/definitions/custom%3A%3Anamespace%3A%3APerson"))
-            .toEqual("custom::namespace::Person")
-    })
-    it("decode multiple json-pointer values", () => {
-        expect(m.getModelName("#/components/schemas/~1~1~0~0"))
-            .toEqual("//~~")
-    })
-    it("support invalid URI encoding", () => {
-        expect(m.getModelName("#/components/schemas/%25%d"))
-            .toEqual("%25%d")
-    })
+  const model = new Model()
+
+  it("should decode JSON Pointer and URI encoding for OpenAPI v3 refs", () => {
+    const actual = model.getModelName("#/components/schemas/a~1b%2Bc")
+    const expected = "a/b+c"
+
+    expect(actual).toStrictEqual(expected)
+  })
+
+  it("should decode JSON Pointer and URI encoding for Swagger v2 refs", () => {
+    const actual = model.getModelName(
+      "#/definitions/custom%3A%3Anamespace%3A%3APerson"
+    )
+    const expected = "custom::namespace::Person"
+
+    expect(actual).toStrictEqual(expected)
+  })
+
+  it("should decode multiple json-pointer values", () => {
+    const actual = model.getModelName("#/components/schemas/~1~1~0~0")
+    const expected = "//~~"
+
+    expect(actual).toStrictEqual(expected)
+  })
+
+  it("should support invalid URI encoding", () => {
+    const actual = model.getModelName("#/components/schemas/%25%d")
+    const expected = "%25%d"
+
+    expect(actual).toStrictEqual(expected)
+  })
 })
