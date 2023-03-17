2acbcaa097b61d4895f421efffc13b80079b7b9e
Vladimir Gorej <vladimir.gorej@gmail.com>
2023-03-17 10:42:43 +0100

    refactor(oas31): change Info compnent to be smart
    Refs #8474

--- src/core/plugins/oas31/spec-extensions/selectors.js
+++ src/core/plugins/oas31/spec-extensions/selectors.js
@@ -20,10 +20,6 @@ export const license = () => (system) => {
   return system.specSelectors.info().get("license", map)
 }
 
-export const contact = () => (system) => {
-  return system.specSelectors.info().get("contact", map)
-}
-
 export const selectLicenseNameField = () => (system) => {
   return system.specSelectors.license().get("name", "License")
 }
@@ -50,6 +46,10 @@ export const selectLicenseIdentifierField = onlyOAS31(() => (system) => {
   return system.specSelectors.license().get("identifier")
 })
 
+export const contact = () => (system) => {
+  return system.specSelectors.info().get("contact", map)
+}
+
 export const selectContactNameField = () => (system) => {
   return system.specSelectors.contact().get("name", "the developer")
 }
@@ -67,6 +67,58 @@ export const makeSelectContactUrl = (system) =>
     () => system.specSelectors.url(),
     () => system.oas3Selectors.selectedServer(),
     () => system.specSelectors.selectContactUrlField(),
+    (specUrl, selectedServer, url) => {
+      if (url) {
+        return safeBuildUrl(url, specUrl, { selectedServer })
+      }
+
+      return undefined
+    }
+  )
+
+export const selectInfoTitleField = () => (system) => {
+  return system.specSelectors.info().get("title")
+}
+
+export const selectInfoSummaryField = onlyOAS31(() => (system) => {
+  return system.specSelectors.info().get("summary")
+})
+
+export const selectInfoDescriptionField = () => (system) => {
+  return system.specSelectors.info().get("description")
+}
+
+export const selectInfoTermsOfServiceField = () => (system) => {
+  return system.specSelectors.info().get("termsOfService")
+}
+
+export const makeSelectInfoTermsOfServiceUrl = (system) =>
+  createSelector(
+    () => system.specSelectors.url(),
+    () => system.oas3Selectors.selectedServer(),
+    () => system.specSelectors.selectInfoTermsOfServiceField(),
+    (specUrl, selectedServer, termsOfService) => {
+      if (termsOfService) {
+        return safeBuildUrl(termsOfService, specUrl, { selectedServer })
+      }
+
+      return undefined
+    }
+  )
+
+export const selectExternalDocsDescriptionField = () => (system) => {
+  return system.specSelectors.externalDocs().get("description")
+}
+
+export const selectExternalDocsUrlField = () => (system) => {
+  return system.specSelectors.externalDocs().get("url")
+}
+
+export const makeSelectExternalDocsUrl = (system) =>
+  createSelector(
+    () => system.specSelectors.url(),
+    () => system.oas3Selectors.selectedServer(),
+    () => system.specSelectors.selectExternalDocsUrlField(),
     (specUrl, selectedServer, url) => {
       if (url) {
         return safeBuildUrl(url, specUrl, { selectedServer })
--- src/core/plugins/oas31/wrap-components/info.jsx
+++ src/core/plugins/oas31/wrap-components/info.jsx
@@ -5,9 +5,9 @@ import React from "react"
 
 const InfoWrapper = (Original, system) => (props) => {
   if (system.specSelectors.isOAS31()) {
-    const OAS31Info = system.getComponent("OAS31Info")
+    const OAS31Info = system.getComponent("OAS31Info", true)
 
-    return <OAS31Info {...props} />
+    return <OAS31Info />
   }
 
   return <Original {...props} />
