c5ac88fe024a522930c9e56b5a44e658c8435fa8
Vladimir Gorej <vladimir.gorej@gmail.com>
2023-04-13 17:43:19 +0200

    feat: introduce new UI + extendable behavior
    Ref #8513

--- src/core/plugins/json-schema-2020-12/index.js
+++ src/core/plugins/json-schema-2020-12/index.js
@@ -3,6 +3,8 @@
  */
 import JSONSchema from "./components/JSONSchema/JSONSchema"
 import BooleanJSONSchema from "./components/BooleanJSONSchema/BooleanJSONSchema"
+import Accordion from "./components/Accordion/Accordion"
+import ChevronRightIcon from "./components/icons/ChevronRight"
 import { upperFirst } from "./fn"
 import { withJSONSchemaContext } from "./hoc"
 
@@ -10,6 +12,8 @@ const JSONSchema202012Plugin = () => ({
   components: {
     JSONSchema202012: JSONSchema,
     BooleanJSONSchema202012: BooleanJSONSchema,
+    JSONSchema202012Accordion: Accordion,
+    JSONSchema202012ChevronRightIcon: ChevronRightIcon,
     withJSONSchema202012Context: withJSONSchemaContext,
   },
   fn: {
