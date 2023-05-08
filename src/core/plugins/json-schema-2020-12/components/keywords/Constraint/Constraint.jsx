70f53a6354b49aa61a9e90f38871a4cd19b78a9d
Vladimir Gorej <vladimir.gorej@gmail.com>
2023-05-08 11:41:36 +0200

    feat(json-schema-2020-12): support string validation keywords
    Refs #8513

--- src/core/plugins/json-schema-2020-12/components/keywords/Constraint/Constraint.jsx
+++ src/core/plugins/json-schema-2020-12/components/keywords/Constraint/Constraint.jsx
@@ -3,14 +3,27 @@
  */
 import React from "react"
 import PropTypes from "prop-types"
+import classNames from "classnames"
 
 /**
  * This component represents various constraint keywords
  * from JSON Schema 2020-12 validation vocabulary.
  */
-const Constraint = ({ constraint }) => (
-  <span className="json-schema-2020-12__constraint">{constraint}</span>
-)
+const Constraint = ({ constraint }) => {
+  const isPattern = /^matches /.test(constraint)
+  const isStringRange = /characters/.test(constraint)
+  const isStringRelated = isPattern || isStringRange
+
+  return (
+    <span
+      className={classNames("json-schema-2020-12__constraint", {
+        "json-schema-2020-12__constraint--string-related": isStringRelated,
+      })}
+    >
+      {constraint}
+    </span>
+  )
+}
 
 Constraint.propTypes = {
   constraint: PropTypes.string.isRequired,
