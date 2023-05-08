a197d19abe9d010cd1778d93dd954f69b5d9a600
Vladimir Gorej <vladimir.gorej@gmail.com>
2023-05-08 13:14:50 +0200

    feat(json-schema-2020-12): add support for required keyword
    Refs #8513

--- src/core/plugins/json-schema-2020-12/components/keywords/Properties/Properties.jsx
+++ src/core/plugins/json-schema-2020-12/components/keywords/Properties/Properties.jsx
@@ -2,12 +2,14 @@
  * @prettier
  */
 import React from "react"
+import classNames from "classnames"
 
 import { schema } from "../../../prop-types"
 import { useComponent } from "../../../hooks"
 
 const Properties = ({ schema }) => {
   const properties = schema?.properties || {}
+  const required = Array.isArray(schema?.required) ? schema.required : []
   const JSONSchema = useComponent("JSONSchema")
 
   /**
@@ -21,7 +23,13 @@ const Properties = ({ schema }) => {
     <div className="json-schema-2020-12-keyword json-schema-2020-12-keyword--properties">
       <ul>
         {Object.entries(properties).map(([propertyName, schema]) => (
-          <li key={propertyName} className="json-schema-2020-12-property">
+          <li
+            key={propertyName}
+            className={classNames("json-schema-2020-12-property", {
+              "json-schema-2020-12-property--required":
+                required.includes(propertyName),
+            })}
+          >
             <JSONSchema name={propertyName} schema={schema} />
           </li>
         ))}
