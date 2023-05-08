5e9ff5af86c7d83fdd0425b94ba8e86f1be9c75f
Vladimir Gorej <vladimir.gorej@gmail.com>
2023-05-08 10:49:38 +0200

    style: fix

--- src/core/plugins/json-schema-2020-12/components/keywords/Constraint/Constraint.jsx
+++ src/core/plugins/json-schema-2020-12/components/keywords/Constraint/Constraint.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+/**
+ * @prettier
+ */
+import React from "react"
 
 /**
  * This component represents various constraint keywords
  * from JSON Schema 2020-12 validation vocabulary.
  */
 const Constraint = ({ constraint }) => (
-  <span className="json-schema-2020-12__constraint">
-    {constraint}
-  </span>
-);
+  <span className="json-schema-2020-12__constraint">{constraint}</span>
+)
 
-export default React.memo(Constraint);
+export default React.memo(Constraint)
