f718a193ffc034c742aec8882a799dee994bfe97
Vladimír Gorej <vladimir.gorej@gmail.com>
2023-05-30 15:01:15 +0200

    refactor(samples): move example generation code to samples plugin (#8727)
    Refs #8577

--- src/core/plugins/samples/index.js
+++ src/core/plugins/samples/index.js
@@ -1,5 +1,32 @@
-import * as fn from "./fn"
+/**
+ * @prettier
+ */
+import {
+  sampleFromSchema,
+  inferSchema,
+  sampleFromSchemaGeneric,
+  createXMLExample,
+  memoizedCreateXMLExample,
+  memoizedSampleFromSchema,
+} from "./fn/index"
+import makeGetJsonSampleSchema from "./fn/get-json-sample-schema"
+import makeGetYamlSampleSchema from "./fn/get-yaml-sample-schema"
+import makeGetXmlSampleSchema from "./fn/get-xml-sample-schema"
+import makeGetSampleSchema from "./fn/get-sample-schema"
 
-export default function () {
-  return { fn }
-}
+const SamplesPlugin = ({ getSystem }) => ({
+  fn: {
+    inferSchema,
+    sampleFromSchema,
+    sampleFromSchemaGeneric,
+    createXMLExample,
+    memoizedSampleFromSchema,
+    memoizedCreateXMLExample,
+    getJsonSampleSchema: makeGetJsonSampleSchema(getSystem),
+    getYamlSampleSchema: makeGetYamlSampleSchema(getSystem),
+    getXmlSampleSchema: makeGetXmlSampleSchema(getSystem),
+    getSampleSchema: makeGetSampleSchema(getSystem),
+  },
+})
+
+export default SamplesPlugin
