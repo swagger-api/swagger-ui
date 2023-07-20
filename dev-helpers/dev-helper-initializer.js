2fb91d3151c9ef4d75a9da7fa4bf7b48762adf69
Richard Wilburn <rhwilburn@gmail.com>
2023-07-21 09:37:26 +1200

    POC - setup

--- dev-helpers/dev-helper-initializer.js
+++ dev-helpers/dev-helper-initializer.js
@@ -4,7 +4,7 @@ window.onload = function() {
   window["SwaggerUIStandalonePreset"] = window["swagger-ui-standalone-preset"]
   // Build a system
   const ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "https://localhost:7003/swagger/v1/swagger.json",
     dom_id: "#swagger-ui",
     presets: [
       SwaggerUIBundle.presets.apis,
@@ -14,20 +14,53 @@ window.onload = function() {
       SwaggerUIBundle.plugins.DownloadUrl
     ],
     // requestSnippetsEnabled: true,
-    layout: "StandaloneLayout"
+    layout: "StandaloneLayout",
+    docExpansion: "full"
   })
 
-  window.ui = ui
-
-  ui.initOAuth({
-    clientId: "your-client-id",
-    clientSecret: "your-client-secret-if-required",
-    realm: "your-realms",
-    appName: "your-app-name",
-    scopeSeparator: " ",
-    scopes: "openid profile email phone address",
-    additionalQueryStringParams: {},
-    useBasicAuthenticationWithAccessCodeGrant: false,
-    usePkceWithAuthorizationCodeGrant: false
+  const ui2 = SwaggerUIBundle({
+    url: "https://localhost:7002/swagger/v1/swagger.json",
+    dom_id: "#swagger-ui2",
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    // requestSnippetsEnabled: true,
+    layout: "StandaloneLayout",
+    docExpansion: "full"
   })
+
+  const ui3 = SwaggerUIBundle({
+    url: "https://localhost:7001/swagger/v1/swagger.json",
+    dom_id: "#swagger-ui3",
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    // requestSnippetsEnabled: true,
+    layout: "StandaloneLayout",
+    docExpansion: "full"
+  })
+
+  window.ui = ui;
+  window.ui2 = ui2;
+  window.ui3 = ui3;
+
+  // ui.initOAuth({
+  //   clientId: "your-client-id",
+  //   clientSecret: "your-client-secret-if-required",
+  //   realm: "your-realms",
+  //   appName: "your-app-name",
+  //   scopeSeparator: " ",
+  //   scopes: "openid profile email phone address",
+  //   additionalQueryStringParams: {},
+  //   useBasicAuthenticationWithAccessCodeGrant: false,
+  //   usePkceWithAuthorizationCodeGrant: false
+  // })
 }
