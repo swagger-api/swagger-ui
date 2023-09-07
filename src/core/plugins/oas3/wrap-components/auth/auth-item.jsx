6d24e6f3e0de2b7d82066121b8aee0225db12701
Vladimir Gorej <vladimir.gorej@smartbear.com>
2023-09-07 09:10:47 +0200

    refactor: utilize nested wrapping

--- src/core/plugins/oas3/wrap-components/auth/auth-item.jsx
+++ src/core/plugins/oas3/wrap-components/auth/auth-item.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { OAS3ComponentWrapFactory } from "../helpers"
+import { OAS3ComponentWrapFactory } from "../../helpers"
 
 export default OAS3ComponentWrapFactory(({ Ori, ...props }) => {
   const {
@@ -8,6 +8,7 @@ export default OAS3ComponentWrapFactory(({ Ori, ...props }) => {
 
   const HttpAuth = getComponent("HttpAuth")
   const type = schema.get("type")
+
 
   if(type === "http") {
     return <HttpAuth key={ name }
