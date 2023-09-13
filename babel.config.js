439a5d2a10d450efbbb0a0011239066caa81db63
Vladimir Gorej <vladimir.gorej@smartbear.com>
2023-09-13 14:59:42 +0200

    build(babel): use browserslist explicitly

--- babel.config.js
+++ babel.config.js
@@ -1,60 +1,80 @@
+const browser = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        debug: false,
+        modules: "auto",
+        useBuiltIns: false,
+        forceAllTransforms: false,
+        ignoreBrowserslistConfig: false,
+      }
+    ],
+    "@babel/preset-react",
+  ],
+    plugins: [
+    [
+      "@babel/plugin-transform-runtime",
+      {
+        corejs: { version: 3, proposals: false },
+        absoluteRuntime: false,
+        helpers: true,
+        regenerator: false,
+        version: "^7.22.11",
+      }
+    ],
+    [
+      "transform-react-remove-prop-types",
+      {
+        additionalLibraries: [
+          "react-immutable-proptypes"
+        ]
+      }
+    ],
+    [
+      "babel-plugin-module-resolver",
+      {
+        alias: {
+          root: ".",
+          core: "./src/core",
+        }
+      }
+    ]
+  ],
+}
+
 module.exports = {
-  "env": {
-    "commonjs": {
-      "presets": [
+  env: {
+    commonjs: {
+      presets: [
         [
           "@babel/preset-env",
           {
-            "debug": false,
-            "modules": "commonjs",
-            "loose": true,
-            "targets": "maintained node versions",
-            "forceAllTransforms": false,
-            "ignoreBrowserslistConfig": true,
+            debug: false,
+            modules: "commonjs",
+            loose: true,
+            useBuiltIns: false,
+            forceAllTransforms: false,
+            ignoreBrowserslistConfig: false,
           }
         ],
         "@babel/preset-react",
       ],
-      "plugins": [
-        [
-          "transform-react-remove-prop-types",
-          {
-            "additionalLibraries": [
-              "react-immutable-proptypes"
-            ]
-          }
-        ],
-      ],
-    },
-    "esm": {
-      "presets": [
-        [
-          "@babel/env",
-          {
-            "debug": false,
-            "modules": false,
-            "ignoreBrowserslistConfig": false,
-            "useBuiltIns": false,
-            "include": [
-              "@babel/plugin-proposal-logical-assignment-operators"
-            ]
-          }
-        ],
-        "@babel/preset-react"
-      ],
-      "plugins": [
+      plugins: [
         [
           "@babel/plugin-transform-runtime",
           {
-            "corejs": 3,
-            "absoluteRuntime": false,
-            "version": "^7.11.2"
+            corejs: { version: 3, proposals: false },
+            absoluteRuntime: false,
+            helpers: true,
+            regenerator: false,
+            version: "^7.22.11",
           }
         ],
         [
           "transform-react-remove-prop-types",
           {
-            "additionalLibraries": [
+            additionalLibraries: [
               "react-immutable-proptypes"
             ]
           }
@@ -62,41 +82,42 @@ module.exports = {
         [
           "babel-plugin-module-resolver",
           {
-            "alias": {
-              "root": ".",
-              "core": "./src/core",
+            alias: {
+              root: ".",
+              core: "./src/core",
             }
           }
         ]
-      ]
+      ],
     },
-    "test": {
-      "presets": [
+    esm: {
+      presets: [
         [
           "@babel/env",
           {
-            "targets": {
-              "node": "20.3.0"
-            },
-            "ignoreBrowserslistConfig": true,
-            "useBuiltIns": false,
+            debug: false,
+            modules: false,
+            ignoreBrowserslistConfig: false,
+            useBuiltIns: false,
           }
         ],
         "@babel/preset-react"
       ],
-      "plugins": [
+      plugins: [
         [
           "@babel/plugin-transform-runtime",
           {
-            "corejs": 3,
-            "absoluteRuntime": false,
-            "version": "^7.11.2"
+            corejs: { version: 3, proposals: false },
+            absoluteRuntime: false,
+            helpers: true,
+            regenerator: false,
+            version: "^7.22.11",
           }
         ],
         [
           "transform-react-remove-prop-types",
           {
-            "additionalLibraries": [
+            additionalLibraries: [
               "react-immutable-proptypes"
             ]
           }
@@ -104,102 +125,16 @@ module.exports = {
         [
           "babel-plugin-module-resolver",
           {
-            "alias": {
-              "root": ".",
-              "core": "./src/core",
+            alias: {
+              root: ".",
+              core: "./src/core",
             }
           }
         ]
       ]
     },
-    "development": {
-      "presets": [
-        [
-          "@babel/env",
-          {
-            "ignoreBrowserslistConfig": false,
-            "useBuiltIns": false,
-            "include": [
-              "@babel/plugin-proposal-logical-assignment-operators"
-            ]
-          }
-        ],
-        "@babel/preset-react"
-      ],
-      "plugins": [
-        [
-          "@babel/plugin-transform-runtime",
-          {
-            "corejs": 3,
-            "absoluteRuntime": false,
-            "version": "^7.11.2"
-          }
-        ],
-        [
-          "transform-react-remove-prop-types",
-          {
-            "additionalLibraries": [
-              "react-immutable-proptypes"
-            ]
-          }
-        ],
-        [
-          "babel-plugin-module-resolver",
-          {
-            "alias": {
-              "root": ".",
-              "core": "./src/core",
-            }
-          }
-        ]
-      ]
-    },
-    "production": {
-      "presets": [
-        [
-          "@babel/env",
-          {
-            "ignoreBrowserslistConfig": false,
-            "useBuiltIns": false,
-            "include": [
-              "@babel/plugin-proposal-logical-assignment-operators"
-            ]
-          }
-        ],
-        "@babel/preset-react"
-      ],
-      "plugins": [
-        "@babel/plugin-transform-class-properties",
-        "@babel/plugin-transform-nullish-coalescing-operator",
-        "@babel/plugin-transform-object-rest-spread",
-        "@babel/plugin-transform-optional-chaining",
-        [
-          "@babel/plugin-transform-runtime",
-          {
-            "corejs": 3,
-            "absoluteRuntime": false,
-            "version": "^7.11.2"
-          }
-        ],
-        [
-          "transform-react-remove-prop-types",
-          {
-            "additionalLibraries": [
-              "react-immutable-proptypes"
-            ]
-          }
-        ],
-        [
-          "babel-plugin-module-resolver",
-          {
-            "alias": {
-              "root": ".",
-              "core": "./src/core",
-            }
-          }
-        ]
-      ]
-    },
-  }
+    development: browser,
+    production: browser,
+  },
 }
 
