96b318c3d0ada7753ab0325f7961a9c37ed9028b
Alexander Pushkov <github.com@accounts.ale.sh>
2023-09-25 18:30:56 +0300

    feature(oauth2): allow passing additional form params to token endpoint

--- src/core/oauth2-authorize.js
+++ src/core/oauth2-authorize.js
@@ -127,6 +127,7 @@ export default function authorize ( { auth, authActions, errActions, configs, au
     state: state,
     redirectUrl: redirectUrl,
     callback: callback,
-    errCb: errActions.newAuthErr
+    errCb: errActions.newAuthErr,
+    authConfigs: authConfigs
   })
 }
