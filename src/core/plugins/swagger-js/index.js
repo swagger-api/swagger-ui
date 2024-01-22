04606e6850c1724ce6ca6ad2f85e9e80beea5e86
Alvin <alvin@dsaid.gov.sg>
2024-01-22 18:10:59 +0800

    patch prefetch without credentials

--- src/core/plugins/swagger-js/index.js
+++ src/core/plugins/swagger-js/index.js
@@ -1,9 +1,27 @@
 import Swagger from "swagger-client"
 
+const createPreFetch = (origPreFetch) => (req) => {
+  let request = origPreFetch(req)
+
+  // never send cookie
+  if(typeof request === "string") {
+    request = {
+      url: req,
+      credentials: "omit"
+    }
+  }else {
+    request = {
+      ...req,
+      credentials: "omit"
+    }
+  }
+  return req
+}
+
 module.exports = function({ configs }) {
   return {
     fn: {
-      fetch: Swagger.makeHttp(configs.preFetch, configs.postFetch),
+      fetch: Swagger.makeHttp(createPreFetch(configs.preFetch), configs.postFetch),
       buildRequest: Swagger.buildRequest,
       execute: Swagger.execute,
       resolve: Swagger.resolve,
