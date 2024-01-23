537f53323b37129df84cba98f0b0e35d85ce3c2f
Alvin <alvin@dsaid.gov.sg>
2024-01-23 23:28:38 +0800

    remove unused

--- src/core/plugins/swagger-js/index.js
+++ src/core/plugins/swagger-js/index.js
@@ -7,7 +7,6 @@ const createExecutor = (origExecutor) => (opts) => {
     ...opts,
     requestInterceptor: (req) => {
       let request = origRequestInterceptor ? origRequestInterceptor(req) : req
-      // never send cookie
       if(typeof request === "string") {
         request = {
           url: req,
@@ -28,8 +27,8 @@ const createExecutor = (origExecutor) => (opts) => {
 module.exports = function({ configs }) {
   return {
     fn: {
-      fetch: Swagger.makeHttp(createPreFetch(configs.preFetch), configs.postFetch),
-      buildRequest: createBuildRequest(Swagger.buildRequest),
+      fetch: Swagger.makeHttp(configs.preFetch, configs.postFetch),
+      buildRequest: Swagger.buildRequest,
       execute: createExecutor(Swagger.execute),
       resolve: Swagger.resolve,
       serializeRes: Swagger.serializeRes,
