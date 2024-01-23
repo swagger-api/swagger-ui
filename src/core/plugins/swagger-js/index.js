70f9dff5827fd5b3b45975d73e280c33c8625deb
Alvin <alvin@dsaid.gov.sg>
2024-01-23 22:57:14 +0800

    wrap execute, use requestInterceptor

--- src/core/plugins/swagger-js/index.js
+++ src/core/plugins/swagger-js/index.js
@@ -3,19 +3,29 @@ import Swagger from "swagger-client"
 const createPreFetch = (origPreFetch) => (req) => {
   let request = origPreFetch ? origPreFetch(req) : req
 
-  // never send cookie
   if(typeof request === "string") {
     request = {
       url: req,
-      credentials: "omit"
-    }
-  }else {
-    request = {
-      ...req,
-      credentials: "omit"
+      requestInterceptor: (req) => {
+        return {
+          ...req,
+          credentials: "omit"
+        }
+      }
     }
   }
-  return req
+  else {
+    request = {
+      ...req,
+      requestInterceptor: (req) => {
+        return {
+          ...req,
+          credentials: "omit"
+        }
+      }
+    }
+  }
+  return request
 }
 
 const createBuildRequest = (origBuildRequest) => (opts) => {
@@ -43,12 +53,37 @@ const createBuildRequest = (origBuildRequest) => (opts) => {
   return origBuildRequest(newOpts)
 }
 
+const createExecutor = (origExecutor) => (opts) => {
+  const origRequestInterceptor = opts.requestInterceptor
+
+  const newOpts = {
+    ...opts,
+    requestInterceptor: (req) => {
+      let request = origRequestInterceptor ? origRequestInterceptor(req) : req
+      // never send cookie
+      if(typeof request === "string") {
+        request = {
+          url: req,
+          credentials: "omit"
+        }
+      }else {
+        request = {
+          ...req,
+          credentials: "omit"
+        }
+      }
+      return request
+    }
+  }
+  return origExecutor(newOpts)
+}
+
 module.exports = function({ configs }) {
   return {
     fn: {
       fetch: Swagger.makeHttp(createPreFetch(configs.preFetch), configs.postFetch),
       buildRequest: createBuildRequest(Swagger.buildRequest),
-      execute: Swagger.execute,
+      execute: createExecutor(Swagger.execute),
       resolve: Swagger.resolve,
       serializeRes: Swagger.serializeRes,
       opId: Swagger.helpers.opId
