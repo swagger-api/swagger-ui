700eb585e033aea84495e46571d774b54e40f8e9
Alvin <alvin@dsaid.gov.sg>
2024-01-23 23:25:20 +0800

    remove unused

--- src/core/plugins/swagger-js/index.js
+++ src/core/plugins/swagger-js/index.js
@@ -1,58 +1,5 @@
 import Swagger from "swagger-client"
 
-const createPreFetch = (origPreFetch) => (req) => {
-  let request = origPreFetch ? origPreFetch(req) : req
-
-  if(typeof request === "string") {
-    request = {
-      url: req,
-      requestInterceptor: (req) => {
-        return {
-          ...req,
-          credentials: "omit"
-        }
-      }
-    }
-  }
-  else {
-    request = {
-      ...req,
-      requestInterceptor: (req) => {
-        return {
-          ...req,
-          credentials: "omit"
-        }
-      }
-    }
-  }
-  return request
-}
-
-const createBuildRequest = (origBuildRequest) => (opts) => {
-  const origRequestInterceptor = opts.requestInterceptor
-
-  const newOpts = {
-    ...opts,
-    requestInterceptor: (req) => {
-      let request = origRequestInterceptor ? origRequestInterceptor(req) : req
-      // never send cookie
-      if(typeof request === "string") {
-        request = {
-          url: req,
-          credentials: "omit"
-        }
-      }else {
-        request = {
-          ...req,
-          credentials: "omit"
-        }
-      }
-      return request
-    }
-  }
-  return origBuildRequest(newOpts)
-}
-
 const createExecutor = (origExecutor) => (opts) => {
   const origRequestInterceptor = opts.requestInterceptor
 
