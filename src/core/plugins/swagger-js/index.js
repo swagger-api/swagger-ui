5badc4c735c8b74a1527d263882bcf087c099cb2
Alvin <alvin@dsaid.gov.sg>
2024-01-23 22:26:18 +0800

    fix buildRequest

--- src/core/plugins/swagger-js/index.js
+++ src/core/plugins/swagger-js/index.js
@@ -18,11 +18,36 @@ const createPreFetch = (origPreFetch) => (req) => {
   return req
 }
 
+const createBuildRequest = (origBuildRequest) => (opts) => {
+  const origRequestInterceptor = opts.requestInterceptor
+
+  const newOpts = {
+    ...opts,
+    requestInterceptor: (req) => {
+      let request = origRequestInterceptor ? origRequestInterceptor(req) : req
+      // never send cookie
+      if(typeof request === "string") {
+        request = {
+          url: req,
+          credentials: "omit"
+        }
+      }else {
+        request = {
+          ...req,
+          credentials: "omit"
+        }
+      }
+      return request
+    }
+  }
+  return origBuildRequest(newOpts)
+}
+
 module.exports = function({ configs }) {
   return {
     fn: {
       fetch: Swagger.makeHttp(createPreFetch(configs.preFetch), configs.postFetch),
-      buildRequest: Swagger.buildRequest,
+      buildRequest: createBuildRequest(Swagger.buildRequest),
       execute: Swagger.execute,
       resolve: Swagger.resolve,
       serializeRes: Swagger.serializeRes,
