5d68885febe4aa7f9cce5f96780c2ad41c06ab6e
Oliwia Rogala <oliwia.rogala@smartbear.com>
2024-02-07 15:53:55 +0100

    refactor(test): move tests to features folder

--- test/e2e-cypress/e2e/features/try-it-out-non-200-response-body.cy.js
+++ test/e2e-cypress/e2e/features/try-it-out-non-200-response-body.cy.js
@@ -10,7 +10,7 @@ describe("#9556: SwaggerUI doesn't render response bodies for non-200 responses"
 
     it("should render response body for a response with 400 status code", () => {
       cy
-        .visit("?url=/documents/bugs/9556.yaml")
+        .visit("?url=/documents/features/try-it-out-non-200-response-body.yaml")
         .get("#operations-default-get_400_any")
         .click()
         .get(".try-out__btn")
