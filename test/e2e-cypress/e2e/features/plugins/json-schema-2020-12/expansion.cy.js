888f9f754e964489a1b70822094865b5ed52614f
Oliwia Rogala <oliwia.rogala@smartbear.com>
2024-02-14 10:10:10 +0100

    test(json-schema-2020-12): update tests

--- test/e2e-cypress/e2e/features/plugins/json-schema-2020-12/expansion.cy.js
+++ test/e2e-cypress/e2e/features/plugins/json-schema-2020-12/expansion.cy.js
@@ -2,9 +2,9 @@
  * @prettier
  */
 
-describe("defaultModelExpandDepth for JSON Schema 2020-12 keywords", () => {
-  it("should expand schemas", () => {
-    cy.visit("/pages/json-schema-2020-12-default-model-expand-depth/").then(
+describe("JSON Schema 2020-12 complex keywords expansion", () => {
+  it("should deeply expand all Schemas and complex keywords", () => {
+    cy.visit("/pages/json-schema-2020-12-expansion/").then(
       () => {
         cy.get(".json-schema-2020-12-accordion")
           .find(".json-schema-2020-12-accordion__icon--collapsed")
@@ -13,7 +13,7 @@ describe("defaultModelExpandDepth for JSON Schema 2020-12 keywords", () => {
         cy.contains("oneOf1-p1-p2-p1").should("exist")
         cy.contains("Prefix items").should("exist")
         cy.contains("exampleDef").should("exist")
-        cy.contains("core").should("exist")
+        cy.contains("https://json-schema.org/draft/2020-12/vocab/core").should("exist")
       }
     )
   })
