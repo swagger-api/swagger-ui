e2be707d7ca739106c7ee5a6a6db2cb0643b3c20
Vladimír Gorej <vladimir.gorej@gmail.com>
2024-03-06 14:20:45 +0100

    fix(oas31): render responses with empty content field (#9664)
    Refs #9199

---
+++ test/e2e-cypress/e2e/features/plugins/oas31/oas31-response-empty-content.cy.js
@@ -0,0 +1,21 @@
+/**
+ * @prettier
+ */
+
+describe("OpenAPI 3.1.0 response with empty content", () => {
+  it("should render a response", () => {
+    cy.visit(
+      "/?configUrl=/configs/oas31-response-no-content.yaml&url=/documents/features/oas31-response-empty-content.yaml"
+    )
+      .get("#operations-Enterprise-get_enterprise_detail")
+      .click()
+      .get(
+        "#operations-Enterprise-get_enterprise_detail [data-code=404] .response-col_description__inner"
+      )
+      .contains("No enterprise matching the requested ID could be found.")
+      .get(
+        "#operations-Enterprise-get_enterprise_detail [data-code=404] .model-example"
+      )
+      .should("not.exist")
+  })
+})
