11eff3a450f1f85381acf316ee4c4964b0fc457b
Oliwia Rogala <oliwia.rogala@smartbear.com>
2024-03-06 15:43:49 +0100

    refactor: change ModelExample to functional component

--- src/core/components/model-example.jsx
+++ src/core/components/model-example.jsx
@@ -1,146 +1,150 @@
-import React from "react"
+/**
+ * @prettier
+ */
+import React, { useEffect, useState } from "react"
 import PropTypes from "prop-types"
 import ImPropTypes from "react-immutable-proptypes"
 import cx from "classnames"
 import randomBytes from "randombytes"
 
-export default class ModelExample extends React.Component {
-  static propTypes = {
-    getComponent: PropTypes.func.isRequired,
-    specSelectors: PropTypes.object.isRequired,
-    schema: PropTypes.object.isRequired,
-    example: PropTypes.any.isRequired,
-    isExecute: PropTypes.bool,
-    getConfigs: PropTypes.func.isRequired,
-    specPath: ImPropTypes.list.isRequired,
-    includeReadOnly: PropTypes.bool,
-    includeWriteOnly: PropTypes.bool,
+const ModelExample = ({
+  getComponent,
+  specSelectors,
+  schema,
+  example,
+  isExecute,
+  getConfigs,
+  specPath,
+  includeReadOnly,
+  includeWriteOnly,
+}) => {
+  const { defaultModelExpandDepth, defaultModelRendering } = getConfigs()
+  const ModelWrapper = getComponent("ModelWrapper")
+  const HighlightCode = getComponent("highlightCode")
+  const exampleTabId = randomBytes(5).toString("base64")
+  const examplePanelId = randomBytes(5).toString("base64")
+  const modelTabId = randomBytes(5).toString("base64")
+  const modelPanelId = randomBytes(5).toString("base64")
+  const isOAS3 = specSelectors.isOAS3()
+
+  const [activeTab, setActiveTab] = useState(
+    !isExecute && defaultModelRendering === "model" && schema
+      ? "model"
+      : "example"
+  )
+
+  useEffect(() => {
+    if (isExecute && example) {
+      setActiveTab("example")
+    }
+  }, [isExecute, example])
+
+  const changeActiveTab = (e) => {
+    const {
+      target: {
+        dataset: { name },
+      },
+    } = e
+
+    setActiveTab(name)
   }
 
-  constructor(props, context) {
-    super(props, context)
-    let { getConfigs, isExecute, schema } = this.props
-    let { defaultModelRendering } = getConfigs()
-
-    let activeTab = defaultModelRendering
-
-    if (defaultModelRendering !== "example" && defaultModelRendering !== "model") {
-      activeTab = "example"
-    }
-
-    if (!schema) {
-      activeTab = "example"
-    }
-
-    if(isExecute) {
-      activeTab = "example"
-    }
-
-    this.state = {
-      activeTab,
-    }
-  }
-
-  activeTab = ( e ) => {
-    let { target : { dataset : { name } } } = e
-
-    this.setState({
-      activeTab: name
-    })
-  }
-
-  UNSAFE_componentWillReceiveProps(nextProps) {
-    if (
-      nextProps.isExecute &&
-      !this.props.isExecute &&
-      this.props.example
-    ) {
-      this.setState({ activeTab: "example" })
-    }
-  }
-
-  render() {
-    let { getComponent, specSelectors, schema, example, isExecute, getConfigs, specPath, includeReadOnly, includeWriteOnly } = this.props
-    let { defaultModelExpandDepth } = getConfigs()
-    const ModelWrapper = getComponent("ModelWrapper")
-    const HighlightCode = getComponent("highlightCode")
-    const exampleTabId = randomBytes(5).toString("base64")
-    const examplePanelId = randomBytes(5).toString("base64")
-    const modelTabId = randomBytes(5).toString("base64")
-    const modelPanelId = randomBytes(5).toString("base64")
-
-    let isOAS3 = specSelectors.isOAS3()
-
-    return (
-      <div className="model-example">
-        <ul className="tab" role="tablist">
-          <li className={cx("tabitem", { active: this.state.activeTab === "example" })} role="presentation">
+  return (
+    <div className="model-example">
+      <ul className="tab" role="tablist">
+        <li
+          className={cx("tabitem", {
+            active: activeTab === "example",
+          })}
+          role="presentation"
+        >
+          <button
+            aria-controls={examplePanelId}
+            aria-selected={activeTab === "example"}
+            className="tablinks"
+            data-name="example"
+            id={exampleTabId}
+            onClick={changeActiveTab}
+            role="tab"
+          >
+            {isExecute ? "Edit Value" : "Example Value"}
+          </button>
+        </li>
+        {schema && (
+          <li
+            className={cx("tabitem", {
+              active: activeTab === "model",
+            })}
+            role="presentation"
+          >
             <button
-              aria-controls={examplePanelId}
-              aria-selected={this.state.activeTab === "example"}
-              className="tablinks"
-              data-name="example"
-              id={exampleTabId}
-              onClick={ this.activeTab }
+              aria-controls={modelPanelId}
+              aria-selected={activeTab === "model"}
+              className={cx("tablinks", { inactive: isExecute })}
+              data-name="model"
+              id={modelTabId}
+              onClick={changeActiveTab}
               role="tab"
             >
-              {isExecute ? "Edit Value" : "Example Value"}
+              {isOAS3 ? "Schema" : "Model"}
             </button>
           </li>
-          { schema && (
-            <li className={cx("tabitem", { active: this.state.activeTab === "model" })} role="presentation">
-              <button
-                aria-controls={modelPanelId}
-                aria-selected={this.state.activeTab === "model"}
-                className={cx("tablinks", { inactive: isExecute })}
-                data-name="model"
-                id={modelTabId}
-                onClick={ this.activeTab }
-                role="tab"
-              >
-                {isOAS3 ? "Schema" : "Model" }
-              </button>
-            </li>
-          )}
-        </ul>
-        {this.state.activeTab === "example" && (
-          <div
-            aria-hidden={this.state.activeTab !== "example"}
-            aria-labelledby={exampleTabId}
-            data-name="examplePanel"
-            id={examplePanelId}
-            role="tabpanel"
-            tabIndex="0"
-          >
-            {example ? example : (
-              <HighlightCode value="(no example available)" getConfigs={ getConfigs } />
-            )}
-          </div>
         )}
-
-        {this.state.activeTab === "model" && (
-          <div
-            aria-hidden={this.state.activeTab === "example"}
-            aria-labelledby={modelTabId}
-            data-name="modelPanel"
-            id={modelPanelId}
-            role="tabpanel"
-            tabIndex="0"
-          >
-            <ModelWrapper
-              schema={ schema }
-              getComponent={ getComponent }
-              getConfigs={ getConfigs }
-              specSelectors={ specSelectors }
-              expandDepth={ defaultModelExpandDepth }
-              specPath={specPath}
-              includeReadOnly = {includeReadOnly}
-              includeWriteOnly = {includeWriteOnly}
+      </ul>
+      {activeTab === "example" && (
+        <div
+          aria-hidden={activeTab !== "example"}
+          aria-labelledby={exampleTabId}
+          data-name="examplePanel"
+          id={examplePanelId}
+          role="tabpanel"
+          tabIndex="0"
+        >
+          {example ? (
+            example
+          ) : (
+            <HighlightCode
+              value="(no example available)"
+              getConfigs={getConfigs}
             />
-          </div>
-        )}
-      </div>
-    )
-  }
-
+          )}
+        </div>
+      )}
+      {activeTab === "model" && (
+        <div
+          aria-hidden={activeTab === "example"}
+          aria-labelledby={modelTabId}
+          data-name="modelPanel"
+          id={modelPanelId}
+          role="tabpanel"
+          tabIndex="0"
+        >
+          <ModelWrapper
+            schema={schema}
+            getComponent={getComponent}
+            getConfigs={getConfigs}
+            specSelectors={specSelectors}
+            expandDepth={defaultModelExpandDepth}
+            specPath={specPath}
+            includeReadOnly={includeReadOnly}
+            includeWriteOnly={includeWriteOnly}
+          />
+        </div>
+      )}
+    </div>
+  )
 }
+
+ModelExample.propTypes = {
+  getComponent: PropTypes.func.isRequired,
+  specSelectors: PropTypes.object.isRequired,
+  schema: PropTypes.object.isRequired,
+  example: PropTypes.any.isRequired,
+  isExecute: PropTypes.bool,
+  getConfigs: PropTypes.func.isRequired,
+  specPath: ImPropTypes.list.isRequired,
+  includeReadOnly: PropTypes.bool,
+  includeWriteOnly: PropTypes.bool,
+}
+
+export default ModelExample
