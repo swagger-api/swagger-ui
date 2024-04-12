24da4208cc1d31ff599135f42c8573fda4c7d22b
Vladimir Gorej <vladimir.gorej@gmail.com>
2024-04-12 22:58:39 +0200

    fix: domNode merge

--- src/core/config/merge.js
+++ src/core/config/merge.js
@@ -6,10 +6,35 @@
  * deepExtend({ a: 1 }, { a: undefined }) => { a: undefined }
  *
  * NOTE1: lodash.merge & lodash.mergeWith prefers to ignore undefined values
- * NOTE2: oauth2RedirectUrl and withCredentials options can be set to undefined. By expecting null instead of undefined, we can't use lodash.merge.
+ * NOTE2: special handling of `domNode` option is now required as `deep-extend` will corrupt it (lodash.merge handles it correctly)
+ * NOTE3: oauth2RedirectUrl and withCredentials options can be set to undefined. By expecting null instead of undefined, we can't use lodash.merge.
  *
  * TODO(vladimir.gorej@gmail.com): remove deep-extend in favor of lodash.merge
  */
 import deepExtend from "deep-extend"
 
-export default deepExtend
+const merge = (target, ...sources) => {
+  let domNode = Symbol.for("domNode")
+  const sourcesWithoutDomNode = []
+
+  for (const source of sources) {
+    if (Object.hasOwn(source, "domNode")) {
+      domNode = source.domNode
+      const sourceWithoutDomNode = { ...source }
+      delete sourceWithoutDomNode.domNode
+      sourcesWithoutDomNode.push(sourceWithoutDomNode)
+    } else {
+      sourcesWithoutDomNode.push(source)
+    }
+  }
+
+  const merged = deepExtend(target, ...sourcesWithoutDomNode)
+
+  if (domNode !== Symbol.for("domNode")) {
+    merged.domNode = domNode
+  }
+
+  return merged
+}
+
+export default merge
