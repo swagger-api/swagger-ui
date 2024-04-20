c8f892d49eb0497cf62b4cbcd1f0092ff52d2b25
Vladimír Gorej <vladimir.gorej@gmail.com>
2024-04-20 08:07:32 +0200

    refactor(config): export config options typecasing mechanism (#9854)

--- src/core/config/index.js
+++ src/core/config/index.js
@@ -1,8 +1,12 @@
-export { default as inlinePluginOptionsFactorization }  from "./factorization/inline-plugin"
-export { default as storeOptionsFactorization }  from "./factorization/store"
-export { default as optionsFromQuery }  from "./sources/query"
-export { default as optionsFromURL }  from "./sources/url"
-export { default as optionsFromSystem }  from "./sources/system"
-export { default as optionsFromRuntime }  from "./sources/runtime"
-export { default as defaultOptions }  from "./defaults"
-export { default as mergeOptions }  from "./merge"
+/**
+ * @prettier
+ */
+export { default as inlinePluginOptionsFactorization } from "./factorization/inline-plugin"
+export { default as storeOptionsFactorization } from "./factorization/store"
+export { default as optionsFromQuery } from "./sources/query"
+export { default as optionsFromURL } from "./sources/url"
+export { default as optionsFromSystem } from "./sources/system"
+export { default as optionsFromRuntime } from "./sources/runtime"
+export { default as defaultOptions } from "./defaults"
+export { default as mergeOptions } from "./merge"
+export { default as typeCastOptions } from "./type-cast"
