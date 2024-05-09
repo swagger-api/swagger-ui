6b5f23af5885e360c45279691a88e682fbea6075
Oliwia Rogala <oliwia.rogala@smartbear.com>
2024-05-09 09:21:51 +0200

    update test

--- test/e2e-cypress/e2e/features/try-it-out-reset.cy.js
+++ test/e2e-cypress/e2e/features/try-it-out-reset.cy.js
@@ -4,29 +4,24 @@
 
 describe("Reset button in try it out", () => {
   it("should reset the edited request body value and execute try it out with the default value", () => {
-    cy.visit("?url=/documents/features/oas3-try-it-out-reset.yaml")
+    cy.visit("?url=/documents/features/try-it-out-reset.yaml")
       .get("#operations-default-post_users")
       .click()
-      // Expand Try It Out
       .get(".try-out__btn")
       .click()
-      // replace multiple default values with bad value
       .get(`.parameters[data-property-name="name"] input[type=text]`)
       .type("{selectall}not the default name value")
       .get(`.parameters[data-property-name="badgeid"] input[type=text]`)
       .type("{selectall}not the default badge value")
-      // Reset Try It Out
+      .get(`.parameters[data-property-name="email"] input[type=text]`)
+      .type("{selectall}not the default email value")
       .get(".try-out__btn.reset")
       .click()
-      // Submit using default value
-      .get(".btn.execute")
-      .click()
-      // No required validation error on body parameter
-      .get(`.parameters[data-property-name="name"] input`)
+      .get(`.parameters[data-property-name="name"] input[type=text]`)
       .should("have.value", "default name")
-      .and("not.have.class", "invalid")
-      .get(`.parameters[data-property-name="badgeid"] input`)
+      .get(`.parameters[data-property-name="badgeid"] input[type=text]`)
       .should("have.value", "12345")
-      .and("not.have.class", "invalid")
+      .get(`.parameters[data-property-name="email"] input[type=text]`)
+      .should("have.value", "jsmith@business.com")
   })
 })
