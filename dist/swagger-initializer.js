8ff5904b51cf8775de3a9e5e4ed9c8615a2bdea5
josemaxeichenberger <jme.jose.max@gmail.com>
2025-04-19 23:02:04 -0300

    swager-demo

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "../../swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
