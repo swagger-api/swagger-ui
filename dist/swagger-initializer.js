37d275359d909476cb0deeb55e2028fad6be43d2
yaniv-almaliah <yaniva@safeguard.co.il>
2025-04-28 14:19:34 +0300

    first commit

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,9 +3,12 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "https://petstore.swagger.io/v2/swagger.json",
+    url: "swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
+    validatorUrl: null, // disable external validation
+    displayOperationId: true,  // <--- THIS makes the summary show at the right
+    docExpansion: 'list',      // <--- This makes all endpoints collapsed initially
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
