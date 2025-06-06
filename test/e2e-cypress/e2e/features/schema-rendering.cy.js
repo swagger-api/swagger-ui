193af5318854003223c8682bde73823d98ae0b96
lukasz.zazulak <lukasz.zazulak@smartbear.com>
2025-06-06 20:41:46 +0200

    ft: show the schema tab in the Try it Out mode

--- test/e2e-cypress/e2e/features/schema-rendering.cy.js
+++ test/e2e-cypress/e2e/features/schema-rendering.cy.js
@@ -6,20 +6,20 @@ describe("OpenAPI 3.0 Schema rendering", () => {
       .click()
       .get(".btn.try-out__btn")
       .click()
-      .get('.opblock-section-request-body')
+      .get(".opblock-section-request-body")
       .within(() => {
-        cy.contains('button', 'Schema')
-          .should('exist')
-          .and('be.visible')
+        cy.contains("button", "Schema")
+          .should("exist")
+          .and("be.visible")
           .click()
 
         cy
           .get('[data-name="modelPanel"]')
-          .should('be.visible')
+          .should("be.visible")
 
         cy
           .get('[data-name="examplePanel"]')
-          .should('not.exist')
+          .should("not.exist")
       })
   })
 })
