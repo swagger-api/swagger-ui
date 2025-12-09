2319a0d78523da1d580f1548f521b23cea2e51d9
Oliwia Rogala <oliwia.rogala@smartbear.com>
2025-12-09 14:30:20 +0100

    fix: bind function in constructor

--- src/standalone/plugins/top-bar/components/DarkModeToggle.jsx
+++ src/standalone/plugins/top-bar/components/DarkModeToggle.jsx
@@ -12,13 +12,13 @@ class DarkModeToggle extends Component {
     this.state = {
       isDarkMode: false,
     }
+    this.toggleIsDarkMode = this.toggleIsDarkMode.bind(this)
   }
 
   componentDidMount() {
     if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
       document.documentElement.classList.add("dark-mode")
       this.setState({ isDarkMode: true })
-      this.toggleIsDarkMode = this.toggleIsDarkMode.bind(this)
     }
   }
 
